d16c1d251170e21ae48ac4d4551053ef9b90f53b
Mikhail Wall <mikhailwall@xtuples-MacBook-Pro.local>
2011-12-21 17:02:51 -0500

    updated account.sql (account model views testing file)

---
+++ server/database/business/account/tests/account.sql
@@ -0,0 +1,6 @@
+insert into xm.account (
+  id, 'number', name, is_active, 'type', owner, parent, notes, primary_contact, secondary_contact)
+values (
+  99999, '99999', 'TEST NAME', true, 'O', 'admin', NULL, 'TEST NOTES', 1, 6);
+
+
