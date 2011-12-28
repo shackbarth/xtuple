36b5454c33f626296d7b03aa20a9c8d03ae19341
Mikhail Wall <mwall@xtuple.com>
2011-12-28 12:28:13 -0500

    updated xm.incident to use private.create_model()

--- server/database/business/account/incident/xm/views/incident.sql
+++ server/database/business/account/incident/xm/views/incident.sql
@@ -1,76 +1,81 @@
-﻿select dropIfExists('VIEW', 'incident', 'xm');
-select dropIfExists('VIEW', 'incident_info', 'xm');
+﻿select private.create_model(
 
--- return rule
+-- Model name, schema, table
 
-create or replace view xm.incident as 
+'incident', 'public', 'incdt',
 
-select
-  incdt_id as id,
-  incdt_number as "number",
-  incdt_crmacct_id as account,
-  incdt_cntct_id as contacts,
-  incdt_summary as "name",
-  incdt_owner_username as "owner",
-  incdt_assigned_username as assigned_to,
-  cast(incdt_timestamp AS date) as start_date,
-  null::date as due_date,
-  null::date as assign_date,
-  null::date as complete_date,
-  incdt_descrip as notes,
-  incdt_incdtpriority_id as priority,
-  btrim(array(
-  select alarm_id 
-  from alarm
-  where alarm_source_id = incdt_id 
-    and alarm_source = 'INCDT')::text,'{}') as alarms,
-  btrim(array(
-  select charass_id 
-  from charass
-  where charass_target_id = incdt_id 
-    and charass_target_type = 'INCDT')::text,'{}') as characteristics,
-  btrim(array(
-  select comment_id
-  from "comment"
-  where comment_source_id = incdt_id
-    and comment_source = 'INCDT')::text,'{}') as "comments",  
-  btrim(array(
-  select incdthist_id
-  from incdthist
-  where incdthist_incdt_id = incdt_id)::text,'{}') as history,  
-  incdt_status != 'L' as is_active,
-  case incdt_status 
-  when 'N' then 'new'
-     when 'F' then 'feedback'
-     when 'C' then 'confirmed'
-     when 'A' then 'assigned'
-     when 'R' then 'resolved'
-     when 'L' then 'closed'
-  else '?'
-   end as incident_status,
-  incdt_public as is_public,
-  incdt_incdtresolution_id as resolution,  
-  incdt_incdtseverity_id as severity,
-  btrim(array(
-  select docass_id 
-  from docass
-  where docass_target_id = incdt_id 
-    and docass_target_type = 'INCDT'
-  union all
-  select docass_id 
-  from docass
-  where docass_source_id = incdt_id 
-    and docass_source_type = 'INCDT'
-  union all
-  select imageass_id 
-  from imageass
-  where imageass_source_id = incdt_id 
-    and imageass_source = 'INCDT')::text,'{}') as documents
-from incdt;
+-- Columns
+
+E'{
+  "incdt.incdt_id as id",
+  "incdt.incdt_number as \\"number\\"",
+  "incdt.incdt_crmacct_id as account",
+  "incdt.incdt_cntct_id as contacts",
+  "incdt.incdt_summary as \\"name\\"",
+  "incdt.incdt_owner_username as \\"owner\\"",
+  "incdt.incdt_assigned_username as assigned_to",
+  "cast(incdt.incdt_timestamp AS date) as start_date",
+  "null::date as due_date",
+  "null::date as assign_date",
+  "null::date as complete_date",
+  "incdt.incdt_descrip as notes",
+  "incdt.incdt_incdtpriority_id as priority",
+  "btrim(array(
+   select alarm_id 
+   from alarm
+   where alarm_source_id = incdt.incdt_id 
+    and alarm_source = \'INCDT\')::text,\'{}\') as alarms",
+  "btrim(array(
+   select charass_id 
+   from charass
+   where charass_target_id = incdt.incdt_id 
+    and charass_target_type = \'INCDT\')::text,\'{}\') as characteristics",
+  "btrim(array(
+   select comment_id
+   from \\"comment\\"
+   where comment_source_id = incdt.incdt_id
+    and comment_source = \'INCDT\')::text,\'{}\') as \\"comments\\"",
+  "btrim(array(
+   select incdthist_id
+   from incdthist
+   where incdthist_incdt_id = incdt.incdt_id)::text,\'{}\') as history",
+  "incdt.incdt_status != \'L\' as is_active",
+  "case incdt.incdt_status 
+   when \'N\' then \'new\'
+     when \'F\' then \'feedback\'
+     when \'C\' then \'confirmed\'
+     when \'A\' then \'assigned\'
+     when \'R\' then \'resolved\'
+     when \'L\' then \'closed\'
+   else \'?\'
+   end as incident_status",
+  "incdt.incdt_public as is_public",
+  "incdt.incdt_incdtresolution_id as resolution",
+  "incdt.incdt_incdtseverity_id as severity",
+  "btrim(array(
+   select docass_id 
+   from docass
+   where docass_target_id = incdt.incdt_id 
+    and docass_target_type = \'INCDT\'
+   union all
+   select docass_id 
+   from docass
+   where docass_source_id = incdt.incdt_id 
+    and docass_source_type = \'INCDT\'
+   union all
+   select imageass_id 
+   from imageass
+   where imageass_source_id = incdt.incdt_id 
+    and imageass_source = \'INCDT\')::text,\'{}\') as documents"
+}',
+
+-- Rules
+
+E'{"
 
 -- insert rule
 
-create or replace rule "_CREATE" as on insert to xm.incident 
+create or replace rule \\"_CREATE\\" as on insert to xm.incident 
   do instead
 
 insert into incdt (
@@ -104,9 +109,11 @@ values (
   new.resolution,
   new.severity );
 
+","
+
 -- update rule
 
-create or replace rule "_UPDATE" as on update to xm.incident
+create or replace rule \\"_UPDATE\\" as on update to xm.incident
   do instead
 
 update incdt set
@@ -120,21 +127,28 @@ update incdt set
   incdt_descrip = new.notes,
   incdt_incdtpriority_id = new.priority,
   incdt_status = case new.incident_status 
-         when 'new' then 'N'
-        when 'feedback' then 'F'
-        when 'confirmed' then 'C'
-        when 'assigned' then 'A'
-        when 'resolved' then 'R'
-        when 'closed' then 'L'
-        else '?' 
+         when \'new\' then \'N\'
+        when \'feedback\' then \'F\'
+        when \'confirmed\' then \'C\'
+        when \'assigned\' then \'A\'
+        when \'resolved\' then \'R\'
+        when \'closed\' then \'L\'
+        else \'?\' 
        end,
   incdt_public = new.is_public,
   incdt_incdtresolution_id = new.resolution,
   incdt_incdtseverity_id = new.severity
 where ( incdt_id = old.id );
 
+","
+
 -- delete rules
 
-create or replace rule "_DELETE" as on delete to xm.incident 
+create or replace rule \\"_DELETE\\" as on delete to xm.incident 
   do instead nothing;
 
+"}',
+
+-- Conditions, Comment, System
+
+'{}', 'Incident Model', true);
