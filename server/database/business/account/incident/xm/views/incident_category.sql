b7905de24b9c0a50525d684fa90d1193e1daa355
Mikhail Wall <mwall@xtuple.com>
2011-12-28 16:33:49 -0500

    updated xm.incident_category to use private.create_model()

--- server/database/business/account/incident/xm/views/incident_category.sql
+++ server/database/business/account/incident/xm/views/incident_category.sql
@@ -1,19 +1,25 @@
-select dropIfExists('VIEW', 'incident_category', 'xm');
+select private.create_model(
 
--- return rule
+-- Model name, schema, table
 
-create or replace view xm.incident_category as
+'incdident_category', 'public', 'incdtcat',
 
-select 
-  incdtcat_id as id,
-  incdtcat_descrip as description,
-  incdtcat_name as name,
-  incdtcat_order as order
-from incdtcat;  
+-- Columns
+
+E'{
+  "incdtcat.incdtcat_id as id",
+  "incdtcat.incdtcat_descrip as description",
+  "incdtcat.incdtcat_name as name",
+  "incdtcat.incdtcat_order as order"
+  }',
+
+-- Rules
+
+E'{"
 
 -- insert rule
 
-create or replace rule "_CREATE" as on insert to xm.incident_category 
+create or replace rule \\"_CREATE\\" as on insert to xm.incident_category 
   do instead
 
 insert into incdtcat (
@@ -26,10 +32,12 @@ values (
   new.description,
   new.name,
   new.order );
+
+","
   
 -- update rule
 
-create or replace rule "_UPDATE" as on update to xm.incident_category
+create or replace rule \\"_UPDATE\\" as on update to xm.incident_category
   do instead
   
 update incdtcat set
@@ -37,11 +45,19 @@ update incdtcat set
   incdtcat_name = new.name,
   incdtcat_order = new.order 
 where ( incdtcat_id = old.id );
+
+","
   
 -- delete rules
 
-create or replace rule "_DELETE" as on delete to xm.incident_category   
+create or replace rule \\"_DELETE\\" as on delete to xm.incident_category   
   do instead
   
 delete from incdtcat 
 where ( incdtcat_id = old.id );
+
+"}',
+
+-- Conditions, Comment, System
+
+'{}', 'Incident Category Model', true);
