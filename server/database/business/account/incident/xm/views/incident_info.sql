5cf2b8e048b20a13e44deaa4659d06470a83ab77
Mikhail Wall <mwall@xtuple.com>
2011-12-29 10:51:19 -0500

    update xm.incident_info to use private.create_model()

--- server/database/business/account/incident/xm/views/incident_info.sql
+++ server/database/business/account/incident/xm/views/incident_info.sql
@@ -1,28 +1,43 @@
-select dropIfExists('VIEW', 'incident_info', 'xm');
+﻿select private.create_model(
 
--- return rule
+-- Model name, schema, table
 
-create or replace view xm.incident_info as
-  
-select
-  id,
-  "number",
-  "name",
-  is_active
-from xm.incident;
+'incident_info', 'public', 'incdt',
+
+-- Columns
+
+E'{
+  "incdt.incdt_id as id",
+  "incdt.incdt_number as \\"number\\"",
+  "incdt.incdt_summary as \\"name\\"",
+  "incdt.incdt_status != \'L\' as is_active"
+}',
+
+-- Rules
+
+E'{"
 
 -- insert rule
 
-create or replace rule "_CREATE" as on insert to xm.incident_info 
+create or replace rule \\"_CREATE\\" as on insert to xm.incident_info 
   do instead nothing;
 
+","
 
 -- update rule
 
-create or replace rule "_UPDATE" as on update to xm.incident_info
+create or replace rule \\"_UPDATE\\" as on update to xm.incident_info
   do instead nothing;
+
+","
   
 -- delete rules
 
-create or replace rule "_DELETE" as on delete to xm.incident_info   
+create or replace rule \\"_DELETE\\" as on delete to xm.incident_info   
   do instead nothing;
+
+"}',
+
+-- Conditions, Comment, System
+
+'{}', 'Incident Info Model', true);
