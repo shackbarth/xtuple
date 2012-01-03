092763547b62b8e796bfd09f630c46651551215e
Mikhail Wall <mwall@xtuple.com>
2012-01-03 11:13:22 -0500

    updated xm.item_substitute to use private.create_model()

--- server/database/core/item/xm/views/item_substitute.sql
+++ server/database/core/item/xm/views/item_substitute.sql
@@ -1,20 +1,26 @@
-﻿select dropIfExists('VIEW', 'item_substitute', 'xm');
+﻿select private.create_model(
 
--- return rule
+-- Model name, schema, table
 
-create or replace view xm.item_substitute as
+'item_substitute', 'public', 'itemsub',
 
-select
-  itemsub_id as id,
-  itemsub_parent_item_id as root_item,
-  itemsub_sub_item_id as substitute_item,
-  itemsub_uomratio as conversion_ratio,
-  itemsub_rank as rank
-from itemsub;
+-- Columns
+
+E'{
+  "itemsub.itemsub_id as id",
+  "itemsub.itemsub_parent_item_id as root_item",
+  "itemsub.itemsub_sub_item_id as substitute_item",
+  "itemsub.itemsub_uomratio as conversion_ratio",
+  "itemsub.itemsub_rank as rank"
+}',
+
+-- Rules
+
+E'{"
 
 -- insert rule
 
-create or replace rule "_CREATE" as on insert to xm.item_substitute
+create or replace rule \\"_CREATE\\" as on insert to xm.item_substitute
   do instead
 
 insert into itemsub (
@@ -30,9 +36,11 @@ values (
   new.conversion_ratio,
   new.rank );
 
+","
+
 -- update rule
 
-create or replace rule "_UPDATE" as on update to xm.item_substitute
+create or replace rule \\"_UPDATE\\" as on update to xm.item_substitute
   do instead
 
 update itemsub set
@@ -41,10 +49,18 @@ update itemsub set
   itemsub_rank = new.rank
 where ( itemsub_id = old.id );
 
+","
+
 -- delete rule
 
-create or replace rule "_DELETE" as on delete to xm.item_substitute
+create or replace rule \\"_DELETE\\" as on delete to xm.item_substitute
   do instead
 
 delete from itemsub
 where ( itemsub_id = old.id );
+
+"}',
+
+-- Conditions, Comment, System
+
+'{}', 'Item Substitute Model', true);
