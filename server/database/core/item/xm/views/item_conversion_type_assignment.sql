b72472cf194068dec572f6e66415c07c4d4e3a24
Mikhail Wall <mwall@xtuple.com>
2012-01-03 08:59:04 -0500

    updated xm.item_conversion_type_assignment to use private.create_model()

--- server/database/core/item/xm/views/item_conversion_type_assignment.sql
+++ server/database/core/item/xm/views/item_conversion_type_assignment.sql
@@ -1,18 +1,23 @@
-﻿select dropIfExists('VIEW', 'item_conversion_type_assignment', 'xm');
+﻿select private.create_model(
 
--- return rule
+-- Model name, schema, table
+'item_conversion_type_assignment', 'public', 'itemuom',
 
-create or replace view xm.item_conversion_type_assignment as
+-- Columns
 
-select  
+E'{
   itemuom_id as id,
   itemuom_itemuomconv_id as item_conversion,
   itemuom_uomtype_id as unit_type
-from itemuom;
+}',
+
+-- Rules
+
+E'{"
 
 -- insert rule
 
-create or replace rule "_CREATE" as on insert to xm.item_conversion_type_assignment
+create or replace rule \\"_CREATE\\" as on insert to xm.item_conversion_type_assignment
   do instead
 
 insert into itemuom (
@@ -24,15 +29,25 @@ values (
   new.item_conversion,
   new.unit_type );
 
+","
+
 -- update rule
 
-create or replace rule "_UPDATE" as on update to xm.item_conversion_type_assignment
+create or replace rule \\"_UPDATE\\" as on update to xm.item_conversion_type_assignment
   do instead nothing;
+
+","
 
 -- delete rule
 
-create or replace rule "_DELETE" as on delete to xm.item_conversion_type_assignment
+create or replace rule \\"_DELETE\\" as on delete to xm.item_conversion_type_assignment
   do instead
 
 delete from itemuom
 where ( itemuom_id = old.id );
+
+"}',
+
+-- Conditions, Comment, System
+
+'{}', 'Item Conversion Type Assignment Model', true);
