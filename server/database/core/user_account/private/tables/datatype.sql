f7c7377a12efa4650ae764f30bdbc33bbf0c96fe
John Rogelstad <john@xtuple.com>
2012-01-25 20:52:09 -0500

    Remove files to be consolidated.

--- server/database/core/user_account/private/tables/datatype.sql
+++
@@ -1,4 +0,0 @@
--- populate data
-
-insert into private.datatype (datatype_name, datatype_descrip, datatype_source) 
-select 'User', '_user','U' where not exists (select * from private.datatype where datatype_name = 'User');
