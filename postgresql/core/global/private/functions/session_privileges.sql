0c51468e9f9955b2ca3d118570852869e1164f55
Steven Negron <steve@catchproductions.net>
2012-02-03 16:00:47 -0500

    Function for session privileges

---
+++ postgresql/core/global/private/functions/session_privileges.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION private.session_privilege() RETURNS privgranted AS $$
+    SELECT ROW(*)::privgranted;
+$$ LANGUAGE SQL;
+
+/*create or replace function private.session_privilege() 
+  returns privgranted as $$
+-- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
+-- See www.xm.ple.com/CPAL for the full text of the software license.
+  select * from privgranted;
+$$ LANGUAGE 'sql';
+
+
+
+select sessio
+
+*/
