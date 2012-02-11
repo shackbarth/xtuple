609cfc6fda0454f4c1473cad23cbb0f60bccdc2e
John Rogelstad <john@xtuple.com>
2012-02-11 08:45:36 -0500

    Update init script to match new locations.

--- postgresql/init_script.sql
+++ postgresql/init_script.sql
@@ -9,72 +9,65 @@
 
 -- [ END ] initdb
 
--- [ START ] core
+-- [ START ] private
 
--- core/global functions
-\i core/global/private/functions/add_column.sql;
-\i core/global/private/functions/add_constraint.sql;
-\i core/global/private/functions/add_primary_key.sql;
-\i core/global/private/functions/any_numeric.sql;
-\i core/global/private/functions/any_text.sql;
-\i core/global/private/functions/commit_record.sql;
-\i core/global/private/functions/create_table.sql;
-\i core/global/private/functions/create_orm_view.sql;
-\i core/global/private/functions/drop_orm_view.sql;
-\i core/global/private/functions/ends_with.sql;
-\i core/global/private/functions/execute_query.sql;
-\i core/global/private/functions/extend_model.sql;
-\i core/global/private/functions/fetch.sql;
-\i core/global/private/functions/get_id.sql;
-\i core/global/private/functions/install_orm.sql;
-\i core/global/private/functions/is_date.sql;
-\i core/global/private/functions/raise_exception.sql;
-\i core/global/private/functions/retrieve_record.sql;
-\i core/global/private/functions/starts_with.sql;
-\i core/global/private/functions/text_gt_date.sql;
-\i core/global/private/functions/text_lt_date.sql;
-\i core/global/private/functions/validate_user.sql;
-\i core/global/private/functions/session_privileges.sql;
-\i core/global/private/functions/session_metrics.sql;
-\i core/global/private/functions/session_locale.sql;
-\i core/global/private/functions/validate_user.sql;
+-- private functions
+\i private/functions/add_column.sql;
+\i private/functions/add_constraint.sql;
+\i private/functions/add_primary_key.sql;
+\i private/functions/any_numeric.sql;
+\i private/functions/any_text.sql;
+\i private/functions/commit_record.sql;
+\i private/functions/create_table.sql;
+\i private/functions/create_orm_view.sql;
+\i private/functions/drop_orm_view.sql;
+\i private/functions/ends_with.sql;
+\i private/functions/execute_query.sql;
+\i private/functions/fetch.sql;
+\i private/functions/get_id.sql;
+\i private/functions/install_orm.sql;
+\i private/functions/is_date.sql;
+\i private/functions/raise_exception.sql;
+\i private/functions/retrieve_record.sql;
+\i private/functions/starts_with.sql;
+\i private/functions/text_gt_date.sql;
+\i private/functions/text_lt_date.sql;
+\i private/functions/validate_user.sql;
+\i private/functions/session_privileges.sql;
+\i private/functions/session_metrics.sql;
+\i private/functions/session_locale.sql;
+\i private/functions/validate_user.sql;
 
--- core/trigger functions
-\i core/global/private/trigger_functions/orm_did_change.sql
+-- private trigger functions
+\i private/trigger_functions/orm_did_change.sql
+\i private/trigger_functions/useracct_duplicate_check.sql
 
--- core/global tables
-\i core/global/private/tables/orm.sql
+-- private operators
+\i private/operators/any_numeric.sql;
+\i private/operators/any_text.sql;
+\i private/operators/ends_with.sql;
+\i private/operators/starts_with.sql;
+\i private/operators/text_gt_date.sql;
+\i private/operators/text_lt_date.sql;
 
--- core/operators
-\i core/global/private/operators/any_numeric.sql;
-\i core/global/private/operators/any_text.sql;
-\i core/global/private/operators/ends_with.sql;
-\i core/global/private/operators/starts_with.sql;
-\i core/global/private/operators/text_gt_date.sql;
-\i core/global/private/operators/text_lt_date.sql;
+-- private tables
+\i private/tables/orm.sql
+\i private/tables/useracct.sql
 
--- core/user_account triggers
-\i core/user_account/private/trigger_functions/useracct_duplicate_check.sql;
+-- private views
+\i private/views/comment.sql;
+\i private/views/docinfo.sql; 
+\i private/views/usr.sql;
 
--- core/user_account tables
-\i core/user_account/private/tables/useracct.sql;
+-- [ END ] private
 
--- core/user_account views
-\i core/user_account/private/views/usr.sql;
+-- [ START ] xm
 
--- core/global/xm/functions
-\i core/global/xm/functions/dispatch.sql;
-\i core/global/xm/functions/fetch_number.sql;
-\i core/global/xm/functions/fetch_id.sql;
+-- xm/functions
+\i xm/functions/address_find_existing.sql;
+\i xm/functions/address_use_count.sql;
+\i xm/functions/dispatch.sql;
+\i xm/functions/fetch_number.sql;
+\i xm/functions/fetch_id.sql;
 
--- core/document xm views
-\i core/document/private/views/docinfo.sql; 
-
--- core/global/xm/functions
-\i core/global/xm/functions/fetch_number.sql;
-\i core/global/xm/functions/fetch_id.sql;
-
--- core/comment/private
-\i core/comment/private/comment.sql; 
-
--- [ END ] core
+-- [ END ] xm
