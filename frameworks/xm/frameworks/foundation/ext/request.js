4e566987a90f635b62403ceac526a18853dd1265
clinuz <cole@xtuple.com>
2012-02-16 10:53:54 -0500

    inspect response for error, use correct parameter for username/password

--- frameworks/xm/frameworks/foundation/ext/request.js
+++ frameworks/xm/frameworks/foundation/ext/request.js
@@ -9,6 +9,17 @@ XM.Request = SC.Request.extend({
   willSend: function(request, response) {
     var session = XM.DataSource.get('session') || {};
     SC.mixin(request.body, session);
+  },
+
+  didReceive: function(request, response) {
+    var body = response.get('body');
+    if(SC.typeOf(body) === SC.T_HASH) {
+      if(body.error) {
+        console.log("\n\n**** ERROR RESPONSE FROM SERVER: %@\n\n".fmt(
+          body.message || "NO MESSAGE"));
+        response.cancel();
+      }
+    }
   }
 
 });
