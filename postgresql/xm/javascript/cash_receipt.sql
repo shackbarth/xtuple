670f595bf54577ee8abcf2ae5ffec7cdb9c6a7b5
Steven Negron <steve@catchproductions.net>
2012-02-28 12:30:17 -0500

    Updates to the orm and also new function for cash receipt

--- postgresql/xm/javascript/cash_receipt.sql
+++ postgresql/xm/javascript/cash_receipt.sql
@@ -1,18 +1,14 @@
 select xt.install_js('XM','CashReceipt','xtuple', $$
   /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
-     See www.xm.ple.com/CPAL for the full text of the software license. 
-     
-     Not Working Still need to run test and pass all correct number of params
-     
-     */
+     See www.xm.ple.com/CPAL for the full text of the software license. */
 
   XM.CashReceipt = {};
   
   XM.CashReceipt.isDispatchable = true;
   
   XM.CashReceipt.post = function() {
-  /* On post it has to pass to integers
-		Passes in 2 params both integer
+  /* check privilege to post cash receipt than pass id and journal number to function postcashreceipt()
+  
    @param {Object} Invoice object
    @returns {Integer}
   */
@@ -20,12 +16,11 @@ select xt.install_js('XM','CashReceipt','xtuple', $$
     if(!data.checkPrivilege('PostCashReceipts')) throw new Error("Access Denied");
     var args = arguments[0];
     
-    return executeSql('select postreceipt($1,$2) as result', [args.id])[0].result;
+    return executeSql('select postcashreceipt($1, fetchJournalNumber('C/R')) AS result', [args.id])[0].result;
   }
   
   XM.CashReceipt.void = function() {
-  /* Check privilege than void invoice set is_void to true
-	Pass 2 params integer for both
+  /* check privilege to void cash receipts than pass id and journal number to function reverseCashReceipt
    @param {Object} Invoice object
    @returns {Integer} 
   */
@@ -33,7 +28,7 @@ select xt.install_js('XM','CashReceipt','xtuple', $$
     if(!data.checkPrivilege('VoidPostedCashReceipts')) throw new Error("Access Denied");
     var args = arguments[0];
     
-    return executeSql('select reversecashreceipt($1,$2) as result', [args.id])[0].result;
+    return executeSql('select reversecashreceipt($1, fetchJournalNumber('C/R')) AS result', [args.id])[0].result;
   }  
   
 $$ );
