8fb8419aca1b5b66650b292f5853652db78220ab
Steven Negron <steve@catchproductions.net>
2012-02-29 16:21:56 -0500

    Update has been made to invoice javscript function still running test on different cases

--- postgresql/xm/javascript/invoice.sql
+++ postgresql/xm/javascript/invoice.sql
@@ -6,30 +6,107 @@ select xt.install_js('XM','Invoice','xtuple', $$
   
   XM.Invoice.isDispatchable = true;
   
-  XM.Invoice.post = function() {
-  /* checks privilege to post than pass id to function postinvoice()
+	XM.Invoice.post = function(invoiceId) {
+	/**
+	 Post a invoice.
 
-   @param {Object} Invoice object
-   @returns {Integer}
-  */
-    data = Object.create(XT.Data);
-    if(!data.checkPrivilege('PostMiscInvoices')) throw new Error("Access Denied");
-    var args = arguments[0];
-    
-    return executeSql('select postinvoice($1) as result', [args.id])[0].result;
-  }
-  
-  XM.Invoice.void = function() {
-  /* checks privilege to void than pass id to function voidinvoice()
+	 @param {Number} Invoice ID 
+	 @returns {Number} 
+	*/
+	  var ret, sql, err,
+			  data = Object.create(XT.Data);
 
-   @param {Object} Invoice object
-   @returns {Integer}
+	  if(!data.checkPrivilege("PostMiscInvoices")) err = "Access denied."
+	  else if(invoiceId === undefined) err = "No Invoice specified";
+
+	  if(!err) {
+			ret = executeSql("select postinvoice($1) AS result;", [invoiceId])[0].result;
+
+			switch (ret)
+			{
+			  case -1: 
+					err = "insertIntoGLSeries";
+					break;
+			  case -4: 
+					err = "insertIntoGLSeries";
+					break;				
+			  case -5: 
+					err = "postGLSeries";
+					break;
+			  case -10: 
+					err = "Unable to post this Invoice because it has already been posted.";
+					break;
+			  case -11:
+					err = "Unable to post this Invoice because the Sales Account was not found.";
+					break;
+			  case -12:
+					err = "Unable to post this Invoice because there was an error processing Line Item taxes.";
+					break;
+			  case -13: 
+					err = "Unable to post this Invoice because there was an error processing Misc. Line Item taxes.";
+					break;
+			  case -14: 
+					err = "Unable to post this Invoice because the Freight Account was not found.";
+					break;				
+			  case -15: 
+					err = "Unable to post this Invoice because there was an error processing Freight taxes.";
+					break;
+			  case -16: 
+					err = "Unable to post this Invoice because there was an error processing Tax Adjustments.";
+					break;
+			  case -17:
+					err = "Unable to post this Invoice because the A/R Account was not found.";
+					break;				
+			  default:
+					return ret;	  
+			}
+	  }
+
+	  throw new Error(err);
+	}
+
+	XM.Invoice.void = function(invoiceId) {
+  /**
+   Void a invoice.
+
+   @param {Number} Invoice ID 
+   @returns {Number} 
   */
-    data = Object.create(XT.Data);
-    if(!data.checkPrivilege('VoidPostedInvoices')) throw new Error("Access Denied");
-    var args = arguments[0];
-    
-    return executeSql('select voidinvoice($1) as result', [args.id])[0].result;
-  }  
+	  var ret, sql, err,
+			  data = Object.create(XT.Data);
+
+	  if(!data.checkPrivilege("VoidPostedInvoices")) err = "Access denied."
+	  else if(invoiceId === undefined) err = "No Invoice specified";
+
+	  if(!err) {
+			ret = executeSql("select voidinvoice($1) AS result;", [invoiceId])[0].result;
+
+			switch (ret)
+			{
+			  case -1: 
+					err = "insertIntoGLSeries";
+					break;
+			  case -4: 
+					err = "insertIntoGLSeries";
+					break;				
+			  case -5: 
+					err = "postGLSeries";
+					break;
+			  case -10: 
+					err = "Unable to void this Credit Memo because it has not been posted.";
+					break;
+			  case -11:
+					err = "Unable to void this Credit Memo because the Sales Account was not found.";
+					break;
+			  case -20:
+					err = "Unable to void this Credit Memo because there A/R Applications posted against this Credit Memo.";
+					break;
+			  default:
+					return ret;	  
+			}
+	  }
+
+	  throw new Error(err);
+	}  
   
 $$ );
