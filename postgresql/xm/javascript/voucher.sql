54225147a0bfed4f2da5d682a9f2bbd2fa6a965d
Mikhail Wall <mwall@xtuple.com>
2012-02-29 15:23:17 -0500

    fixes to voucher js functions (3rd pass)

--- postgresql/xm/javascript/voucher.sql
+++ postgresql/xm/javascript/voucher.sql
@@ -6,33 +6,75 @@
   
   XM.Voucher.isDispatchable = true;
   
-  XM.Voucher.post = function() {
-  /* checks privilege to post then pass id to function postvoucher()
+  /** 
+   Posts 1 or more vouchers to the payables sub-ledger
 
-   @param {Object} Voucher object
-   @returns {Integer}
+   @param {Number} Voucher ID (optional)
+   @returns {Number}
   */
+  XM.Voucher.post = function(voucherId) {
+    var data, ret, err;
+
     data = Object.create(XT.Data);
-    if(!data.checkPrivilege('PostVouchers')) throw new Error("Access Denied");
-    var args = arguments[0];
-    
-    if(args.id === undefined) return executeSql('select postvoucher(false) as result')[0].result;
-    
-    return executeSql('select postvoucher($1, false) as result', [args.id])[0].result;
-    
+    if(!data.checkPrivilege('PostVouchers')) err = "Access Denied.";
+
+    if(!err) {
+      if(voucherId === undefined) ret = executeSql("select postvoucher(false) as result;")[0].result;
+
+      else ret = executeSql("select postvoucher($1, false) as result;", [voucherId])[0].result;
+    }
+
+    switch (ret)
+    {
+      case -5:
+        err = "The Cost Category for one or more Item Sites "
+              + "for the Purchase Order covered by this Voucher "
+              + "is not configured with Purchase Price Variance "
+              + "or P/O Liability Clearing Account Numbers or "
+              + "the Vendor of this Voucher is not configured "
+              + "with an A/P Account Number. Because of this, "
+              + "G/L Transactions cannot be posted for this "
+              +"Voucher.";
+      default:
+        return ret;
+    }
+
+    throw new Error(err);
   }
   
-  XM.Voucher.void = function() {
-  /* checks privilege to void then pass id to function voidapopenvoucher()
+  /** 
+   Voids a voucher that has already been posted to the payables sub-ledger
 
-   @param {Object} Voucher object
-   @returns {Integer}
+   @param {Number} Voucher ID
+   @returns {Number}
   */
+  XM.Voucher.void = function(voucherId) {
+    var data, ret, err;
+
     data = Object.create(XT.Data);
-    if(!data.checkPrivilege('VoidPostedVouchers')) throw new Error("Access Denied");
-    var args = arguments[0];
-    
-    return executeSql('select voidapopenvoucher($1) as result', [args.id])[0].result;
-  }  
-  
+    if(!data.checkPrivilege('VoidPostedVouchers')) err = "Access Denied.";
+    else if(voucherId === undefined) err = "No voucher specified";
+
+    if(!err) {
+      ret = executeSql("select voidapopenvoucher($1) as result;", [voucherId])[0].result;
+    }
+
+    switch (ret)
+    {
+      case -5:
+        err = "The Cost Category for one or more Item Sites "
+              + "for the Purchase Order covered by this Voucher "
+              + "is not configured with Purchase Price Variance "
+              + "or P/O Liability Clearing Account Numbers or "
+              + "the Vendor of this Voucher is not configured "
+              + "with an A/P Account Number. Because of this, "
+              + "G/L Transactions cannot be posted for this "
+              +"Voucher.";
+      default:
+        return ret;
+    }
+
+    throw new Error(err);
+  }
+
 $$ );
