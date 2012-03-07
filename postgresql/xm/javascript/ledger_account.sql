c00333a9cd3c3e4775866fd69a914ee379f58364
Steven Negron <steve@catchproductions.net>
2012-03-07 11:40:28 -0500

    Merge commit 'c26deb332abf93b875c96ce3375321318c648290' into dev
    Conflicts:
    	postgresql/init_script.sql
    	postgresql/xm/javascript/general_ledger.sql
    	postgresql/xm/javascript/invoice.sql

---
+++ postgresql/xm/javascript/ledger_account.sql
@@ -0,0 +1,61 @@
+﻿select xt.install_js('XM','LedgerAccount','xtuple', $$
+  /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
+     See www.xm.ple.com/CPAL for the full text of the software license. */
+  
+  XM.LedgerAccount = {};
+
+  XM.LedgerAccount.isDispatchable = true,
+
+  /** 
+  Insert a duplicate acct record as a child of the 'parent' accnt with the
+  company, profit, and/or sub fields changed.
+
+  @param {String, String, String, Number, String} company, profit, subAccnt, accountId, description
+  @returns {Number}
+  */
+  XM.LedgerAccount.duplicate = function(company, profit, subAccount, accountId, description) {
+    var data = Object.create(XT.Data),
+        sql = "insert into accnt"
+              + "(accnt_number, accnt_descrip,"
+              + "accnt_comments, accnt_type, accnt_extref,"
+              + "accnt_closedpost, accnt_forwardupdate,"
+              + "accnt_subaccnttype_code, accnt_curr_id,"
+              + "accnt_company, accnt_profit, accnt_sub) "
+              + "select accnt_number, (accnt_descrip||' '||$1),"
+              + "accnt_comments, accnt_type, accnt_extref,"
+              + "accnt_closedpost, accnt_forwardupdate,"
+              + "accnt_subaccnttype_code, accnt_curr_id,",
+        ret, err;
+
+    if(!data.checkPrivilege('MaintainChartOfAccounts')) err = "Access Denied.";
+    else if(
+            !(company || profit || subAccount) ||
+            (accountId === undefined)
+           )
+      return -1;
+
+    if(!err) {
+
+      if(company.length)
+        sql += company + ",";
+      else
+        sql += " accnt_company,";
+      if(profit.length)
+        sql += profit + ",";
+      else
+        sql += "accnt_profit,";
+      if(subAccount.length)
+        sql += subAccount + " ";
+      else
+        sql += "accnt_sub";
+
+      sql += " from accnt"
+             + " where (accnt_id = $2);";
+      
+      return executeSql(sql, [description, accountId]);
+    }
+
+    throw new Error(err);
+  }
+
+$$ );
--- postgresql/xm/javascript/payable.sql
+++ postgresql/xm/javascript/payable.sql
@@ -1,10 +1,15 @@
-select xt.install_js('XM','Payable','xtuple', $$
+﻿select xt.install_js('XM','Payable','xtuple', $$
   /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
      See www.xm.ple.com/CPAL for the full text of the software license. */
   
   XM.Payable = {};
 
   XM.Payable.isDispatchable = true,
+
+  XM.Payable.SELECT_ALL_VENDORS = 0,
+  XM.Payable.SELECT_VENDOR = 1,
+  XM.Payable.SELECT_VENDOR_TYPE = 2,
+  XM.Payable.SELECT_VENDOR_TYPE_PATTERN = 3,
 
   /**
    Approve a payable for payment.
@@ -40,5 +45,96 @@ select xt.install_js('XM','Payable','xtuple', $$
     throw new Error(err);
   }
 
-$$ );
+  /**
+   Approve all due open items for payment.
 
+   @param {Number} targetType 
+   @param {Number} target
+   @param {Number} BankAccount
+   @returns {Boolean} 
+  */
+  XM.Payable.approveAllDue = function(targetType, target, bankAccountId) {
+    var sql = "select selectDueItemsForPayment(vend_id, {bankAccnt}) as result from vendinfo where {conditions};",
+        data = Object.create(XT.Data)
+        ret, err;
+
+    if(!data.checkPrivilege("MaintainPayments")) err = "Access denied.";
+    else if(targetType !== XM.Payable.SELECT_ALL_VENDORS && target === undefined) 
+      err = "No Vendor information specified";
+    else if(bankAccountId === undefined) err = "No bank account specified";
+
+    if(!err) {
+      switch (targetType)
+      {
+        case XM.Payable.SELECT_ALL_VENDORS: 
+          sql = sql.replace(/{bankAccnt}/, bankAccountId)
+                   .replace(/{conditions}/, 'true');
+          break;
+         case XM.Payable.SELECT_VENDOR_TYPE:
+          sql = sql.replace(/{bankAccnt}/, bankAccountId)
+                   .replace(/{conditions}/, 'vend_vendtype_id = ' + target);
+          break;
+        case XM.Payable.SELECT_VENDOR_TYPE_PATTERN:
+          sql = sql.replace(/{bankAccnt}/, bankAccountId)
+                   .replace(/{conditions}/, "vend_vendtype_id in "
+                                            + "(select vendtype_id "
+                                            + "from vendtype "
+                                            + "where vendtype_code ~ " + target + ")");
+          break;
+        default:
+          sql = sql.replace(/{bankAccnt}/, bankAccountId)
+                   .replace(/{conditions}/, 'vend_id = ' + target);
+      }
+      return executeSql(sql)[0].result;
+    }
+    
+    throw new Error(err);
+  }
+
+  /**
+   Approve all discounted open items for payment.
+
+   @param {Number} targetType 
+   @param {Number} target
+   @param {Number} BankAccount
+   @returns {Boolean} 
+  */
+  XM.Payable.approveAllDiscounts = function(targetType, target, bankAccountId) {
+    var sql = "select selectDiscountItemsForPayment(vend_id, {bankAccnt}) as result from vendinfo where {conditions};",
+        data = Object.create(XT.Data)
+        ret, err;
+
+    if(!data.checkPrivilege("MaintainPayments")) err = "Access denied.";
+    else if(targetType !== XM.Payable.SELECT_ALL_VENDORS && target === undefined) 
+      err = "No Vendor information specified";
+    else if(bankAccountId === undefined) err = "No bank account specified";
+
+    if(!err) {
+      switch (targetType)
+      {
+        case XM.Payable.SELECT_ALL_VENDORS: 
+          sql = sql.replace(/{bankAccnt}/, bankAccountId)
+                   .replace(/{conditions}/, 'true');
+          break;
+         case XM.Payable.SELECT_VENDOR_TYPE:
+          sql = sql.replace(/{bankAccnt}/, bankAccountId)
+                   .replace(/{conditions}/, 'vend_vendtype_id = ' + target);
+          break;
+        case XM.Payable.SELECT_VENDOR_TYPE_PATTERN:
+          sql = sql.replace(/{bankAccnt}/, bankAccountId)
+                   .replace(/{conditions}/, "vend_vendtype_id in "
+                                            + "(select vendtype_id "
+                                            + "from vendtype "
+                                            + "where vendtype_code ~ " + target + ")");
+          break;
+        default:
+          sql = sql.replace(/{bankAccnt}/, bankAccountId)
+                   .replace(/{conditions}/, 'vend_id = ' + target);
+      }
+      return executeSql(sql)[0].result;
+    }
+    
+    throw new Error(err);
+  }
+
+$$ );
