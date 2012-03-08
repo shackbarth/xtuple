2e61dc1f4660e06e2e62bd8bb4b8a54d703fe8e6
John Rogelstad <john@xtuple.com>
2012-03-08 12:04:28 -0500

    remove function that was moved to datasource.

--- postgresql/init_script.sql
+++ postgresql/init_script.sql
@@ -30,7 +30,6 @@
 \i xt/functions/is_date.sql;
 \i xt/functions/raise_exception.sql;
 \i xt/functions/retrieve_record.sql;
-\i xt/functions/sc_model_gen.sql;
 \i xt/functions/starts_with.sql;
 \i xt/functions/text_gt_date.sql;
 \i xt/functions/text_lt_date.sql;
