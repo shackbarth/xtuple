b0fc3e8a6829c08241bcc7d7ce05adeecf52236c
Steven Negron <steve@catchproductions.net>
2012-03-20 15:54:37 -0400

    Item parent 70 percent complete still need to add tax

--- frameworks/xm/xbos/xtuple/__core__/item/models/item.js
+++ frameworks/xm/xbos/xtuple/__core__/item/models/item.js
@@ -6,19 +6,32 @@
 /*globals XM */
 
 sc_require('xbos/__generated__/_item');
+sc_require('mixins/crm_documents');
 sc_require('mixins/core_documents');
+sc_require('mixins/document');
 
 /**
   @class
 
   @extends XM._Item
+  @extends XM.CrmDocuments
+  @extends XM.CoreDocuments
+  @extends XM.Document
 */
-XM.Item = XM._Item.extend( XM.CoreDocuments,
+XM.Item = XM._Item.extend( XM.Document, XM.CoreDocuments, XM.CrmDocuments,
   /** @scope XM.Item.prototype */ {
 
   // .................................................
   // CALCULATED PROPERTIES
   //
+
+  number: SC.Record.attr(Number, {
+   toType: function(record, key, value) {
+    if(value) return value.toUpperCase();
+   }
+  }),
+  // item_tax need to create item_tax.js sub class and orm file
+  // item_tax_recoverable sets a true flag when a new item is created in the parent | make sure to update the orm to pull this in 
 
   //..................................................
   // METHODS
@@ -27,7 +40,90 @@ XM.Item = XM._Item.extend( XM.CoreDocuments,
   //..................................................
   // OBSERVERS
   //
-
+  _xm_itemTypeDidChange: function() {
+   var status = this.get('status'),
+       itemType = this.get('itemType'),
+       isPickList = false,
+       isSold = false,
+       weight = false,
+       config = false,
+       shipUOM = false,
+       capUOM = false,
+       planType = false,
+       purchased = false,
+       freight = false;
+   if(status & SC.Record.READY) {
+    switch(itemType) {
+    case "P" :
+    case "M" :
+     this.set('isPicklist', true),
+     this.set('isSold', true),
+     this.set('weight', true),
+     this.set('config', true),
+     this.set('shipUOM', true),
+     this.set('capUOM', true),
+     this.set('planType', true),
+     this.set('purchased', true),
+     this.set('freight', true);
+    break;
+    case "F" : 
+     this.set('planType', true);
+    break;
+    case "B" :
+     this.set('capUOM', true),
+     this.set('planType', true),
+     this.set('purchased', true),
+     this.set('freight', true);
+    break;
+    case "C" :
+    case "Y" :
+     this.set('isPicklist', true),
+     this.set('isSold', true),
+     this.set('weight', true),
+     this.set('capUOM', true),
+     this.set('shipUOM', true),
+     this.set('planType', true),
+     this.set('freight', true);
+    break;
+    case "R" :
+     this.set('isSold', true),
+     this.set('weight', true),
+     this.set('capUOM', true),
+     this.set('shipUOM', true),
+     this.set('freight', true),
+     this.set('config', true);
+    break;
+    case "T" :
+     this.set('isPicklist', true),
+     this.set('weight', true),
+     this.set('capUOM', true),
+     this.set('shipUOM', true),
+     this.set('freight', true),
+     this.set('purchased', true),
+     this.set('isSold', true);
+    break;
+    case "O" :
+     this.set('capUOM', true),
+     this.set('planType', true),
+     this.set('purchased', true), 
+     this.set('freight', true);
+    break;
+    case "A" :
+     this.set('isSold', true),
+     this.set('planType', true),
+     this.set('freight', true);
+    break;
+    case "K" :
+     this.set('isSold', true),
+     this.set('weight', true);
+    break;
+    }
+  }
+  }.observes('itemType'),
+  validate: function() {
+   var errors = arguments.callee.base.apply(this, arguments);
+  return errors;
+  }.observes('inventoryUnit', 'productCategory', 'classCode', 'priceUnit'),
 });
 
 /**
