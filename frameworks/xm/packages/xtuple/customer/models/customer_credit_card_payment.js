37b4dd2f63d942f693bbe5acd0b9d3a3976ccdca
John Rogelstad <john@xtuple.com>
2012-03-30 11:20:13 -0400

    add customer credit card model

---
+++ frameworks/xm/packages/xtuple/customer/models/customer_credit_card_payment.js
@@ -0,0 +1,33 @@
+// ==========================================================================
+// Project:   xTuple Postbooks - Business Management System Framework        
+// Copyright: ©2012 OpenMFG LLC, d/b/a xTuple                             
+// ==========================================================================
+
+/*globals XM */
+
+// PLACE ME IN ../client/frameworks/xm/packages/xtuple/customer/models
+
+sc_require('mixins/_customer_credit_card_payment');
+
+/**
+  @class
+
+  @extends XM.Record
+*/
+XM.CustomerCreditCardPayment = XT.Record.extend(XM._CustomerCreditCardPayment,
+  /** @scope XM.CustomerCreditCardPayment.prototype */ {
+
+  // .................................................
+  // CALCULATED PROPERTIES
+  //
+
+  //..................................................
+  // METHODS
+  //
+
+  //..................................................
+  // OBSERVERS
+  //
+
+});
+
