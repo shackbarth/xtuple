1094fc4bec3f651622886834b02f7b9d77a03e2e
John Rogelstad <john@xtuple.com>
2012-04-02 18:24:39 -0400

    add function to calculate due date based on terms.

--- frameworks/xm/packages/xtuple/__core__/miscellaneous/models/terms.js
+++ frameworks/xm/packages/xtuple/__core__/miscellaneous/models/terms.js
@@ -24,6 +24,10 @@ XM.Terms = XM.Document.extend(XM._Terms,
   //..................................................
   // METHODS
   //
+  
+  calculateDueDate: function(date) {
+    return XM.Terms.calculateDueDate(this, date);
+  }
 
   //..................................................
   // OBSERVERS
@@ -31,6 +35,39 @@ XM.Terms = XM.Document.extend(XM._Terms,
 
 });
 
+/**
+  Calculate a due date based on a given document date.
+  
+  @param {XM.Terms} terms
+  @param {SC.DateTime} document date.
+  @returns SC.DateTime
+*/
+XM.Terms.calculateDueDate = function(terms, date) {
+  if (!SC.kindOf(terms, XM.Terms)) return date;
+  var termsType = terms.get('termsType'),
+      dueDays = terms.get('dueDays'),
+      cutOffDay = terms.get('cutOffDay'),
+      result = date;
+      
+  // handle days
+  if (termsType === 'D') {
+    result = date.advance({ day: dueDays });
+    
+  // handle proximo (day of month)
+  } else if (termsType === 'P') {
+    result = SC.DateTime.create({ 
+      month: date.get('month'), 
+      day: dueDays, 
+      year: date.get('year')
+    });
+    
+    // if day has already cut off, then advance to next month
+    if (date.get('day') > cutOffDay) {
+      result = result.advance({ month: 1 });
+    }
+  }
+  return result
+}
 
 XM.Terms.mixin( /** @scope XM.Terms */ {
 
