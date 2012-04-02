337ace9be590c89f35ff7bb2486c0f17d1c5cd9e
Mikhail Wall <mwall@xtuple.com>
2012-04-02 09:26:16 -0400

    first pass on bankAccount in progress

--- frameworks/xm/packages/xtuple/__core__/bank_account/models/bank_account.js
+++ frameworks/xm/packages/xtuple/__core__/bank_account/models/bank_account.js
@@ -10,11 +10,14 @@ sc_require('mixins/_bank_account');
 /**
   @class
 
-  @extends XT.Record
+  @extends XM.Document
 */
-XM.BankAccount = XT.Record.extend(XM._BankAccount,
-  /** @scope XM.BankAccount.prototype */ {
+XM.BankAccount = XM.Document.extend(XM._BankAccount,
+  /** @scope XM.BankAccount.prototype */ { 
 
+  // see document mixin for object behavior(s)
+  documentKey: 'name',
+  
   // .................................................
   // CALCULATED PROPERTIES
   //
