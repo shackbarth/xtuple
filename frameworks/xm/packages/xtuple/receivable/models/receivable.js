2af6d7c7145be4539ec1729a48cfcd7142c898bf
John Rogelstad <john@xtuple.com>
2012-04-03 18:40:53 -0400

    Refactor most of receivable into subledger so payable can share the code.

--- frameworks/xm/packages/xtuple/receivable/models/receivable.js
+++ frameworks/xm/packages/xtuple/receivable/models/receivable.js
@@ -10,149 +10,22 @@ sc_require('mixins/_receivable');
 /**
   @class
 
-  @extends XM.TaxableDocument
+  @extends XM.SubLedger
 */
-XM.Receivable = XM.TaxableDocument.extend(XM._Receivable,
+XM.Receivable = XM.SubLedger.extend(XM._Receivable,
   /** @scope XM.Receivable.prototype */ {
-
-  numberPolicy: XT.AUTO_OVERRIDE_NUMBER,
-  
-  /**
-    Aging control for paid and balance values.
-    
-    @type SC.DateTime
-    @default currentDate
-  */
-  asOf: null,
-  
-  /** @private */
-  applicationsLength: 0,
-  
-  /** @private */
-  applicationsLengthBinding: SC.Binding.from('*applications.length').oneWay().noDelay(),
 
   // .................................................
   // CALCULATED PROPERTIES
   //
 
-  paid: function() {
-    var asOf = this.get('asOf'),
-        applications = this.get('applications'),
-        paid = 0;
-    for (var i = 0; i < applications.get('length'); i++) {
-      var application = applications.objectAt(i); 
-          postDate = application.get('postDate');
-      if (SC.DateTime.compare(asOf, postDate) >= 0) {
-        paid = paid + application.get('paid');
-      }
-    }
-    return SC.Math.round(paid, XT.MONEY_SCALE);
-  }.property('applicationsLength', 'asOf').cacheable(),
-
-  /**
-    @type Number
-  */
-  balance: function() {
-    var asOf = this.get('asOf'),
-        documentDate = this.get('documentDate'),
-        amount = this.get('amount') || 0,
-        paid = this.get('paid') || 0, ret = 0;
-    if (SC.DateTime.compare(asOf, documentDate) >= 0) {
-      ret = SC.Math.round(amount - paid, XT.MONEY_SCALE);
-    }
-    return ret;
-  }.property('amount', 'paid').cacheable(),
-
   //..................................................
   // METHODS
   //
-  
-  /**
-    For new records this dispatches a function here that handles the two
-    step process of creating the record, then posting it. Otherwise it
-    commits normally.
-  */
-  commitRecord: function() {
-    var status = this.get('status');
-    
-    // if new dispatch post function
-    if (status === SC.Record.READY_NEW) {
-      var receivable = this.get('changeSet'),
-          store = this.get('store'),
-          storeKey = this.get('storeKey'),
-          rev = SC.Store.generateStoreKey(),
-          that = this;
-      
-      // update status of this record
-      store.writeStatus(storeKey, SC.Record.BUSY_CREATING);
-      store.dataHashDidChange(storeKey, rev, true);
-      this.notifyPropertyChange('status');
-        
-      // update status of children
-      store._propagateToChildren(storeKey, function(storeKey) {
-        var rev = SC.Store.generateStoreKey(),
-            rec = store.materializeRecord(storeKey);
-        store.writeStatus(storeKey, SC.Record.BUSY_CREATING);
-        store.dataHashDidChange(storeKey, rev, true);
-        rec.notifyPropertyChange('status');
-      });
-      
-      // callback - notify store of results
-      callback = function(err, result) {
-        if (err) store.dataSourceDidError(storeKey, err);
-        else store.dataSourceDidComplete(storeKey);
-      }
-
-      // define call
-      dispatch = XT.Dispatch.create({
-        className: 'XM.Receivable',
-        functionName: 'post',
-        parameters: receivable,
-        target: that,
-        action: callback
-      });
-      
-      // do it
-      store.dispatch(dispatch);
-      return this;
-    }
-    return arguments.callee.base.apply(this, arguments);
-  },
-  
-  /**
-    Only new records may be destroyed.
-  */
-  destroy: function() {
-    var status = this.get('status');
-    if (status === SC.Record.READY_NEW) {
-      return arguments.callee.base.apply(this, arguments);
-    }
-    return false;
-  },
-  
-  init: function() {
-    arguments.callee.base.apply(this, arguments);
-    this.currencyRate.set('isEditable', false);
-    this.closeDate.set('isEditable', false);
-    this.createdBy.set('isEditable', false);
-    if (!this.get('asOf')) this.set('asOf', SC.DateTime.create());
-  },
 
   //..................................................
   // OBSERVERS
   //
-  
-  validate: function() {
-    var errors = arguments.callee.base.apply(this, arguments), 
-        val, err;
-
-    // validate Lines
-    isErr = this.get('miscTax') > this.get('amount');
-    err = XT.errors.findProperty('code', 'xt1005');
-    this.updateErrors(err, isErr);
-
-    return errors;
-  }.observes('amount', 'miscTax'),
   
   amountDidChange: function() {
     if (this.isNotDirty()) return;
@@ -161,16 +34,6 @@ XM.Receivable = XM.TaxableDocument.extend(XM._Receivable,
         commissionDue = amount * commissionPct / 100;
     this.setIfChanged('commissionDue', commissionDue);
   }.observes('customer', 'amount'),
-  
-  termsDidChange: function() {
-    var documentDate = this.get('documentDate'),
-        dueDate = this.get('dueDate'),
-        terms = this.get('terms');
-    if (documentDate && terms && !dueDate) {
-      dueDate = terms.calculateDueDate(documentDate);
-      this.set('dueDate', dueDate);
-    }
-  }.observes('terms', 'documentDate'),
   
   customerDidChange: function() {
     if (this.isNotDirty()) return;
@@ -183,60 +46,10 @@ XM.Receivable = XM.TaxableDocument.extend(XM._Receivable,
   }.observes('customer'),
   
   statusDidChange: function() {
+    arguments.callee.base.apply(this, arguments);
     if (this.get('status') == SC.Record.READY_CLEAN) {
       this.customer.set('isEditable', false);
-      this.documentDate.set('isEditable', false);
-      this.documentType.set('isEditable', false);
-      this.number.set('isEditable', false);
-      this.orderNumber.set('isEditable', false);
-      this.terms.set('isEditable', false);
-      this.customer.set('isEditable', false);
-      this.currency.set('isEditable', false);
     }
   }.observes('status')
   
 });
-
-XM.Receivable.mixin( /** @scope XM.Receivable */ {
-
-/**
-  Credit Memo document type.
-  
-  @static
-  @constant
-  @type String
-  @default C
-*/
-  CREDIT_MEMO: 'C',
-
-/**
-  Debit Memo document type.
-  
-  @static
-  @constant
-  @type String
-  @default D
-*/
-  DEBIT_MEMO: 'D',
-
-/**
-  Invoice document type.
-  
-  @static
-  @constant
-  @type String
-  @default I
-*/
-  INVOICE: 'I',
-
-/**
-  Customer Deposit document type.
-  @static
-  @constant
-  @type String
-  @default R
-*/
-  CUSTOMER_DEPOSIT: 'R'
-
-});
-
