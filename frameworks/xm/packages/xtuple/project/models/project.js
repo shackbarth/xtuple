e07793857749a5db5f06e3af11692149d17c96a7
John Rogelstad <john@xtuple.com>
2012-04-05 16:17:49 -0400

    merge snegron

--- frameworks/xm/packages/xtuple/project/models/project.js
+++ frameworks/xm/packages/xtuple/project/models/project.js
@@ -1,6 +1,6 @@
 // ==========================================================================
 // Project:   xTuple Postbooks - Business Management System Framework        
-// Copyright: ©2012 OpenMFG LLC, d/b/a xTuple                             
+// Copyright: ¬©2012 OpenMFG LLC, d/b/a xTuple                             
 // ==========================================================================
 
 /*globals XM */
@@ -14,132 +14,157 @@ sc_require('mixins/_project');
   @extends XM.Document
   @extends XM.Documents
 */
-XM.Project = XM.Document.extend(XM._Project, XM.Documents,
-  /** @scope XM.Project.prototype */ {
+XM.Project = XM.Document.extend(XM._Project, XM.Documents, 
+/** @scope XM.Project.prototype */ {	
 
-  // .................................................
-  // CALCULATED PROPERTIES
-  //
+/** @private */
+budgetedTotalHours: 0,
 
-  number: SC.Record.attr(Number, {
-   toType: function(record, key, value) {
-    if(value) return value.toUpperCase();
-   }
-  }),
+/** @private */ 
+actualTotalHours: 0,
 
-  /** 
-  @field
-  @type Number
-  */
-  budgetedHours: function() {
-  var _budgetedHours = this.get('budgetedHours'),
-      total = 0;
+/** @private */
+budgetedTotalExpenses: 0,
 
-  for(var i = 0; i < _budgetedHours.get('length'); i++) {
-    total = total + _budgetedHours.objectAt(i).get('budgetedHours');
+/** @private */
+actualTotalExpenses: 0,
+
+// .................................................
+// CALCULATED PROPERTIES
+//
+
+number: SC.Record.attr(Number, {
+  toType: function(record, key, value) {
+  if(value) return value.toUpperCase();
   }
+}),
 
-  return total;
-  }.property('_budgetedHoursLength').cacheable(),
+balanceHoursTotal: function() {
+  var hours = this.get('budgetedTotalHours') - this.get('actualTotalHours');
+  return SC.Math.round(hours, XT.QTY_SCALE);
+}.property('budgetedTotalHours','actualTotalHours'),
 
-  /**
-  @field
-  @type Number
-  */
-  actualHours: function() {
-  var _actualHours = this.get('actualHours'),
-      total = 0;
+balanceActualExpensesTotal: function() {
+  var expenses = this.get('budgetedTotalExpenses') - this.get('actualTotalExpenses');
+  return SC.Math.round(expenses, XT.MONEY_SCALE);
+}.property('budgetedTotalExpenses','actualTotalExpenses'),
 
-  for(var i = 0; i < _actualHours.get('length'); i++) {
-    total = total + _actualHours.objectAt(i).get('actualHours');
+copy: function() { return XM.Project.copy(this,number,offset) },
+
+//..................................................
+// METHODS
+//
+
+updateBudgetedHours: function() {
+  var tasks = this.get('tasks'),
+  budgetedHours = 0;
+  for (var i = 0; i < tasks.get('length'); i++) {
+  var task = tasks.objectAt(i),
+      status = task.get('status'),
+      hours = status & SC.Record.DESTROYED ? 0 : task.get('budgetedHours');
+      budgetedHours = budgetedHours + hours;
+	}
+  this.setIfChanged('budgetedTotalHours', SC.Math.round(budgetedHours, XT.QTY_SCALE));
+},	
+
+updateActualHours: function() {
+  var tasks = this.get('tasks'),
+  actualHours = 0;
+  for (var i = 0; i < tasks.get('length'); i++) {
+  var task = tasks.objectAt(i),
+      status = task.get('status'),
+      hours = status & SC.Record.DESTROYED ? 0 : task.get('actualHours');
+      actualHours = actualHours + hours;
   }
+  this.setIfChanged('actualTotalHours', SC.Math.round(actualHours, XT.QTY_SCALE));
+},
 
-  return total;
-  }.property('_actualHoursLength').cacheable(),
-
-  /**
-  @field
-  @type Number
-  */
-  budgetedExpenses: function() {
-  var _budgetedExpenses = this.get('budgetedExpenses'),
-      total = 0;
-
-  for(var i = 0; i < _budgetedExpenses.get('length'); i++) {
-    total = total + _budgetedExpenses.objectAt(i).get('budgetedExpenses');
+updateBudgetedExpenses: function() {
+  var tasks = this.get('tasks'),
+  budgetedExpenses = 0;
+  for (var i = 0; i < tasks.get('length'); i++) {
+  var task = tasks.objectAt(i),
+      status = task.get('status'),
+      expenses = status & SC.Record.DESTROYED ? 0 : task.get('budgetedExpenses');
+      budgetedExpenses = budgetedExpenses + expenses;
   }
+  this.setIfChanged('budgetedTotalExpenses', SC.Math.round(budgetedExpenses, XT.MONEY_SCALE));
+},	
 
-  return total;
-  }.property('_budgetedExpensesLength').cacheable(),
-
-  /**
-  @field
-  @type Number
-  */
-  actualExpenses: function() {
-  var _actualExpenses = this.get('actualExpenses'),
-      total = 0;
-
-  for(var i = 0; i < _actualExpenses.get('length'); i++) {
-    total = total + _actualExpenses.objectAt(i).get('actualExpenses');
+updateActualExpenses: function() {
+  var tasks = this.get('tasks'),
+  actualExpenses = 0;
+  for (var i = 0; i < tasks.get('length'); i++) {
+  var task = tasks.objectAt(i),
+      status = task.get('status'),
+      expenses = status & SC.Record.DESTROYED ? 0 : task.get('actualExpenses');
+      actualExpenses = actualExpenses + expenses;
   }
+  this.setIfChanged('actualTotalExpenses', SC.Math.round(actualExpenses, XT.MONEY_SCALE));
+},	
 
-  return total;
-  }.property('_actualExpensesLength').cacheable(),
+//..................................................
+// OBSERVERS
+//
 
-  /**
-  @field
-  @type Number
-  */
-  balanceHours: function() {
-  var budgetedHours = this.get('budgetedHours'),
-      actualHours = this.get('actualHours');
+statusDidChange: function(){
+  var status = this.get('status');
+  if (status === SC.Record.READY_CLEAN) {
+    this.updateActualHours(),
+	  this.updateBudgetedHours();
+    this.updateActualExpenses(),
+    this.updateBudgetedExpenses();
+  }
+}.observes('status'),
 
-  return budgetedHours - actualHours;
-  }.property('budgetedHours', 'actualHours').cacheable(),
-
-  /**
-  @field
-  @type Number
-  */
-  balanceExpenses: function() {
-  var budgetedExpenses = this.get('budgetedExpenses'),
-      actualExpenses = this.get('actualExpenses');
-
-  return budgetedExpenses - actualExpenses;
-  }.property('budgetedExpenses', 'actualExpenses').cacheable(),
-
-  //..................................................
-  // METHODS
-  //
-
-  //..................................................
-  // OBSERVERS
-  //
-
-  _xm_projectStatusDidChange: function() {
-    var status = this.get('status'),
-        projectStatus = this.get('projectStatus');
-
-    if(status & SC.Record.READY) {
-      if(projectStatus === XM.Project.COMPLETED) this.set('completeDate', SC.DateTime.create());    
+projectStatusDidChange: function() {
+  var status = this.get('status'),
+      projectStatus = this.get('projectStatus');
+  if (this.isDirty()) {
+    if (projectStatus === XM.Project.IN_PROCESS) {
+      this.set('assignDate', SC.DateTime.create());
+    } else if (projectStatus === XM.Project.COMPLETED) {
+	    this.set('completeDate', SC.DateTime.create());
     }
-  }.observes('projectStatus'),
+  }
+}.observes('projectStatus'),
 
-   /* @private */
-  _projectsLength: 0,
-  
-  /* @private */
-  _projectsLengthBinding: '*projects.length',
-  
-  /* @private */
-  _projectsDidChange: function() {
-    var documents = this.get('documents'),
-        projects = this.get('projects');
+projectNumberDidChange: function() {
+  if (this.get('status') === SC.Record.READY_DIRTY) {
+    this.number.set('isEditable', false);
+  }
+},//.observes('status')
 
-    documents.addEach(projects);    
-  }.observes('projectsLength'),
 });
+
+/**
+A utility function to copy an project.
+
+@param {XM.Project} project
+@return {XM.Project} copy of the project
+*/
+XM.Project.copy = function(project,number,offset) {
+  if (!SC.kindOf(project, XM.Project)) return NO;
+
+  var store = project.get('store'),
+  hash = project.get('attributes');
+
+  for (var i = 0; i < hash.tasks.length; i++) { 
+    if (offset > 0) {
+      hash.tasks[i].dueDate = hash.tasks[i].dueDate.advance({day: + offset});
+    }
+    hash.tasks[i].projectTaskStatus = 'P';
+    delete hash.tasks[i].assignDate;
+  } 
+  hash.projectStatus = 'P';
+  hash.number = number;
+  if (offset > 0) {
+    hash.dueDate = hash.dueDate.advance({day: + offset});
+  }
+  delete hash.startDate;
+  delete hash.guid;
+  return store.createRecord(XM.Project, hash).normalize();
+}
 
 XM.Project.mixin( /** @scope XM.Project */ {
 
