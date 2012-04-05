a4e72da71c7c2a1809f596e25ef56d403c278b1a
Steven Negron <steve@catchproductions.net>
2012-04-05 18:05:26 -0400

    item conversion sub class

--- frameworks/xm/packages/xtuple/__core__/item/models/item_conversion.js
+++ frameworks/xm/packages/xtuple/__core__/item/models/item_conversion.js
@@ -13,31 +13,44 @@ sc_require('mixins/_item_conversion');
   @extends XT.Record
 */
 XM.ItemConversion = XT.Record.extend(XM._ItemConversion,
-  /** @scope XM.ItemConversion.prototype */ {
+/** @scope XM.ItemConversion.prototype */ {
 
-  // .................................................
-  // CALCULATED PROPERTIES
-  //
+// .................................................
+// CALCULATED PROPERTIES
+//
 
-	/**
-		Unit conversion.
-	*/	
-	unitAvailableTypes: function(){
-    var fromUnit = this.get('fromUnit'),
-        toUnit = this.get('toUnit'),
-				unitType = this.get('unitType');
-		if(unitType === 2 || 3 || 4){
-			 this.set('toUnit') = this.get('toUnit');
+
+
+//Observe on to unit and update property think about it like project 
+//bool true or false 1 True and 2,3,4 False
+
+unitAvailableTypes: function(add,remove) {
+  var status = this.get('status'),
+	    multiple = this.getPath('UnitType.multiple'),
+			unitType = this.getPath('ItemConversionTypeAssignment.unitType');
+	if (status == SC.Record.READY_CLEAN) {
+	  if (multiple === true){
+		   this.set('unitType', this.getPath('ItemConversionTypeAssignment.unitType'));
+		} else if (multiple === false) {
+		   this.set('unitType', this.getPath('ItemConversionTypeAssignment.unitType'));
 		}
-	},
-	
-  //..................................................
-  // METHODS
-  //
+	}
+},
 
-  //..................................................
-  // OBSERVERS
-  //
+//..................................................
+// METHODS
+//
+
+//..................................................
+// OBSERVERS
+//
+fromUnitDidChange: function() {
+if (this.get('status') === SC.Record.READY_DIRTY) {
+	this.fromUnit.set('isEditable', false);
+	this.toUnit.set('isEditable', false);
+}
+},//.observes('status')
+
 
 });
 
