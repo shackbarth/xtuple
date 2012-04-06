2cf955087c723097b69d9f2b6aebabc3588dca13
M. Night Shyamalan <mike@cxmobile.local>
2012-04-06 15:19:43 -0400

    first pass on layout income statement

--- frameworks/xm/packages/xtuple/ledger/models/layout_income_statement.js
+++ frameworks/xm/packages/xtuple/ledger/models/layout_income_statement.js
@@ -25,39 +25,69 @@ XM.LayoutIncomeStatement = XM.Document.extend(XM._LayoutIncomeStatement,
   // METHODS
   //
 
-  setOptions: function() {
-    
+  init: function() {
+    arguments.callee.base.apply(this, arguments);
+
+    /**
+      sync option labels' isEditable properties with 
+      associated boolean flag's default state
+    */
+    this.budgetLabel.set('isEditable', false);
+    this.isAlternateTotal.set('isEditable', false);
+    this.alternateTotalLabel.set('isEditable', false);
+  },
+
+  /**
+    when an income statement option flag changes state, 
+    keep all associated options properties in sync
+  */
+  setOptions: function(key) {
+    switch(key) {
+      case 'isShowTotal':
+        var isShowTotal = this.get('isShowTotal');
+
+        if(isShowTotal) {
+          this.isAlternateTotal.set('isEditable', true);
+        } else {
+          this.set('isAlternateTotal', false);
+          this.isAlternateTotal.set('isEditable', false);
+        }
+        break;
+      case 'isAlternateBudget':
+        var isAlternateBudget = this.get('isAlternateBudget');
+
+        if(isAlternateBudget) {
+          this.budgetLabel.set('isEditable', true);
+        } else {
+          this.budgetLabel.set('isEditable', false);
+        }
+        break;
+
+      /**
+        case isAlternateTotal:
+      */
+      default:
+        var isAlternateTotal = this.get('isAlternateTotal');
+
+        if(isAlternateTotal) {
+          this.alternateTotalLabel.set('isEditable', true);
+        } else {
+          this.alternateTotalLabel.set('isEditable', false);
+        }
+    }
   },
 
   //..................................................
   // OBSERVERS
   //
 
+  /**
+    call setOptions() when an associated boolean flag changes
+  */
   optionsDidChange: function() {
     var prop = arguments[1];
 
-    switch(prop) {
-      case 'isShowTotal':
-        if(isShowTotal) {
-          this.isAlternateTotal.set('isEditable', true);
-        } else {
-          this.isAlternateTotal.set('isEditable', false);          
-        }
-        break;
-      case 'isAlternateBudget':
-        if(isAlternateBudget) {
-          
-        } else {
-          
-        }
-        break;
-      default:
-        if(isAlternateTotal) {
-          
-        } else {
-          
-        }
-    }
+    this.setOptions(prop);
   }.observes('isShowTotal', 'isAlternateBudget', 'isAlternateTotal'),
 
 });
