7e1787a436c91326fa52dc3c6b1992ceefec53cf
John Rogelstad <john@xtuple.com>
2012-04-09 07:23:08 -0400

    First pass start on FRE models.

---
+++ frameworks/xm/packages/xtuple/ledger/models/balance_sheet.js
@@ -0,0 +1,23 @@
+// ==========================================================================
+// Project:   xTuple Postbooks - Business Management System Framework        
+// Copyright: ©2012 OpenMFG LLC, d/b/a xTuple                             
+// ==========================================================================
+
+/*globals XM */
+
+/**
+  @class
+
+  @extends XM.FinancialStatement
+*/
+XM.BalanceSheet = XM.FinancialStatement.extend(
+  /** @scope XM.BalanceSheet.prototype */ {
+  
+  className: 'XM.BalanceSheet'
+  
+});
+
+XM.BalanceSheet.retrieve = function(columnLayout, period) {
+  return false; // TODO: implement this
+}
+
