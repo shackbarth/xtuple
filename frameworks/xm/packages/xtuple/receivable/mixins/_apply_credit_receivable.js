19f8b2fda20c9a0232245b6806374a02f5635485
John Rogelstad <john@xtuple.com>
2012-04-17 14:47:26 -0400

    start on credit application.

---
+++ frameworks/xm/packages/xtuple/receivable/mixins/_apply_credit_receivable.js
@@ -0,0 +1,121 @@
+// ==========================================================================
+// Project:   xTuple Postbooks - Business Management System Framework        
+// Copyright: ©2012 OpenMFG LLC, d/b/a xTuple                             
+// ==========================================================================
+
+/*globals XM */
+
+/**
+  @scope XM.ApplyCreditReceivable
+  @mixin
+
+  This code is automatically generated and will be over-written. Do not edit directly.
+*/
+XM._ApplyCreditReceivable = {
+  /** @scope XM.ApplyCreditReceivable.prototype */
+  
+  className: 'XM.ApplyCreditReceivable',
+
+  nestedRecordNamespace: XM,
+
+  // .................................................
+  // PRIVILEGES
+  //
+
+  privileges: {
+    "all": {
+      "create": false,
+      "read": "ApplyARMemos",
+      "update": false,
+      "delete": false
+    }
+  },
+
+  //..................................................
+  // ATTRIBUTES
+  //
+  
+  /**
+    @type Number
+  */
+  guid: SC.Record.attr(Number),
+
+  /**
+    @type XM.CustomerInfo
+  */
+  customer: SC.Record.toOne('XM.CustomerInfo', {
+    label: '_customer'.loc()
+  }),
+
+  /**
+    @type Date
+  */
+  documentDate: SC.Record.attr(SC.DateTime, {
+    format: '%Y-%m-%d',
+    useIsoDate: false,
+    label: '_documentDate'.loc()
+  }),
+
+  /**
+    @type Date
+  */
+  dueDate: SC.Record.attr(SC.DateTime, {
+    format: '%Y-%m-%d',
+    useIsoDate: false,
+    label: '_dueDate'.loc()
+  }),
+
+  /**
+    @type String
+  */
+  documentType: SC.Record.attr(String, {
+    label: '_documentType'.loc()
+  }),
+
+  /**
+    @type String
+  */
+  number: SC.Record.attr(String, {
+    label: '_number'.loc()
+  }),
+
+  /**
+    @type Money
+  */
+  amount: SC.Record.attr(Money, {
+    label: '_amount'.loc()
+  }),
+
+  /**
+    @type XM.Currency
+  */
+  currency: SC.Record.toOne('XM.Currency', {
+    label: '_currency'.loc()
+  }),
+
+  /**
+    @type Number
+  */
+  currencyRate: SC.Record.attr(Number, {
+    label: '_currencyRate'.loc()
+  }),
+
+  /**
+    @type XM.ReceivableApplication
+  */
+  applications: SC.Record.toMany('XM.ReceivableApplication', {
+    isNested: true,
+    inverse: 'receivable',
+    label: '_applications'.loc()
+  }),
+
+  /**
+    @type XM.ReceivablePendingApplication
+  */
+  pendingApplications: SC.Record.toMany('XM.ReceivablePendingApplication', {
+    isNested: true,
+    inverse: 'receivable',
+    label: '_pendingApplications'.loc()
+  })
+
+};
