0b8c84b22e5120ea6e13f3a722c4dbb315715763
Mikhail Wall <mwall@xtuple.com>
2012-05-10 14:34:24 -0400

    working on 'notes' view

--- apps/postbooks/views/notes_tile_view.js
+++ apps/postbooks/views/notes_tile_view.js
@@ -1,100 +1,63 @@
 Postbooks.CreateNotesTileView = function(controller) {
 
-  /**
-  var view = Postbooks.TileView.create({ title: "_notes".loc() }),
-      layers = view.get('layers'),
-      y = 42,
-      K = Postbooks,
-      left = 12, right = 12,
-      widget = null,
-      key;
-  */
- 
-  var key = 'notes';
+  // global for testing textSurface.value binding
+  NOTES_VALUE = controller;
 
-  /**
-  widget = SC.TextFieldWidget.create({
-    layout: { top: y, left: left, height: 70, right: right },
-    borderColor: 'white',
-    isSingleLine: false,
-    valueBinding: SC.Binding.from(key, controller)
-  });
-  layers.pushObject(widget);
-  */
+  var key = 'notes',
+      K = Postbooks;
+  
+  var layoutSurface = SC.LayoutSurface.create();
+  layoutSurface.set('frame', SC.MakeRect(0, 42, 320, 320));
+  layoutSurface.set('backgroundColor', "white");
 
-  var draggableSurface = SC.CompositeSurface.create({
-    mouseDown: function(evt) {
-      // console.log('draggableSurface#mouseDown');
-      this._clientX = evt.clientX;
-      this._clientY = evt.clientY;
-      return true;
-    },
+  var topbar = SC.View.create({
+    layout: { top: 3, left: 0, right: 0, height: 32 },
 
-    mouseDragged: function(evt) {
-      // console.log('draggableSurface#mouseDragged');
-      SC.AnimationTransaction.begin({ duration: 0 });
-      var frame = this.get('frame');
-      frame.x = frame.x + evt.clientX - this._clientX;
-      frame.y = frame.y + evt.clientY - this._clientY;
-      this._clientX = evt.clientX;
-      this._clientY = evt.clientY;
-      SC.AnimationTransaction.end();
-      return true;
-    },
+    willRenderLayers: function(context) { 
+      var title = "_notes".loc();
+           
+      // title bar
+      context.fillStyle = "#fdf6e3";
+      context.fillRect(0, 0, context.width, 25);
 
-    mouseUp: function(evt) {
-      // console.log('draggableSurface#mouseUp');
-      SC.AnimationTransaction.begin({ duration: 0 });
-      var frame = this.get('frame');
-      frame.x = frame.x + evt.clientX - this._clientX;
-      frame.y = frame.y + evt.clientY - this._clientY;
-      delete this._clientX;
-      delete this._clientY;
-      SC.AnimationTransaction.end();
-      return true;
+      // image frame
+      context.fillStyle = base00;
+      context.fillRect(20, 4, 24, 24);
+
+      // title text
+      context.font = "12pt "+K.TYPEFACE;
+      context.fillStyle = 'black';
+      context.textAlign = 'left';
+      context.textBaseline = 'middle';
+
+      context.fillText(title, 72, 16 );
     }
   });
-  draggableSurface.set('frame', SC.MakeRect(0, 42, 320, 320));
-  draggableSurface.set('backgroundColor', "#fdf6e3");
-
-  for (name in controller) {
-    if (controller.hasOwnProperty(name)) {
-      console.log('controller property: %@'.fmt(name));
-    }
-  }
-
-  var content = controller.content;
-  for (name in content) {
-    if (content.hasOwnProperty(name)) {
-      console.log('content property: %@'.fmt(name));
-    }
-  }
-
-  var notes = controller.get('notes');
-  console.log('controller notes: %@'.fmt(notes));
 
   var view = SC.TextSurface.create({
-    // value: 'Hello world',
+    _sc_borderColor: "transparent",
+    _sc_font: " 11pt "+K.TYPEFACE,
+
+    // testing value binding
     valueDidChange: function() {
       var value = this.get('value');
       console.log('value: %@'.fmt(value));
     }.observes('value')
   });
-  SC.Binding.from(key, controller).to('value', view).sync().connect().flushPendingChanges();
 
+  //create binding from record object to value property of textSurface
+  notesBinding = SC.Binding.from(key, controller).to('value', view).sync().connect();
+  bindingsRef = view.get('bindings');
+  bindingsRef.pushObject(notesBinding);
+
+  // testing value binding
   controller.addObserver('notes', function() {
     console.log('notes value: %@'.fmt(this.get('notes')));
   });
 
-  NOTES_VALUE = controller;
+  view.set('frame', SC.MakeRect(0, 38, 310, 272));
 
-  var bindingRef = view.bindings;
+  layoutSurface.get('subsurfaces').pushObjects([topbar, view]);
 
-  console.log('bindings length: %@'.fmt(bindingRef.length));
-
-  view.set('frame', SC.MakeRect(0, 34, 310, 276));
-
-  draggableSurface.get('subsurfaces').pushObject(view);
-
-  return draggableSurface;
+  return layoutSurface;
 };
