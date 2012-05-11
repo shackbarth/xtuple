e8889077bb25e3ce3d8e2e159ba4402781a9635b
Dave Cramer <davecramer@gmail.com>
2012-05-10 21:31:55 -0400

    changed to ASCII

--- postgresql/drop_xm_views.sql
+++ postgresql/drop_xm_views.sql
@@ -1,7 +1,7 @@
-﻿DO $$
-  /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
+DO $$
+ /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple.
      See www.xm.ple.com/CPAL for the full text of the software license. */
-  
+
   var sql = "select dropifexists('VIEW', viewname, schemaname, true) "
             + "from pg_views "
             + "where schemaname = 'xm';",
