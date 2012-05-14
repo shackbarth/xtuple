6430beb2dfa3a8136bdbcd6c263ba89909db22dd
John Rogelstad <john@xtuple.com>
2012-05-14 11:01:59 -0400

    initialize javascript before dropping orms.

--- postgresql/delete_system_orms.sql
+++ postgresql/delete_system_orms.sql
@@ -1,7 +1,8 @@
 DO $$
   /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
      See www.xm.ple.com/CPAL for the full text of the software license. */
-
+  plv8.execute("select xt.js_init()");
+  
   var sql = "delete from xt.orm "
             + "where orm_json ~ $1;",
      deleteCondition = '"isSystem":true';
