070fd9cf258a0771ce3eef7c8bda59a1ada1aa69
Erich Ocean <erich.ocean@me.com>
2012-05-17 13:52:27 -0700

    don't use color constants

--- apps/postbooks/view.js
+++ apps/postbooks/view.js
@@ -8,24 +8,6 @@ sc_require('views/carousel');
 sc_require('views/tile_view');
 sc_require('widgets/relation');
 
-var base03 =   "#002b36";
-var base02 =   "#073642";
-var base01 =   "#586e75";
-var base00 =   "#657b83";
-var base0 =    "#839496";
-var base1 =    "#93a1a1";
-var base2 =    "#eee8d5";
-var base3 =    "#fdf6e3";
-var yellow =   "#b58900";
-var orange =   "#cb4b16";
-var red =      "#dc322f";
-var magenta =  "#d33682";
-var violet =   "#6c71c4";
-var blue =     "#268bd2";
-var cyan =     "#2aa198";
-var green =    "#859900";
-var white =    "white";
-
 Postbooks.TilesForClass = function(klass, controller, isRoot) {
   console.log('Postbooks.TilesForClass(', klass, ')');
 
@@ -469,11 +451,10 @@ Postbooks.CreateListViewForClass = function(klass, controller) {
         }
 
         // Clear background.
-        ctx.fillStyle = base3;
-        ctx.fillRect(0, 0, w, h);
+        ctx.clearRect(0, 0, w, h);
 
         // Draw view name.
-        ctx.fillStyle = base03;
+        ctx.fillStyle = 'white';
         
         var K = Postbooks;
         ctx.font = "11pt "+K.TYPEFACE;
@@ -481,7 +462,7 @@ Postbooks.CreateListViewForClass = function(klass, controller) {
         ctx.textAlign = "center";
         ctx.fillText(text, w/2, h/2);
       } else {
-        ctx.fillStyle = base3;
+        ctx.fillStyle = 'white';
         ctx.fillRect(0, 0, ctx.width, ctx.height);
       }
     },
