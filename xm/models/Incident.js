70fd349d35b84eb814f51a550e8d715f0e05d226
John Rogelstad <john@xtuple.com>
2012-06-15 13:55:16 -0400

    Add support for non-nested models.

--- xm/models/Incident.js
+++ xm/models/Incident.js
@@ -4,6 +4,43 @@
   
   @extends XT.Model
 */
+XM.IncidentCategory = XT.Model.extend(
+  /** @scope XM.IncidentCategory.prototype */ {
+
+  recordType: 'XM.IncidentCategory'
+  
+});
+
+/**
+  @class
+  
+  @extends XT.Model
+*/
+XM.IncidentSeverity = XT.Model.extend(
+  /** @scope XM.IncidentSeverity.prototype */ {
+
+  recordType: 'XM.IncidentSeverity'
+  
+});
+
+/**
+  @class
+  
+  @extends XT.Model
+*/
+XM.IncidentResolution = XT.Model.extend(
+  /** @scope XM.IncidentResolution.prototype */ {
+
+  recordType: 'XM.IncidentResolution'
+  
+});
+
+
+/**
+  @class
+  
+  @extends XT.Model
+*/
 XM.IncidentInfo = XT.Model.extend(
   /** @scope XM.IncidentInfo.prototype */ {
 
@@ -25,7 +62,53 @@ XM.IncidentInfo = XT.Model.extend(
     type: Backbone.HasOne,
     key: 'assignedTo',
     relatedModel: 'XM.UserAccountInfo'
+  },{
+    type: Backbone.HasOne,
+    key: 'priority',
+    relatedModel: 'XM.Priority',
+    includeInJSON: 'guid'
+  },{
+    type: Backbone.HasOne,
+    key: 'category',
+    relatedModel: 'XM.IncidentCategory',
+    includeInJSON: 'guid'
   }]
+  
+});
+
+/**
+  @class
+  
+  @extends XT.Collection
+*/
+XM.IncidentCategoryCollection = XT.Collection.extend(
+  /** @scope XM.IncidentCategoryCollection.prototype */ {
+
+  model: XM.IncidentCategory
+  
+});
+
+/**
+  @class
+  
+  @extends XT.Collection
+*/
+XM.IncidentSeverityCollection = XT.Collection.extend(
+  /** @scope XM.IncidentSeverityCollection.prototype */ {
+
+  model: XM.IncidentSeverity
+  
+});
+
+/**
+  @class
+  
+  @extends XT.Collection
+*/
+XM.IncidentResolutionCollection = XT.Collection.extend(
+  /** @scope XM.IncidentResolutionCollection.prototype */ {
+
+  model: XM.IncidentResolution
   
 });
 
@@ -40,3 +123,4 @@ XM.IncidentInfoCollection = XT.Collection.extend(
   model: XM.IncidentInfo
   
 });
+
