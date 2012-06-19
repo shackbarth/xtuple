1a004b9ab01156d988f0b3e1d15c13b451fe5ce8
John Rogelstad <john@xtuple.com>
2012-06-19 19:21:31 -0400

    Add reference tables for crm to startup.

--- xt/foundation/startup.js
+++ xt/foundation/startup.js
@@ -1,8 +1,74 @@
-
+// Session
 XT.StartupTask.addTask(function() {
   var options = {};
   options.success = function() {
     XT.StartupTask.taskComplete();
   };
   XT.session.loadSessionObjects(XT.session.ALL, options);
+});
+
+// Crm
+XT.StartupTask.addTask(function() {
+  var options = {};
+  options.success = function() {
+    XT.StartupTask.taskComplete();
+  };
+  XM.priorities = new XM.PriorityCollection();
+  XM.priorities.fetch(options);
+});
+
+// Incident
+XT.StartupTask.addTask(function() {
+  var options = {};
+  options.success = function() {
+    XT.StartupTask.taskComplete();
+  };
+  XM.incidentCategories = new XM.IncidentCategoryCollection();
+  XM.incidentCategories.fetch(options);
+});
+
+XT.StartupTask.addTask(function() {
+  var options = {};
+  options.success = function() {
+    XT.StartupTask.taskComplete();
+  };
+  XM.incidentResolutions = new XM.IncidentResolutionCollection();
+  XM.incidentResolutions.fetch(options);
+});
+
+XT.StartupTask.addTask(function() {
+  var options = {};
+  options.success = function() {
+    XT.StartupTask.taskComplete();
+  };
+  XM.incidentSeverities = new XM.IncidentSeverityCollection();
+  XM.incidentSeverities.fetch(options);
+});
+
+// Opportunity
+XT.StartupTask.addTask(function() {
+  var options = {};
+  options.success = function() {
+    XT.StartupTask.taskComplete();
+  };
+  XM.opportunityStages = new XM.OpportunityStageCollection();
+  XM.opportunityStages.fetch(options);
+});
+
+XT.StartupTask.addTask(function() {
+  var options = {};
+  options.success = function() {
+    XT.StartupTask.taskComplete();
+  };
+  XM.opportunityTypes = new XM.OpportunityTypeCollection();
+  XM.opportunityTypes.fetch(options);
+});
+
+XT.StartupTask.addTask(function() {
+  var options = {};
+  options.success = function() {
+    XT.StartupTask.taskComplete();
+  };
+  XM.opportunitySources = new XM.OpportunitySourceCollection();
+  XM.opportunitySources.fetch(options);
 });
