4bb0a573ac02ce170e7664844da6c16f588c7c21
W. Cole Davis <cole@xtuple.com>
2012-06-29 12:40:20 -0400

    add necessary functionality to work on session, control entry to tests, add ability to specify tests from command line, work with logfile (pipe output from framework so it does not interfere with test output but is still accessible), and various other fixes

--- vows/main.js
+++ vows/main.js
@@ -14,10 +14,20 @@ _                   = require("underscore");
 io                  = require("socket.io-client");
 
 //......................................
+// XVOWS IS THE GLOBAL NAMESPACE AVAILABLE
+// FOR SHARING VOWS SPECIFIC INFORMATION
+// AND FUNCTIONALITY
+XVOWS = {};
+// PROCESS ANY INCOMING ARGS REAL QUICK
+(function(args) {
+  XVOWS.args = args.slice(2);
+})(process.argv);
+//......................................
 // INCLUDE ALL THE NECESSARY XT FRAMEWORK
 // DEPENDENCIES
 [
   "foundation",
+  "log",
   "datasource",
   "math",
   "request",
@@ -35,6 +45,25 @@ io                  = require("socket.io-client");
 }).forEach(function(path) {
   require(path);
 });
+// CRUSH QUIET SMASH AND DESTROY ANY NORMAL OUTPUT FOR NOW
+// ...actually just...pipe it to some file...
+XVOWS.log = XT.log = function() {
+  var out = XT.$A(arguments).map(function(arg) {
+    return typeof arg === "string"? arg: _util.inspect(arg,true,3);
+  }).join('\n');
+  if (XVOWS.outfile && XVOWS.outfile.writable) {
+    XVOWS.outfile.write("%@\n".f(out), "utf8");
+  } else {
+    XVOWS.outfile = _fs.createWriteStream(_path.join(__dirname, "run.log"), {
+      flags: "a",
+      encoding: "utf8"
+    });
+    XVOWS.outfile.on("error", function(err) {
+      throw err;
+    });
+    XVOWS.outfile.write("\n[ENTRY] started %@\n".f((new Date()).toLocaleString()));
+  }
+};
 //......................................
 // INCLUDE ALL THE NECESSARY XM FRAMEWORK
 // DEPENDENCIES
@@ -61,19 +90,64 @@ require(_path.join(__dirname, "../xm", "startup.js"));
 //console.log(_util.inspect(XT));
 //console.log(_util.inspect(XM));
 
-XT.dataSource.connect();
-
-var timer = setInterval(function() {
-  if (XT.dataSource.isConnected) {
-    clearInterval(timer);
+(function(){
+  // for when a session is retrieved or needs to be
+  // selected and completes...
+  var selectionDone = function() {
+    // register a callback for when startup tasks are
+    // completed
+    console.log("selectionDone(): ");
+    XT.getStartupManager().registerCallback(XVOWS.begin);
+    XT.getStartupManager().start();
+  };
+  
+  XT.dataSource.connect(function() {
     XT.session.acquireSession({
       username: "admin",
       password: "Assemble!Aurora",
       organization: "aurora"
     }, function(result) {
-      console.log(result);
+      if (result.code === 1) {
+        // force new session, always
+        XT.session.selectSession("FORCE_NEW_SESSION", selectionDone);
+      } else if (result.code === 4) {
+        // we're ok? not sure how but, whatever...
+        selectionDone();
+      } else {
+        throw new Error("could not acquire valid session");
+      }
     });
+  });
+})();
+
+XVOWS.findAllTests = function() {
+  var path = _path.join(__dirname, "tests");
+  var tests = XVOWS.tests = {};
+  _fs.readdirSync(path).filter(function(file) {
+    return _path.extname(file) === ".js"? true: false;
+  }).forEach(function(file) {
+    tests[file] = _path.join(__dirname, "tests", file);
+  });
+};
+
+XVOWS.begin = function() {
+  XVOWS.findAllTests();
+  
+  // were there special requests from the command line
+  if (XVOWS.args.length > 0) {
+    XVOWS.args.map(function(file) {
+      return _path.extname(file) === ".js"? file: file + ".js";
+    }).forEach(function(file) {
+      if (XVOWS.tests[file]) {
+        require(XVOWS.tests[file]);
+      } else { console.warn("could not find requested test " + file); }
+    });
+  } else {
+    // nothing special, run them all!
+    for (var test in XVOWS.tests) {
+      if (XVOWS.tests.hasOwnProperty(test)) {
+        require(XVOWS.tests[test]);
+      }
+    }
   }
-}, 1000);
-
-
+};
