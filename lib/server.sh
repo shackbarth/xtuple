dd6cb2849c5f5f88813efc8ce74f25f0d3b7b599
W. Cole Davis <cole@xtuple.com>
2012-07-09 14:53:45 -0400

    adding forever, fixing object.extend to handle prototypal inheritance of functions with observers, fixes to session

--- lib/server.sh
+++ lib/server.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-
-cd lib/xt 
-npm install 
-cd ../.. 
-node ./lib/main.js $@
-
+user=`whoami`
+forever=`which forever`
+if [ -z "$forever" ]; then
+  if [ "$user" == "root" ]; then
+    npm install forever -g
+  else
+    sudo npm install forever -g
+  fi
+fi
+cd lib/xt
+npm install
+cd ../..
+forever stopall
+forever start -a -l forever.log -o datasource.log -e datasource-err.log ./lib/main.js $@
 exit 0
