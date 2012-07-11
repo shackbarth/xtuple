bf76e27cd2533703e7af37e7bfeae568f21221f1
Ben Thompson <ben@xikar.com>
2012-07-11 14:44:27 -0500

    Switch to use batches and less nesting.

--- vows/tests/country.js
+++ vows/tests/country.js
@@ -1,14 +1,15 @@
 /*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
-  plusplus:true, immed:true, eqeqeq:true, forin:true, latedef:true,
+  immed:true, eqeqeq:true, forin:true, latedef:true,
   newcap:true, noarg:true, undef:true */
-/*jslint bitwise: true, nomen: true, indent:2 */
 /*global XVOWS:true, XT:true, XM:true, _:true, setTimeout:true,
-  clearTimeout:true, vows:true, assert:true */
+  clearTimeout:true, vows:true, module:true, assert:true, console:true */
 
 (function () {
   "use strict";
 
-  var createHash, updateHash;
+  var createHash,
+    updateHash,
+    model = new XM.Country();
 
   createHash = {
     name: 'Elbonia',
@@ -23,7 +24,7 @@
   };
 
   vows.describe('XM.Country CRUD test').addBatch({
-    'CREATE': XVOWS.create('XM.Country', {
+    'CREATE ': XVOWS.create(model, {
       '-> Set values': {
         topic: function (model) {
           model.set(createHash);
@@ -32,67 +33,85 @@
         'Last Error is null': function (model) {
           assert.isNull(model.lastError);
         },
-        'Abbreviation is required': function (model) {
-          assert.isTrue(_.contains(model.requiredAttributes, "abbreviation"));
+        '-> Save': XVOWS.save(model)
+      }
+    })
+  }).addBatch({
+    'Check `requiredAttributes`': {
+      topic: function () {
+        return model;
+      },
+      'Abbreviation is required': function (model) {
+        assert.isTrue(_.contains(model.requiredAttributes, "abbreviation"));
+      },
+      'currencyAbbreviation is required': function (model) {
+        assert.isTrue(_.contains(model.requiredAttributes, "currencyAbbreviation"));
+      },
+      'Name is required': function (model) {
+        assert.isTrue(_.contains(model.requiredAttributes, "name"));
+      }
+    }
+  }).addBatch({
+    'Check `validateEdit`': {
+      topic: function () {
+        return model;
+      },
+      'Abbreviation must be 2 letters': function (model) {
+        var err = model.validate({ abbreviation: 'TOO_LONG'});
+        assert.equal(err.code, 'xt1006'); // Error code for invalid length
+        assert.equal(err.params.length, 2); // The length it should be
+      },
+      'Currency Abbreviation must be 3 letters': function (model) {
+        var err = model.validate({ currencyAbbreviation: 'TOO_LONG'});
+        assert.equal(err.code, 'xt1006'); // Error code for invalid length
+        assert.equal(err.params.length, 3); // The length it should be
+      }
+    }
+  }).addBatch({
+    'READ': {
+      topic: function () {
+        return model;
+      },
+      'ID is a number': function (model) {
+        assert.isNumber(model.id);
+      },
+      'Name is `Elbonia`': function (model) {
+        assert.equal(model.get('name'), createHash.name);
+      },
+      'Abbreviation is `EL`': function (model) {
+        assert.equal(model.get('abbreviation'), createHash.abbreviation);
+      },
+      'Currency Name is `Pico`': function (model) {
+        assert.equal(model.get('currencyName'), createHash.currencyName);
+      },
+      'Currency Abbreviation is `PIC`': function (model) {
+        assert.equal(model.get('currencyAbbreviation'),
+          createHash.currencyAbbreviation);
+      }
+    }
+  }).addBatch({
+    'UPDATE ': XVOWS.update(model, {
+      '-> Set values': {
+        topic: function () {
+          model.set(updateHash);
+          return model;
         },
-        'currencyAbbreviation is required': function (model) {
-          assert.isTrue(_.contains(model.requiredAttributes, "currencyAbbreviation"));
+        'Last Error is null': function (model) {
+          assert.isNull(model.lastError);
         },
-        'Name is required': function (model) {
-          assert.isTrue(_.contains(model.requiredAttributes, "name"));
+        'Abbreviation is `EB`': function (model) {
+          assert.equal(model.get('abbreviation'), updateHash.abbreviation);
         },
-        // Validation tests
-        'Abbreviation must be 2 letters': function (model) {
-          var err = model.validate({ abbreviation: 'TOO_LONG'});
-          assert.equal(err.code, 'xt1006'); // Error code for invalid length
-          assert.equal(err.params.length, 2); // The length it should be
+        'Status is `READY_DIRTY`': function (model) {
+          assert.equal(model.getStatusString(), 'READY_DIRTY');
         },
-        'Currency Abbreviation must be 3 letters': function (model) {
-          var err = model.validate({ currencyAbbreviation: 'TOO_LONG'});
-          assert.equal(err.code, 'xt1006'); // Error code for invalid length
-          assert.equal(err.params.length, 3); // The length it should be
-        },
-        '-> Save and READ': XVOWS.save({
-          'Name is `Elbonia`': function (model) {
-            assert.equal(model.get('name'), createHash.name);
-          },
-          'Abbreviation is `EL`': function (model) {
-            assert.equal(model.get('abbreviation'), createHash.abbreviation);
-          },
-          'Currency Name is `Pico`': function (model) {
-            assert.equal(model.get('currencyName'), createHash.currencyName);
-          },
-          'Currency Abbreviation is `PIC`': function (model) {
-            assert.equal(model.get('currencyAbbreviation'),
-              createHash.currencyAbbreviation);
-          },
-          '-> UPDATE': {
-            topic: function (model) {
-              model.set(updateHash);
-              return model;
-            },
-            'Last Error is null': function (model) {
-              assert.isNull(model.lastError);
-            },
-            'Abbreviation is EB': function (model) {
-              assert.equal(model.get('abbreviation'), updateHash.abbreviation);
-            },
-            'Status is READY_DIRTY': function (model) {
-              assert.equal(model.getStatusString(), 'READY_DIRTY');
-            },
-            '-> Commit': XVOWS.save({
-              'Abbreviation is EB' : function (model) {
-                assert.equal(model.get('abbreviation'),
-                  updateHash.abbreviation);
-              },
-              '-> DESTROY': XVOWS.destroy({
-                'FINISH XM.Country': function () {
-                  XVOWS.next();
-                }
-              })
-            })
-          }
-        })
+        '-> Commit': XVOWS.save(model)
+      }
+    })
+  }).addBatch({
+    'DESTROY': XVOWS.destroy(model, {
+      'FINISH XM.Address': function () {
+        XVOWS.next();
       }
     })
   }).run();
