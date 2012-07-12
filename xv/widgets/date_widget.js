810d456b6516010b4f0c9fd8c3823a45d426e245
Steve Hackbarth <stephenhackbarth@gmail.com>
2012-07-12 16:28:49 -0400

    jshinting

--- xv/widgets/date_widget.js
+++ xv/widgets/date_widget.js
@@ -1,6 +1,6 @@
 /*jshint node:true, indent:2, curly:true eqeqeq:true, immed:true, latedef:true, newcap:true, noarg:true,
 regexp:true, undef:true, strict:true, trailing:true, white:true */
-/*global XT:true, enyo:true, _:true */
+/*global XT:true, enyo:true, _:true, Globalize:true */
 (function () {
   "use strict";
 
