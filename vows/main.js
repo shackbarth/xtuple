867531587f0c1dedeaca0ac2010f7386e204aefb
Ben Thompson <ben@xikar.com>
2012-07-18 14:09:33 -0600

    Remove query.js dependancy.

--- vows/main.js
+++ vows/main.js
@@ -245,8 +245,6 @@ XVOWS.destroy = function (model, vows, obj) {
   "log",
   "datasource",
   "math",
-  // TODO - Remove query if John decides against it.
-  "query",
   "request",
   "response",
   "session",
