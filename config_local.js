ccdd65d5e18a77c9018ffb139d77146e2efffb61
W. Cole Davis <cole@xtuple.com>
2012-08-03 14:01:59 -0400

    go ahead and commit in the configuration and change later as needed

---
+++ config_local.js
@@ -0,0 +1,52 @@
+/*jshint node:true, indent:2, curly:false, eqeqeq:true, immed:true, latedef:true, newcap:true, noarg:true,
+regexp:true, undef:true, strict:true, trailing:true, white:true */
+/*global */
+
+(function () {
+  "use strict";
+  
+  module.exports = {
+    debugging: true,
+    version: "beta1",
+    requireDatabase: true,
+    requireServer: true,
+    requireCache: true,
+    functorsDirectory: "./lib/functors",
+    routesDirectory: "./lib/routes",
+    routersDirectory: "./lib/routers",
+    //database: {
+    //  user: "admin",
+    //  password: "admin",
+    //  //organization: "production",
+    //  organization: "40beta",
+    //  //hostname: "bigiron.xtuple.com",
+    //  hostname: "localhost",
+    //  port: 5432
+    //},
+    proxy: {
+      hostname: "localhost",
+      port: 9000
+    },
+    datasource: {
+      sessionTimeout: 15,
+      securePort: 443,
+      secureKeyFile: "./lib/private/key.pem",
+      secureCertFile: "./lib/private/cert.crt",
+      secureSaltFile: "./lib/private/salt.txt"
+    },
+    cache: {
+      session: {
+        hostname: "localhost",
+        port: 27017,
+        schemaDirectory: "./mongo_schemas/session",
+        database: "xtdb"
+      },
+      users: {
+        hostname: "localhost",
+        port: 27017,
+        schemaDirectory: "./mongo_schemas/users",
+        database: "xtusers"
+      }
+    }
+  };
+}());
