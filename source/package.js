70260034e78a3f252eadf74596d321f699709ab7
W. Cole Davis <cole@xtuple.com>
2012-08-07 12:19:51 -0400

    first step refactor with history

---
+++ source/package.js
@@ -0,0 +1,15 @@
+enyo.depends(
+  "$lib/underscore",
+  "$lib/backbone",
+  "$lib/backbone_relational",
+  "$lib/layout",
+  "$lib/onyx",
+  "$lib/globalize",
+  "$lib/gts-plugins",
+  "$lib/date_format",
+  "$lib/socket_io",
+  "xt",
+  "xm",
+  "xv",
+  "app.js"
+);
