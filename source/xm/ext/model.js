920a456337ad15d0bfd9c33d9cc3e88495de2760
John Rogelstad <john@xtuple.com>
2012-08-15 14:28:10 -0400

    Fix problem with destroy.

--- source/xm/ext/model.js
+++ source/xm/ext/model.js
@@ -254,6 +254,7 @@ white:true*/
     destroy: function (options) {
       options = options ? _.clone(options) : {};
       var klass = this.getClass(),
+        canDelete = klass.canDelete(this),
         success = options.success,
         model = this,
         result,
@@ -273,12 +274,12 @@ white:true*/
           });
         };
       if ((parent && parent.canUpdate(this)) ||
-          (!parent && klass.canDelete(this))) {
+          (!parent && canDelete)) {
         this.setStatus(K.DESTROYED_DIRTY, {cascade: true});
         this._wasNew = this.isNew(); // Hack so prototype call will still work
 
         // If it's top level commit to the server now.
-        if (!parent && klass.canDelete(this)) {
+        if (!parent && canDelete) {
           findChildren(this); // Lord Vader ... rise
           this.setStatus(K.BUSY_DESTROYING, {cascade: true});
           options.wait = true;
