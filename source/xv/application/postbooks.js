88b2edbf4fd6689ff206b74e44a1b68aeea852f9
John Rogelstad <john@xtuple.com>
2012-08-16 08:58:06 -0400

    merge screen carousel into postbooks container.

--- source/xv/application/postbooks.js
+++ source/xv/application/postbooks.js
@@ -1,7 +1,7 @@
 
 enyo.kind({
   name: "XV.Postbooks",
-  kind: "Control",
+  kind: "Panels",
   classes: "xt-postbooks enyo-unselectable",
   components: [
     { name: "container", kind: "XV.PostbooksContainer" }
@@ -16,8 +16,20 @@ enyo.kind({
 
 enyo.kind({
   name: "XV.PostbooksContainer",
-  kind: "XV.ScreenCarousel",
+  kind: "Panels",
   classes: "xt-postbooks-container enyo-unselectable",
+  draggable: false,
+  published: {
+    currentView: "",
+    carouselEvents: {
+      crm: "crm",
+      billing: "billing",
+      setup: "setup",
+      workspace: "workspace",
+      dashboard: "dashboard",
+      search: "search"
+    }
+  },
   components: [
     { name: "dashboard", kind: "XV.Dashboard" },
     { name: "crm", kind: "XV.Crm" },
@@ -26,15 +38,85 @@ enyo.kind({
     { name: "workspace", kind: "XV.WorkspaceContainer" },
     { name: "search", kind: "XV.Search" }
   ],
-  carouselEvents: {
-    crm: "crm",
-    billing: "billing",
-    setup: "setup",
-    workspace: "workspace",
-    dashboard: "dashboard",
-    search: "search"
-  },
+  previousView: "",
   getModuleByName: function (name) {
     return this.$[name];
   },
+  currentViewChanged: function () {
+    var children = this.children;
+    var viewName = this.getCurrentView();
+    var view = this.$[viewName];
+    var idx = children.indexOf(view);
+    var prev = this.previousView;
+
+    //this.log(this.name, "currentViewChanged", viewName);
+
+    if (idx === -1) {
+
+      this.log(this.name, "Could not find requested view %@".f(viewName), this.children, view, this.$);
+
+      // can't do anything if we can't find the requested
+      // view, so return the currentView to the one it was
+      // if any
+      if (prev || prev === null) {
+        this.currentView = prev;
+      } else { this.currentView = null; }
+    } else {
+
+      // if the index is the same as the current view don't
+      // do anything
+      if (idx === this.getIndex()) {
+        return;
+      }
+
+      // we found the view requested so go ahead and update
+      this.previousView = viewName;
+      this.setIndex(idx);
+    }
+  },
+  create: function () {
+
+    // need to point any special carousel events to the
+    // proper handler
+    var carouselEvents = this.getCarouselEvents();
+    var handlers = this.handlers;
+    var evt;
+
+    if (carouselEvents) {
+
+      for (evt in carouselEvents) {
+        if (carouselEvents.hasOwnProperty(evt)) {
+          handlers[evt] = "handleCarouselEvent";
+        }
+      }
+    }
+
+    // carry on
+    this.inherited(arguments);
+  },
+  handleCarouselEvent: function (inSender, inEvent) {
+    var carouselEvents = this.getCarouselEvents(),
+      evt = inEvent.eventName,
+      viewName = carouselEvents[evt],
+      previous = this.getActive().name;
+
+    if (viewName) {
+      this.setCurrentView(viewName);
+      inEvent.previous = previous;
+      this.$[viewName].waterfall("onPanelChange", inEvent);
+    }
+
+    // we got this, stop bubbling
+    return true;
+  },
+  completed: function () {
+    var active;
+
+    this.inherited(arguments);
+    active = this.getActive();
+    if (active && active.activated) {
+      active.activated();
+    }
+  }
+    
 });
--- source/xv/views/package.js
+++ source/xv/views/package.js
@@ -2,7 +2,6 @@ enyo.depends(
   "list.js",
   "module.js",
   "pullout.js",
-  "screen_carousel.js",
   "workspace.js",
   "workspace_box.js",
   "repeater_box.js",
