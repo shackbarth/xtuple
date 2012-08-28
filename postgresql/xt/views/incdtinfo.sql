f10d7f54efdbca637129d09bf91e9f9362198268
John Rogelstad <john@xtuple.com>
2012-08-28 10:37:25 -0400

    improve sorting performance. allow a way to sort on priority order for incident.

--- postgresql/xt/views/incdtinfo.sql
+++ postgresql/xt/views/incdtinfo.sql
@@ -5,8 +5,9 @@ create or replace view xt.incdtinfo as
    select incdt_id, incdt_number::text, incdt_summary, incdt_status, incdt_incdtcat_id, 
      incdt_crmacct_id, incdt_cntct_id, incdt_incdtpriority_id, incdt_incdtseverity_id,
      incdt_incdtresolution_id, incdt_owner_username, incdt_assigned_username,
-     incdt_timestamp, incdt_updated
-   from incdt;
+     incdt_timestamp, incdt_updated, coalesce(incdtpriority_order, 99999) as incdtpriority_order
+   from incdt
+     left join incdtpriority on (incdt_incdtpriority_id=incdtpriority_id);
 
 grant all on table xt.incdtinfo to xtrole;
 
