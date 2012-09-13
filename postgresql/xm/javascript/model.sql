3f26505aa3bc4412f3f378834f34ecbc826d9518
John Rogelstad <john@xtuple.com>
2012-09-13 13:04:51 -0400

    Add a function that determines whether a record has been referenced by foreign keys.

--- postgresql/xm/javascript/model.sql
+++ postgresql/xm/javascript/model.sql
@@ -79,6 +79,57 @@ select xt.install_js('XM','Model','xtuple', $$
 
     return result ? result.id : 0;
   }
+
+  /**
+    Return whether a model is referenced by another table.
+  */
+  XM.Model.used = function(recordType, id) {
+    var nameSpace = recordType.beforeDot(),
+      type = recordType.afterDot(),
+      map = XT.Orm.fetch(nameSpace, type),
+      sql,
+      fkeys,
+      uses,
+      i,
+      attr,
+      seq,
+      tableName;
+      
+    /* Determine where this record is used by analyzing foreign key linkages */
+    sql = "select pg_namespace.nspname AS schemaname, " +
+          "con.relname AS tablename, " +
+          "conkey AS seq, " + 
+          "conrelid AS class_id " +
+          "from pg_constraint, pg_class f, pg_class con, pg_namespace " +
+          "where confrelid=f.oid " +
+          "and conrelid=con.oid " +
+          "and f.relname = $1 " +
+          "and con.relnamespace=pg_namespace.oid; "
+    fkeys = plv8.execute(sql, [map.table]);
+    if (DEBUG) { plv8.elog(NOTICE, 'keys:' ,fkeys.length) }
+    for (i = 0; i < fkeys.length; i++) {
+      /* Validate */
+
+      sql = "select attname " + 
+            "from pg_attribute, pg_class " +
+            "where ((attrelid=pg_class.oid) " +
+            " and (pg_class.oid = $1) " +
+            " and (attnum = $2)); ";
+
+      classId = fkeys[i].class_id;
+      seq =  fkeys[i].seq[0];
+      tableName = fkeys[i].schemaname + '.' + fkeys[i].tablename;
+      if (DEBUG) { plv8.elog(NOTICE, 'vars:', classId, seq, tableName) }
+      attr = plv8.execute(sql, [classId, seq])[0].attname;
+
+      /* See if there are dependencies */
+      sql = 'select * from ' + tableName + ' where ' + attr + ' = $1 limit 1;'
+      uses = plv8.execute(sql, [id]);
+      if (uses.length) { return true; }
+    }
+
+    return false
+  }
   
 $$ );
 
