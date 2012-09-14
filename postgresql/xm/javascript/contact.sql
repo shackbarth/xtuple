d3ce99c5414d9b8ff80b68069d8dc13bb33f42c7
John Rogelstad <john@xtuple.com>
2012-09-14 07:51:36 -0400

    Formatting tweaks.

--- postgresql/xm/javascript/contact.sql
+++ postgresql/xm/javascript/contact.sql
@@ -5,86 +5,45 @@ select xt.install_js('XM','Contact','xtuple', $$
   XM.Contact = {};
 
   XM.Contact.isDispatchable = true,
-
-  /** 
-  Returns the address id of an address found that matches the arguments.
-
-  @param {Object} Address object
-  @returns {Boolean}
-  */
   
-  XM.Contact.merge = function(sourceContactId,targetContactId,purge) {
+  XM.Contact.merge = function(sourceContactId, targetContactId, purge) {
+    var err;
 
-	/**
-	 Merge contacts
-
-	 @param {number,number,boolean}
-	 @returns {boolean} 
-  */
-	  var ret, sql, err,
-			  data = Object.create(XT.Data);
-
-	  if(!data.checkPrivilege("MergeContacts")) err = "Access denied."
-	  else if(sourceContactId === undefined) err = "Not defined";
-	  else if(targetContactId === undefined) err = "Not defined";
-	  else if(purge === undefined) err = "Not defined";
-
-	  if(!err) {
-
-			ret = plv8.execute("select cntctmerge($1,$2,$3) AS result;", [sourceContactId,targetContactId,purge])[0].result;
-
-			return ret;
-	  }
-
-	  throw new Error(err);
+    if (!data.checkPrivilege("MergeContacts")) { 
+      err = "Access denied."
+    } else if (sourceContactId === undefined) {
+      err = "Not defined";
+    } else if (targetContactId === undefined) {
+      err = "Not defined";
+    } else if (purge === undefined) {
+      err = "Not defined";
+    }
+    if(!err) {
+      return plv8.execute("select cntctmerge($1,$2,$3) AS result;", [sourceContactId, targetContactId, purge])[0].result;
+    }
+    throw new Error(err);
   }
 
-	XM.Contact.used = function(contactId) {
+  XM.Contact.restore = function(mergeContactId) {
+    var ret,
+      sql,
+      err,
+      data = Object.create(XT.Data);
 
-	/**
-	 Used contacts
-
-	 @param {number}
-	 @returns {boolean} 
-  */
-	  var ret, sql, err,
-			  data = Object.create(XT.Data);
-
-	  if(!data.checkPrivilege("MergeContacts")) err = "Access denied."
-	  else if(contactId === undefined) err = "Not defined";
-
-	  if(!err) {
-
-			ret = plv8.execute("select cntctused($1) AS result;", [contactId])[0].result;
-
-			return ret;
-	  }
-
-	  throw new Error(err);
+    if(!data.checkPrivilege("MergeContacts")) {
+      err = "Access denied."
+     } else if (mergeContactId === undefined) {
+       err = "Not defined";
+     }
+     if(!err) {
+       return plv8.execute("select cntctrestore($1) AS result;", [mergeContactId])[0].result;
+     }
+     throw new Error(err);
   }
-	
-	XM.Contact.restore = function(mergeContactId) {
 
-	/**
-	 Restore contacts
-
-	 @param {number}
-	 @returns {boolean} 
-  */
-	  var ret, sql, err,
-			  data = Object.create(XT.Data);
-
-	  if(!data.checkPrivilege("MergeContacts")) err = "Access denied."
-	  else if(mergeContactId === undefined) err = "Not defined";
-
-	  if(!err) {
-
-			ret = plv8.execute("select cntctrestore($1) AS result;", [mergeContactId])[0].result;
-
-			return ret;
-	  }
-
-	  throw new Error(err);
+  XM.Contact.used = function(id) {
+    return plv8.execute("select cntctused($1) AS result;", [id])[0].result;
   }
+
 $$ );
 
