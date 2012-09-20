74613e4595a616cb2634951a40fe6f56c5998720
Ben Thompson <ben@xtuple.com>
2012-09-20 16:17:47 -0600

    Trying to track down why CRM requires have the wrong path.

--- source/vows/lib/enyo_placeholder.js
+++ source/vows/lib/enyo_placeholder.js
@@ -1,14 +1,19 @@
 (function () {
-  
+
   var _path = X.path, _ = X._;
-  
+
   enyo = {
     relativePath: "",
     depends: function () {
       var root = this.relativePath, files = X.$A(arguments);
+console.log("##### Files from xvows_init lines 472+ #####\r");
+console.log(files + "\r");
       _.each(files, function (file) {
         require(_path.join(root, file));
+console.log(root + "/" + file + "\r");
       });
+console.log("##### The path is not correct for CRM package.js files, so they get dupped #####\r");
+console.log("##### and now we hang sometime after this loop.... #####\r");
     },
     getCookie: function () {
       return X.json(XVOWS.details);
