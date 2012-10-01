425fb0b11366dc63c2e64227332cdd863dca0b8a
W. Cole Davis <cole@coled.net>
2012-10-01 01:40:00 -0400

    udpate string

--- source/xt/en/strings.js
+++ source/xt/en/strings.js
@@ -282,7 +282,7 @@ XT.stringsFor("en_US", {
   "_phone": "Phone",
   "_pleaseLogin": "Please Login",
   "_postalCode": "Postal Code",
-  "_postbooks": "Postbooks",
+  "_postbooks": "PostBooks",
   "_price": "Price",
   "_priceUnit": "Price Unit",
   "_priceUnitRatio": "Price Unit Ratio",
