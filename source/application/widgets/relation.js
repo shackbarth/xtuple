f20291234b1ec8f1b6e5d9241001d966d21b3cca
Linda Nichols <lnichols@xtuple.com>
2012-11-01 09:25:41 -0400

    Issue #18646: Moved images in client/assets to enyo-x/assets.

--- source/application/widgets/relation.js
+++ source/application/widgets/relation.js
@@ -41,7 +41,7 @@ regexp:true, undef:true, trailing:true, white:true */
             onfocus: "receiveFocus"
           },
           {kind: "onyx.MenuDecorator", onSelect: "itemSelected", components: [
-            {kind: "onyx.IconButton", src: "assets/triangle-down-large.png",
+            {kind: "onyx.IconButton", src: "lib/enyo-x/assets/triangle-down-large.png",
               classes: "xv-relationwidget-icon"},
             {name: 'popupMenu', floating: true, kind: "onyx.Menu",
               components: [
