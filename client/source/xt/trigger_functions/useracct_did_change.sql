f427164af6f9f46b229407a05cc2615d5d74f953
John Rogelstad <john@xtuple.com>
2012-11-03 06:35:42 -0400

    issue #18760: Fix unfinished trigger, prevent recursion

--- client/source/xt/trigger_functions/useracct_did_change.sql
+++ client/source/xt/trigger_functions/useracct_did_change.sql
@@ -2,6 +2,7 @@ create or replace function xt.useracct_did_change() returns trigger as $$
 /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
    See www.xm.ple.com/CPAL for the full text of the software license. */
 
+ var sql = "select setUserPreference('" + NEW.useracct_username + "', '{name}', $1)";
  if (TG_OP === 'INSERT') {
    /* Set a unique id. Some ids come from pg_user oid, so there could be overlap */
    var sql = "select nextval('xt.useracct_useracct_id_seq') as sequence;",
@@ -25,12 +26,17 @@ create or replace function xt.useracct_did_change() returns trigger as $$
    }
  }
 
- select setUserPreference(new.nodeusr_username, 'DisableExportContents', case when new.useracct_disable_export then 't' else 'f' end );
- select setUserPreference(new.nodeusr_username, 'propername', new.useracct_propername);
- select setUserPreference(new.nodeusr_username, 'email', new.useracct_email);
- select setUserPreference(new.nodeusr_username, 'initials', new.useracct_initials);
- select setUserPreference(new.nodeusr_username, 'locale_id', new.useracct_locale_id::text);
- select setUserPreference(new.nodeusr_username, 'active', case when new.useracct_active then 't' else 'f' end );
+ /* Avoid recursive behavior by only updating from one side */
+ if (!XT.UserPreferencesUpdating) {
+   XT.UserPreferencesUpdating = true;
+   plv8.execute(sql.replace("{name}", 'DisableExportContents'), [NEW.useracct_disable_export ? 't' : 'f'] );
+   plv8.execute(sql.replace("{name}", 'propername'), [NEW.useracct_propername]);
+   plv8.execute(sql.replace("{name}", 'email'), [NEW.useracct_email]);
+   plv8.execute(sql.replace("{name}", 'initials'), [NEW.useracct_initials]);
+   plv8.execute(sql.replace("{name}", 'locale_id'), [NEW.useracct_locale_id + ""]);
+   plv8.execute(sql.replace("{name}", 'active'), [NEW.useracct_active ? 't' : 'f'] );
+   XT.UserPreferencesUpdating = false;
+ }
   
  return NEW;
 
--- client/source/xt/trigger_functions/usrpref_did_change.sql
+++ client/source/xt/trigger_functions/usrpref_did_change.sql
@@ -4,26 +4,31 @@ create or replace function xt.usrpref_did_change() returns trigger as $$
 
  var sql = 'update xt.useracct set {col} = $1 where useracct_username = $2';
 
- switch (NEW.usrpref_name)
- {
- case 'active':
-   plv8.execute(sql.replace("{col}", "useracct_active"), [NEW.usrpref_value === 't', NEW.usrpref_username]);
-   break;
- case 'DisableExportContents':
-   plv8.execute(sql.replace("{col}", "useracct_disable_export"), [NEW.usrpref_value === 't', NEW.usrpref_username]);
-   break;
- case 'propername':
-   plv8.execute(sql.replace("{col}", "useracct_propername"), [NEW.usrpref_value, NEW.usrpref_username]);
-   break;
- case 'email':
-   plv8.execute(sql.replace("{col}", "useracct_email"), [NEW.usrpref_value, NEW.usrpref_username]);
-   break
- case 'initials':
-   plv8.execute(sql.replace("{col}", "useracct_initials"), [NEW.usrpref_value, NEW.usrpref_username]);
-   break;
- case 'locale_id':
-   plv8.execute(sql.replace("{col}", "useracct_locale_id"), [NEW.usrpref_value - 0, NEW.usrpref_username]);
-   break;
+ /* Avoid recursive behavior by only updating from one side */
+ if (!XT.UserPreferencesUpdating) {
+   XT.UserPreferencesUpdating = true;
+   switch (NEW.usrpref_name)
+   {
+   case 'active':
+     plv8.execute(sql.replace("{col}", "useracct_active"), [NEW.usrpref_value === 't', NEW.usrpref_username]);
+     break;
+   case 'DisableExportContents':
+     plv8.execute(sql.replace("{col}", "useracct_disable_export"), [NEW.usrpref_value === 't', NEW.usrpref_username]);
+     break;
+   case 'propername':
+     plv8.execute(sql.replace("{col}", "useracct_propername"), [NEW.usrpref_value, NEW.usrpref_username]);
+     break;
+   case 'email':
+     plv8.execute(sql.replace("{col}", "useracct_email"), [NEW.usrpref_value, NEW.usrpref_username]);
+     break
+   case 'initials':
+     plv8.execute(sql.replace("{col}", "useracct_initials"), [NEW.usrpref_value, NEW.usrpref_username]);
+     break;
+   case 'locale_id':
+     plv8.execute(sql.replace("{col}", "useracct_locale_id"), [NEW.usrpref_value - 0, NEW.usrpref_username]);
+     break;
+   }
+   XT.UserPreferencesUpdating = false;
  }
  return NEW;
 
