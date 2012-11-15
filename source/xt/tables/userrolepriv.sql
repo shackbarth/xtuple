ecf7a2e189d610d581554d2898a9b529a212107d
John Rogelstad <john@xtuple.com>
2012-11-15 18:29:33 -0500

    issue #18811: tweak

--- source/xt/tables/userrolepriv.sql
+++ source/xt/tables/userrolepriv.sql
@@ -9,6 +9,6 @@ select xt.add_constraint('userrolepriv',
   'foreign key (userrolepriv_userrole_id) references xt.userrole (userrole_id)');
 select xt.add_constraint('userrolepriv',
   'userrolepriv_userrolepriv_priv_id_fkey',
-  'foreign key (userrolepriv_priv_id) references priv (priv_id)');
+  'foreign key (userrolepriv_priv_id) references xt.priv (priv_id)');
 
 comment on table xt.userrolepriv is 'Core table for User Role Privilege Assignments';
