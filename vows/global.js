1806f4182b6a2916716088f0993d2ab551283895
Steve Hackbarth <stephenhackbarth@gmail.com>
2012-11-16 16:53:31 -0500

    issue #18648: vows proof of concept

--- vows/global.js
+++ vows/global.js
@@ -7,53 +7,47 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
 
   var vows = require('vows'),
     assert = require('assert');
-
   require('../main.js');
 
   vows.describe('Check user').addBatch({
-    'when we set up a user': {
+    'when we create a user': {
       topic: function () {
         var that = this,
           schemaLoaded = false,
           privsLoaded = false,
-          schemaOpts = {},
-          privOpts = {};
+          username = "user" + Math.random(),
+          tryUser = function () {
+            if (schemaLoaded && privsLoaded) {
+              var m = new XM.User({id: username, password: '123'}, {isNew: true}),
+                error = function (model, error) {
+                  console.log("error! ", error);
+                };
 
-        var tryUser = function () {
-          if (schemaLoaded && privsLoaded) {
-            var m = new XM.User({id: 'admin@xtuple.com'});
-            //var opts = {
-            //  success: function () {
-            //    X.log('found user');
-            //  },
-            //  error: function () {
-            //    X.log('something broke');
-            //  }
-            //};
-            m.fetch({success: that.callback});
-          }
-        };
-
-        // Set up internal session
-        schemaOpts.success = function () {
-          X.log('Schema Loaded');
-          schemaLoaded = true;
-          tryUser();
-        };
-        privOpts.success = function () {
-          X.log('Privileges Loaded');
-          privsLoaded = true;
-          tryUser();
-        };
+              m.save(null, {force: true, success: that.callback, error: error});
+            }
+          },
+          schemaOpts = {
+            success: function () {
+              X.log('Schema Loaded');
+              schemaLoaded = true;
+              tryUser();
+            }
+          },
+          privOpts = {
+            success: function () {
+              X.log('Privileges Loaded');
+              privsLoaded = true;
+              tryUser();
+            }
+          };
 
         XT.session = Object.create(XT.Session);
         XT.session.loadSessionObjects(XT.session.SCHEMA, schemaOpts);
         XT.session.loadSessionObjects(XT.session.PRIVILEGES, privOpts);
       },
-      'we get a predictable result': function (error, result) {
-        assert.isNull(error);
+      'the user is saved successfully': function (model, result) {
         assert.isNotNull(result);
-        console.log(result);
+        assert.equal('123', result.get('password'));
       }
     }
   }).export(module);
