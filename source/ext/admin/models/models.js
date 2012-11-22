e8230a07ae8d24918d9ed8de59bd2465acdd9015
Steve Hackbarth <stephenhackbarth@gmail.com>
2012-11-22 18:41:12 -0500

    issue #18649: removed symlink

--- source/ext/admin/models/models.js
+++ source/ext/admin/models/models.js
@@ -1,1 +1,188 @@
-../../../../../node-datasource/lib/ext/models.js
+/*jshint indent:2, curly:true eqeqeq:true, immed:true, latedef:true,
+newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true
+white:true*/
+/*global XT:true, XM:true, Backbone:true, _:true */
+
+(function () {
+  "use strict";
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.DatabaseServer = XM.Model.extend({
+    /** @scope XM.DatabaseServer.prototype */
+
+    recordType: 'XM.DatabaseServer',
+
+    idAttribute: 'name',
+
+    databaseType: 'global',
+
+    autoFetchId: false
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.Datasource = XM.Model.extend({
+    /** @scope XM.Datasource.prototype */
+
+    recordType: 'XM.Datasource',
+
+    idAttribute: 'name',
+
+    databaseType: 'global',
+
+    autoFetchId: false
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.Organization = XM.Model.extend({
+    /** @scope XM.Organization.prototype */
+
+    recordType: 'XM.Organization',
+
+    idAttribute: 'name',
+
+    databaseType: 'global',
+
+    autoFetchId: false
+
+  });
+
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.Session = XM.Model.extend({
+    /** @scope XM.Session.prototype */
+
+    recordType: 'XM.Session',
+
+    idAttribute: 'sid',
+
+    databaseType: 'global',
+
+    autoFetchId: false
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SessionOrganization = XM.Model.extend({
+    /** @scope XM.SessionOrganization.prototype */
+
+    recordType: 'XM.SessionOrganization',
+
+    databaseType: 'global'
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.User = XM.Model.extend({
+    /** @scope XM.User.prototype */
+
+    recordType: 'XM.User',
+
+    autoFetchId: false,
+
+    databaseType: 'global'
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.UserOrganization = XM.Model.extend({
+    /** @scope XM.UserOrganization.prototype */
+
+    recordType: 'XM.UserOrganization',
+
+    databaseType: 'global'
+
+  });
+
+  // ..........................................................
+  // COLLECTIONS
+  //
+
+  /**
+    @class
+
+    @extends XM.Collection
+  */
+  XM.DatabaseServerCollection = XM.Collection.extend({
+    /** @scope XM.DatabaseServerCollection.prototype */
+
+    model: XM.DatabaseServer
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Collection
+  */
+  XM.DatasourceCollection = XM.Collection.extend({
+    /** @scope XM.DatasourceCollection.prototype */
+
+    model: XM.DatabaseServer
+  });
+
+  /**
+    @class
+
+    @extends XM.Collection
+  */
+  XM.OrganizationCollection = XM.Collection.extend({
+    /** @scope XM.OrganizationCollection.prototype */
+
+    model: XM.Organization
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Collection
+  */
+  XM.SessionCollection = XM.Collection.extend({
+    /** @scope XM.SessionCollection.prototype */
+
+    model: XM.Session
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Collection
+  */
+  XM.UserCollection = XM.Collection.extend({
+    /** @scope XM.UserCollection.prototype */
+
+    model: XM.User
+
+  });
+}());
