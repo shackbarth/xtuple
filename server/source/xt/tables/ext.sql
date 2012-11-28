47ff1f3f28bd3824f9f6916de36dda1a3f9f850f
John Rogelstad <john@xtuple.com>
2012-11-28 13:14:49 -0500

    issue #18575: Add new tables for extensions

---
+++ server/source/xt/tables/ext.sql
@@ -0,0 +1,10 @@
+-- table definition
+
+select xt.create_table('ext');
+select xt.add_column('ext','ext_id', 'serial');
+select xt.add_column('ext','ext_name', 'text');
+select xt.add_column('ext','ext_descrip', 'text');
+select xt.add_column('ext','ext_location', 'text');
+select xt.add_column('ext','ext_notes', 'text');
+
+comment on table xt.ext is 'Extensions';
