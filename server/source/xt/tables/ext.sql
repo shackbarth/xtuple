99b143fa6af0e313fed5bb3dd0a76b0adc38e972
Steve Hackbarth <stephenhackbarth@gmail.com>
2012-12-03 11:18:51 -0500

    issue #18575: added pk in ext table

--- server/source/xt/tables/ext.sql
+++ server/source/xt/tables/ext.sql
@@ -1,7 +1,7 @@
 -- table definition
 
 select xt.create_table('ext');
-select xt.add_column('ext','ext_id', 'serial');
+select xt.add_column('ext','ext_id', 'serial', 'primary key');
 select xt.add_column('ext','ext_name', 'text');
 select xt.add_column('ext','ext_descrip', 'text');
 select xt.add_column('ext','ext_location', 'text');
