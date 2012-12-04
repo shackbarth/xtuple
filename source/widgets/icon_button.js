77312342dd5955042180366276f807c508788de1
John Rogelstad <john@xtuple.com>
2012-12-04 13:14:43 -0500

    issue #18907: Prevent icon button from issuing events twice when tooltip showing.

--- source/widgets/icon_button.js
+++ source/widgets/icon_button.js
@@ -2,45 +2,53 @@
 regexp:true, undef:true, trailing:true, white:true */
 /*global XT:true, Globalize:true, enyo:true, _:true */
 
-(function() {
+(function () {
 	/**
-	 @class Use to create an IconButton decorated with a Tooltip.
-	 @name XV.IconButton
-	 */
-	enyo.kind( /** @lends XV.IconButton# */ {
-		name: "XV.IconButton",
-		kind: "onyx.TooltipDecorator",
-		published: {
-			src: "",
-			content: ""
-		},
-		style: "margin: 0;",
-		components: [
-			{name: "iconButton", kind: "onyx.IconButton", showing: true},
-			{name: "toolTip", kind: "onyx.Tooltip"}
-		],
+   @class Use to create an IconButton decorated with a Tooltip.
+   @name XV.IconButton
+   */
+  enyo.kind( /** @lends XV.IconButton# */ {
+    name: "XV.IconButton",
+    kind: "onyx.TooltipDecorator",
+    published: {
+      src: "",
+      content: ""
+    },
+    style: "margin: 0;",
+    handlers: {
+      ontap: "tapped"
+    },
+    components: [
+      {name: "iconButton", kind: "onyx.IconButton", showing: true},
+      {name: "toolTip", kind: "onyx.Tooltip"}
+    ],
 
-		create: function() {
-			this.inherited(arguments);
-			this.srcChanged();
-			this.contentChanged();
-		},
+    create: function () {
+      this.inherited(arguments);
+      this.srcChanged();
+      this.contentChanged();
+    },
 
-		srcChanged: function() {
-			this.$.iconButton.setSrc(this.src);
-		},
+    srcChanged: function () {
+      this.$.iconButton.setSrc(this.src);
+    },
 
-		contentChanged: function() {
-			this.$.toolTip.setContent(this.content);
-		},
+    contentChanged: function () {
+      this.$.toolTip.setContent(this.content);
+    },
+		
+    tapped: function (inSender, inEvent) {
+      // Prevent sending event twice
+      if (inSender.name === 'toolTip') { return true; }
+    },
 
-		setActive: function() {
-			this.$.iconButton.setActive(this.active);
-		},
+    setActive: function () {
+      this.$.iconButton.setActive(this.active);
+    },
 
-		setDisabled: function() {
-			this.$.iconButton.setDisabled(this.disabled);
-		}
-	});
+    setDisabled: function () {
+      this.$.iconButton.setDisabled(this.disabled);
+    }
+  });
 	
 }());
