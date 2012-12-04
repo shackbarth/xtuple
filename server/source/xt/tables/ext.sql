3a05d0767cc4d779565423c0fe165b21ccbadc0e
Steve Hackbarth <stephenhackbarth@gmail.com>
2012-12-04 12:34:57 -0500

    issue #18575: added priv name to extension

--- server/source/xt/tables/ext.sql
+++ server/source/xt/tables/ext.sql
@@ -6,5 +6,6 @@ select xt.add_column('ext','ext_name', 'text');
 select xt.add_column('ext','ext_descrip', 'text');
 select xt.add_column('ext','ext_location', 'text');
 select xt.add_column('ext','ext_notes', 'text');
+select xt.add_column('ext','ext_priv_name', 'text');
 
 comment on table xt.ext is 'Extensions';
