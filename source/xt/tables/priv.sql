1cb65e088a718a611f1399e7f3c9d2a5f8b55575
Steve Hackbarth <stephenhackbarth@gmail.com>
2012-12-05 14:33:05 -0500

    issue #19026: add sequence to priv table

--- source/xt/tables/priv.sql
+++ source/xt/tables/priv.sql
@@ -10,4 +10,18 @@ select xt.add_column('priv','priv_context', 'text');
 select xt.add_column('priv','priv_module', 'text');
 select xt.add_constraint('priv','priv_name_idx', 'unique(priv_name)');
 
+do $$
+  var sql = "select * from pg_class c where c.relkind = 'S' and relname = 'priv_priv_id_seq' and relnamespace::regclass::text = 'xt';",
+  qry = plv8.execute(sql);
+
+  if (!qry.length) {
+    sql = 'create sequence xt.priv_priv_id_seq;'
+    plv8.execute(sql);
+    return true;
+  }
+  return false;
+$$ language plv8;
+
+alter table xt.priv alter column priv_id set default nextval('xt.priv_priv_id_seq'::regclass);
+
 comment on table xt.priv is 'Core table for privileges';
