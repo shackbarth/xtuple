34c1c38f321fbfe368249c416911c9e7f358adc5
Steve Hackbarth <stephenhackbarth@gmail.com>
2012-12-07 15:11:16 -0500

    issue #18575: moved in incident_plus client source

--- tools/buildExtensions.sh
+++ tools/buildExtensions.sh
@@ -6,10 +6,14 @@
 rm -rf builds
 mkdir builds
 
+cp source/incident_plus/client/root-package.js package.js
+tools/deploy.sh
+mkdir builds/incident_plus
+mv build/app.js builds/incident_plus/incident_plus.js
+
 cp source/project/client/root-package.js package.js
 tools/deploy.sh
 mkdir builds/project
 mv build/app.js builds/project/project.js
-#mv build/app.css builds/project/project.css
 
 rm package.js
