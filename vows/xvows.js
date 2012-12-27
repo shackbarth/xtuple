0a6e1d389f3dc072d317aa8742f5f2be4be63df3
Steve Hackbarth <stephenhackbarth@gmail.com>
2012-12-27 15:55:50 -0700

    issue #19133: added jenkins code in comments

--- vows/xvows.js
+++ vows/xvows.js
@@ -4,8 +4,16 @@
   "use strict";
 
   var init = require("./lib/xvows_init");
-  init.parseArgs();
-  //init.parseArgs({user: "dev", password: "dev"});
+  init.parseArgs(/*{user: "dev", password: "dev"}*/);
   init.initAll();
+
+/*
+The node script in Jenkins looks like this:
+
+var init = require(process.cwd() + "/../../../../Devel/git/client/vows/lib/xvows_init");
+init.parseArgs({user: "dev", password: "dev"});
+X.basePath = "/home/shackbarth/Devel/git/client/vows";
+init.initAll();
+*/
 
 }());
