70820452ff26ab318c0bd08e4f9f6cdcd9e8fa7f
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-01-08 12:18:50 -0500

    issue #19051: made node path configurable

--- sample_config.js
+++ sample_config.js
@@ -24,6 +24,7 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
       caFile: null,
       saltFile: "./lib/private/salt.txt",
       psqlPath: "psql",
+      nodePath: "node",
 
       // These fields need to be filled in for the datasource
       // to be able to email
