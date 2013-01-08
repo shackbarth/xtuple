2dd2f4609f0fb7446a1cec5965fffbda1516d351
Dave Cramer <davecramer@gmail.com>
2013-01-08 06:05:16 -0500

    production mode

--- update.sh
+++ update.sh
@@ -1,5 +1,26 @@
-#!/bin/sh
-git pull
+#!/bin/sh 
+PRODUCTION=''
+
+while getopts ":p" opt; do
+  case $opt in
+    p)
+      PRODUCTION=true
+      echo 'production mode'
+      ;;
+  esac
+done
+
+git_status=$(git pull  2> /dev/null)
+#echo ${git_status}
+if [[  ${git_status} =~ 'Already up-to-date.' ]]  
+  then 
+  if [ $PRODUCTION ]
+        then
+        echo $PRODUCTION 
+        git checkout `git describe --tags`
+  fi
+fi
+
 git submodule update --recursive
 npm install
 
