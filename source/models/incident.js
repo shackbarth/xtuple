d1179a5e2859cfd23549cc1db04440dac74d2b98
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-01-08 16:51:01 -0500

    issue #18983: brought over some code from connect

--- source/models/incident.js
+++ source/models/incident.js
@@ -1,7 +1,7 @@
 /*jshint indent:2, curly:true eqeqeq:true, immed:true, latedef:true,
 newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true
 white:true*/
-/*global XT:true, XM:true, Backbone:true, _:true, console:true */
+/*global Globalize:true, XT:true, XM:true, Backbone:true, _:true, console:true */
 
 (function () {
   "use strict";
@@ -203,7 +203,13 @@ white:true*/
       }
       options = options ? _.clone(options) : {};
 
-      var success = options.success;
+      var success = options.success,
+        status = this.getStatus(),
+        statusString = this.getIncidentStatusString() ? this.getIncidentStatusString().toUpperCase() : undefined,
+        isNotUpdated = _.size(this.prime) === 0,
+        newComment = _.find(this.get('comments').models, function (comment) {
+          return comment.getStatus() === XM.Model.READY_NEW;
+        });
 
       options.success = function (model, resp, options) {
         var profile = model.getValue("category.emailProfile"),
@@ -240,6 +246,22 @@ white:true*/
         if (success) { success(model, resp, options); }
       };
 
+      // Set change text
+      if (status === XM.Model.READY_NEW && this.get('status') !== 'N') {
+        this._lastChange = "_incidentCreatedStatus".loc()
+                                                   .replace("{status}", statusString);
+      } else if (status === XM.Model.READY_NEW) {
+        this._lastChange = "_incidentCreated".loc();
+      } else if (this.original('status') !== this.get('status')) {
+        this._lastChange = "_incidentChangedStatus".loc()
+                                                   .replace("{status}", statusString);
+      } else if (newComment && isNotUpdated) {
+        this._lastChange = "_incidentNewComment".loc();
+      } else {
+        this._lastChange = "_incidentUpdated".loc();
+      }
+      this._lastChange += ":";
+
       // Handle both `"key", value` and `{key: value}` -style arguments.
       if (_.isObject(key) || _.isEmpty(key)) {
         value = options;
@@ -247,6 +269,7 @@ white:true*/
 
       XM.Document.prototype.save.call(this, key, value, options);
     }
+
 
   });
 
@@ -317,6 +340,62 @@ white:true*/
 
   // Incident status mixin
   XM.Incident = XM.Incident.extend(XM.IncidentStatus);
+
+  // email-relevant mixin
+  XM.Incident = XM.Incident.extend({
+
+    getChangeString: function () {
+      return this._lastChange;
+    },
+
+    getLastCommentString: function () {
+      var comments = this.get('comments'),
+        comment,
+        ret = "";
+      if (comments.length) {
+        // Sort by date descending and take first
+        comments.comparator = function (a, b) {
+          var aval = a.get('created'),
+            bval = b.get('created');
+          return XT.date.compare(bval, aval);
+        };
+        comments.sort();
+        comment = comments.models[0];
+        ret = "_latestComment".loc() +
+              " (" + comment.get('createdBy') + ")" +
+              "\n\n" +
+              comment.get('text');
+      }
+      return ret;
+    },
+
+    getHistoryString: function () {
+      var history = this.get('history'),
+        ret = "",
+        isFirst = true;
+      if (history.length) {
+        // Sort by date ascending
+        history.comparator = function (a, b) {
+          var aval = a.get('created'),
+            bval = b.get('created');
+          return XT.date.compare(aval, bval);
+        };
+        history.sort();
+        _.each(history.models, function (model) {
+          var offset = (new Date()).getTimezoneOffset(), // hack: the data should include timezone
+            created = new Date(model.get('created').getTime() + offset * 60 * 1000),
+            fdate = Globalize.format(created, "d"),
+            ftime = Globalize.format(created, "t");
+          if (!isFirst) { ret += "\n"; }
+          isFirst = false;
+          ret += (fdate + ' ' + ftime).rightPad(' ', 24);
+          ret += model.get('createdBy').slice(0, 17).rightPad(' ', 18);
+          ret += model.get('description');
+        });
+      }
+      return ret;
+    }
+  });
 
   /**
     @class
