d217695f42c4a89de22f9f1ccc398d36d2351272
John Rogelstad <john@xtuple.com>
2013-01-10 14:07:10 -0500

    issue #18406: Progress getting delete interactions to work.

--- source/application/views/module_container.js
+++ source/application/views/module_container.js
@@ -17,7 +17,7 @@ trailing:true white:true*/
           attributes: {src: "lib/enyo-x/assets/splash/index.html"}}
       ]},
       {name: "setup", label: "_setup".loc(), panels: [
-        {name: "configureList", kind: "XV.ConfigurationsList"},
+        {name: "configureList", kind: "XV.ConfigurationsList", toggleSelected: false},
         {name: "userAccountList", kind: "XV.UserAccountList"},
         {name: "userAccountRoleList", kind: "XV.UserAccountRoleList"}
       ]}
