9e2590e4ea3d2b74cc193f4d6b1f068250180b86
John Rogelstad <john@xtuple.com>
2013-01-16 08:13:29 -0500

    issue #18983: Might as well add all the other things that are crm specific

--- source/crm/client/models/startup.js
+++ source/crm/client/models/startup.js
@@ -17,6 +17,72 @@ white:true*/
         XM.incidentEmailProfiles.fetch(options);
       }
     });
+    
+    XT.StartupTasks.push({
+      taskName: "loadIncidentCategories",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
+        XM.incidentCategories = new XM.IncidentCategoryCollection();
+        XM.incidentCategories.fetch(options);
+      }
+    });
+
+    XT.StartupTasks.push({
+      taskName: "loadIncidentResolutions",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
+        XM.incidentResolutions = new XM.IncidentResolutionCollection();
+        XM.incidentResolutions.fetch(options);
+      }
+    });
+    
+    XT.StartupTasks.push({
+      taskName: "loadIncidentSeverities",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
+        XM.incidentSeverities = new XM.IncidentSeverityCollection();
+        XM.incidentSeverities.fetch(options);
+      }
+    });
+
+    XT.StartupTasks.push({
+      taskName: "loadOpportunityStages",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
+        XM.opportunityStages = new XM.OpportunityStageCollection();
+        XM.opportunityStages.fetch(options);
+      }
+    });
+
+    XT.StartupTasks.push({
+      taskName: "loadOpportunityTypes",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
+        XM.opportunityTypes = new XM.OpportunityTypeCollection();
+        XM.opportunityTypes.fetch(options);
+      }
+    });
+
+    XT.StartupTasks.push({
+      taskName: "loadOpportunitySources",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
+        XM.opportunitySources = new XM.OpportunitySourceCollection();
+        XM.opportunitySources.fetch(options);
+      }
+    });
   };
 
 }());
