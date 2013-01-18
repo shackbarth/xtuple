2d4161ca83cd70c237d6029862568aa92686802d
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-01-18 12:19:08 -0500

    issue #19290: moved report route over

--- vows/report_test.js
+++ vows/report_test.js
@@ -8,16 +8,14 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
 (function () {
   "use strict";
 
-  vows.describe('a sample vow').addBatch({
-    'access top page': {
+  vows.describe('Report route').addBatch({
+    'accessing the report route': {
       topic: function () {
-        zombie.visit('http://localhost:2000/login', this.callback);
+        zombie.visit('http://localhost:2000/report?details={json:true}', this.callback);
       },
-      'response should be 200 OK': function (err, browser, status){
-        console.log(err);
-        console.log(browser);
-        console.log(status);
-        assert.equal(status, 200);
+      'should send us to Pentaho': function (err, browser, status){
+        assert.equal(browser.text("title"), "Report Web Viewer"); // that's pentaho's title
+        assert(browser.success);
       }
     },
   }).export(module);
