9ee8ebc06cbc02d0cde54cb2fc20a5e38246640a
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-01-21 16:38:53 -0500

    issue #19290: resetpassword uses ajax

--- source/ext/datasource.js
+++ source/ext/datasource.js
@@ -245,29 +245,15 @@ white:true*/
           id: id,
           newUser: options.newUser
         },
-        complete = function (response) {
-          var params = {}, error;
+        ajax = new enyo.Ajax({
+          url: "http://localhost:2000/resetPassword", // XXX temp until migration
+          //url: "/resetPassword",
+          success: options ? options.success : undefined,
+          error: options ? options.error : undefined
+        });
 
-          // handle error
-          if (response.isError) {
-            if (options && options.error) {
-              params.error = response.message;
-              error = XT.Error.clone('xt1001', { params: params });
-              options.error.call(that, error);
-            }
-            return;
-          }
-
-          // handle success
-          if (options && options.success) {
-            options.success.call(that, response.data);
-          }
-        };
-
-      return XT.Request
-               .handle('function/resetPassword')
-               .notify(complete)
-               .send(payload);
+      ajax.response(this.ajaxSuccess);
+      ajax.go(payload);
     },
 
     /*
