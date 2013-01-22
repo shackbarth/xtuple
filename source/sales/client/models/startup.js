aa16b3baffaa08d0be30e169cdcffb9afc843704
mikerodonnell89 <mikerodonnell89@gmail.com>
2013-01-22 16:23:30 -0500

    Incident #19130 - reworked startup.js into something that might actually work

--- source/sales/client/models/startup.js
+++ source/sales/client/models/startup.js
@@ -8,25 +8,65 @@ white:true*/
 
   XT.extensions.sales.initStartup = function () {
     XT.StartupTasks.push({
-      taskName: "loadStuff",
+      taskName: "loadCustomerTypes",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
+        XM.customerTypes = new XM.CustomerTypeCollection();
+        XM.customerTypes.fetch(options);
+      }
+    });
+    XT.StartupTasks.push({
+      taskName: "loadSalesReps",
       task: function () {
         var options = {
           success: _.bind(this.didComplete, this)
         };
         XM.salesReps = new XM.SalesRepCollection();
         XM.salesReps.fetch(options);
+      }
+    });
+    XT.StartupTasks.push({
+      taskName: "loadTaxZones",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
         XM.taxZones = new XM.TaxZoneCollection();
         XM.taxZones.fetch(options);
+      }
+    });
+    XT.StartupTasks.push({
+      taskName: "loadShipVias",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
         XM.shipVias = new XM.ShipViaCollection();
         XM.shipVias.fetch(options);
+      }
+    });
+    XT.StartupTasks.push({
+      taskName: "loadShipCharges",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
         XM.shipCharges = new XM.ShipChargeCollection();
         XM.shipCharges.fetch(options);
+      }
+    });
+    XT.StartupTasks.push({
+      taskName: "loadShipZones",
+      task: function () {
+        var options = {
+          success: _.bind(this.didComplete, this)
+        };
         XM.shipZones = new XM.ShipZoneCollection();
         XM.shipZones.fetch(options);
       }
     });
   };
-
-//add TaxZone, ShipVia, ShippingForm, ShippingCharges
 
 }());
