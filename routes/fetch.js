2543d096a5b9a3ca2730869cbd9cc6ca515a0b92
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-01-22 12:39:13 -0500

    issue #19290: dispatch

--- routes/fetch.js
+++ routes/fetch.js
@@ -5,6 +5,37 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
 (function () {
   "use strict";
 
+  var dispatchEngine = function (payload, callback) {
+    var organization,
+      query,
+      options;
+
+    // TODO: authenticate
+
+    if (payload && payload.databaseType === 'global') {
+      // run this query against the global database
+
+      options = JSON.parse(JSON.stringify(payload)); // clone
+      //options.username = GLOBAL_USERNAME; // TODO
+      options.success = function (resp) {
+        callback(null, resp);
+      };
+      options.error = function (model, err) {
+        callback(err);
+      };
+      XT.dataSource.dispatch(payload.name, payload.func, payload.params, options);
+
+    } else {
+      // run this query against an instance database
+
+      //payload.username = INSTANCE_USERNAME; // TODO
+      //organization = INSTANCE_ORGANIZATION; // TODO
+      query = "select xt.fetch('%@')".f(JSON.stringify(payload));
+      X.database.query(organization, query, callback);
+    }
+  };
+  exports.dispatchEngine = dispatchEngine;
+
   /**
     Can be called by websockets, or the below fetch function, or REST, etc.
    */
@@ -39,10 +70,7 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
   };
   exports.fetchEngine = fetchEngine;
 
-  /**
-    Accesses the fetchEngine (above) for a GET request a la Express
-   */
-  exports.fetch = function (req, res) {
+  var routeAdapter = function (req, res, engineFunction) {
     var callback = function (err, resp) {
       if (err) {
         res.send(500, {data: err});
@@ -50,8 +78,23 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
         res.send({data: resp});
       }
     };
-    fetchEngine(req.query, callback);
+    engineFunction(req.query, callback);
   };
+
+  /**
+    Accesses the dispatchEngine (above) for a request a la Express
+   */
+  exports.dispatch = function (req, res) {
+    routeAdapter(req, res, dispatchEngine);
+  };
+
+  /**
+    Accesses the fetchEngine (above) for a request a la Express
+   */
+  exports.fetch = function (req, res) {
+    routeAdapter(req, res, fetchEngine);
+  };
+
 
 }());
 
--- routes/todo.md
+++ routes/todo.md
@@ -2,6 +2,7 @@ DONE
 maintenance.js
 report.js -- note that this will have to be updated per the updates in 19268
 fetch.js -- needs functor facade
+dispatch.js -- needs functor facade
 auth.js
 selection.js -- merged into auth.js and renamed scope
 file.js
@@ -9,34 +10,33 @@ redirector.js -- need to put into use with a dedicated server on port 80
 email.js -- test with new client code
 extensions.js -- test with new client code
 resetPassword.js -- test with new client code
+logout.js -- in auth.js
 
 UNNECESSARY (?)
 data.js -- dead code?
 database.js -- admin route. dead code?
 datasource.js -- admin route. dead code?
 organization.js -- admin route. dead code?
-session.js -- replaced by oauth
-session functor?
+session route -- replaced by oauth
+session functor
 export.js -- can we deprecate this w/ pentaho instead of porting?
+oauth2auth.js
+oauth2token.js
 
 NOT DONE
 dataFromKey -- this is being worked on in 19268
-oauth2auth.js
-oauth2token.js
 
 NOT DONE functors -- will need a facade
 commit.js
 retrieve.js
-dispatch.js
 
 NOT DONE functors -- strip off facade
 changePassword.js -- John is currently working on this in 19343
-logout.js
 
 FUTURE REFACTORING POSSIBILITIES
 errors return 200 with isError: true. Better to return 500?
 the big 4 routes are all very similar. it would be possible to reuse code.
-the routes could declare their paths
+the routes could declare their paths. Is the indirection worth the modularity?
 we should put these under test
 unify response formatting
 deprecate databasetype === 'global'
