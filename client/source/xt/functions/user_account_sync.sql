b76b52c6deb70a74cd5a44459b87170c81023ddc
John Rogelstad <john@xtuple.com>
2013-01-25 07:55:55 -0500

    issue #19224: Make sure we always set user to enhanced auth.

--- client/source/xt/functions/user_account_sync.sql
+++ client/source/xt/functions/user_account_sync.sql
@@ -19,11 +19,11 @@ create or replace function xt.user_account_sync(data_hash text) returns void as 
     findXtUser = 'select useracct_username from xt.useracct where useracct_username = $1',
     defaultLocale = "(select coalesce((select locale_id from locale where lower(locale_code) = 'default' limit 1), " +
       "(select locale_id from locale order by locale_id limit 1))) ",
+    setEnhancedAuth = 'select setuserpreference($1, \'UseEnhancedAuthentication\', \'t\');',
     createXtUser = 'insert into xt.useracct ' +
       ' (useracct_username, useracct_active, useracct_propername, useracct_initials, ' + 
       ' useracct_email, useracct_locale_id, useracct_disable_export) ' +
-      ' values ($1, $2, $3, \'\', $4, ' + defaultLocale + ', false);' +
-      'select setuserpreference($1, \'UseEnhancedAuthentication\', \'t\');',
+      ' values ($1, $2, $3, \'\', $4, ' + defaultLocale + ', false);',
     updateXtUser = 'update xt.useracct set' +
       ' useracct_active = $2,' +
       ' useracct_propername = $3, ' +
@@ -37,7 +37,7 @@ create or replace function xt.user_account_sync(data_hash text) returns void as 
     plv8.execute(createPgUser);
   }
 
-  sql = xtUser.length ? updateXtUser : createXtUser;
+  sql = (xtUser.length ? updateXtUser : createXtUser) + setEnhancedAuth;
   plv8.execute(sql, [attrs.username, attrs.active, attrs.propername, attrs.email]);
   
 $$ language plv8;
