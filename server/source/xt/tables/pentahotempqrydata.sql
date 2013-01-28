5b40fa053196ccb8f142ac64524baddf2067475c
root <root@auroraserver.(none)>
2013-01-28 01:34:11 -0500

    incident #19268 pentaho temporary query data table and orm

---
+++ server/source/xt/tables/pentahotempqrydata.sql
@@ -0,0 +1,9 @@
+﻿select xt.create_table('pentahotempqrydata');
+
+select xt.add_column('pentahotempqrydata','pentahotempqrydata_key', 'text', 'primary key');
+select xt.add_column('pentahotempqrydata','pentahotempqrydata_query', 'text');
+select xt.add_column('pentahotempqrydata','pentahotempqrydata_data', 'text');
+select xt.add_column('pentahotempqrydata','pentahotempqrydata_created', 'timestamp');
+
+
+comment on table xt.pentahotempqrydata is 'Temp query data requested from node by client to be rendered by Pentaho';
