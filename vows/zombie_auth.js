a099696df38cb183822aabcdba56b86ce03ba13f
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-01-28 14:28:29 -0500

    issue #19290: zombie auth working POC

--- vows/zombie_auth.js
+++ vows/zombie_auth.js
@@ -1,15 +1,18 @@
 /*jshint node:true, indent:2, curly:false, eqeqeq:true, immed:true, latedef:true, newcap:true, noarg:true,
 regexp:true, undef:true, strict:true, trailing:true, white:true */
-/*global XT:true */
+/*global XT:true, XM:true, XV: true */
 
+// global objects
 XT = {};
+XM = {};
+XV = {};
 
 var assert = require('assert'),
   zombie = require('zombie');
 (function () {
   "use strict";
 
-  var loadApp = function (username, password) {
+  var loadApp = function (username, password, masterCallback) {
     zombie.visit('http://localhost:2000', {debug: false}, function (e, browser) {
       //
       // This is the login screen
@@ -21,28 +24,39 @@ var assert = require('assert'),
           //
           // We skip the scope screen because we're using a user that only has one org
           //
-
-          // Note: make sure the app is BUILT
+          // Note: make sure the app is built
 
           // not quite sure why zombie doesn't do this redirect, but oh well.
           browser.visit('http://localhost:2000/client/index.html', function (e, browser) {
-            //console.log(browser.errors);
-            function appIsReady(window) {
+            browser.wait(function (window) {
+              // this function defines what we're waiting for: for the app state to be 6 (= RUNNING)
               return window.XT.app.state === 6;
-            }
-            browser.wait(appIsReady, function () {
-              XT = browser.window.XT;
+            }, function () {
+              // this is the function that gets run when the above function returns true
 
-              //console.log(XT.session.schema);
-              //console.log(XT.session.schema.toJSON());
-              //console.log(JSON.stringify(XT.session.extensions));
-              process.exit(0);
+              // add the global objects to our global namespace
+              XM = browser.window.XM;
+              XT = browser.window.XT;
+              XV = browser.window.XV;
+
+              // give control back to whoever called us
+              masterCallback();
             });
           });
         });
     });
   };
 
-  loadApp('admin', 'somenew');
+  exports.loadAdd = loadApp;
+
+  var sampleUse = function () {
+    loadApp('admin', 'somenew', function () {
+      //console.log("App is loaded");
+      //console.log(XM.incidentCategories.toJSON());
+      //console.log(XT.session.schema.toJSON());
+      process.exit(0);
+    });
+  };
+
 }());
 
