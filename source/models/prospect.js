71cc61124300efc1ba791edecb0c1f5e10461f70
John Rogelstad <john@xtuple.com>
2013-01-28 13:56:42 -0500

    issue #19130: Add orm model

---
+++ source/models/prospect.js
@@ -0,0 +1,62 @@
+/*jshint indent:2, curly:true eqeqeq:true, immed:true, latedef:true,
+newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true
+white:true*/
+/*global XT:true, XM:true, Backbone:true, _:true */
+
+(function () {
+  "use strict";
+
+  /**
+    @class
+
+    @extends XT.AccountDocument
+  */
+  XM.Prospect = XM.AccountDocument.extend({
+    /** @scope XM.Prospect.prototype */
+
+    recordType: 'XM.Prospect',
+
+    defaults: {
+      isActive: true
+    },
+
+    requiredAttributes: [
+      "isActive"
+    ]
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Info
+  */
+  XM.ProspectRelation = XM.Info.extend({
+    /** @scope XM.ProspectRelation.prototype */
+
+    recordType: 'XM.ProspectRelation',
+
+    editableModel: 'XM.Prospect',
+
+    descriptionKey: "name"
+
+  });
+
+
+  // ..........................................................
+  // COLLECTIONS
+  //
+
+  /**
+    @class
+
+    @extends XM.Collection
+  */
+  XM.ProspectRelationCollection = XM.Collection.extend({
+    /** @scope XM.ProspectRelationCollection.prototype */
+
+    model: XM.ProspectRelation
+
+  });
+
+}());
