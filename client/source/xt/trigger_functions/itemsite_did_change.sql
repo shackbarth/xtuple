2dbb6d21ecc235d77b9a8918a427209598c6bde7
John Rogelstad <john@xtuple.com>
2013-01-29 07:04:26 -0500

    issue 19314: Rename file and tweak others.

--- client/source/xt/trigger_functions/itemsite_did_change.sql
+++ client/source/xt/trigger_functions/itemsite_did_change.sql
@@ -1,4 +1,4 @@
-create or replace function xt.itemsitebeforetriggerxt() returns trigger as $$
+create or replace function xt.itemsite_did_change() returns trigger as $$
 /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
    See www.xm.ple.com/CPAL for the full text of the software license. */
 
