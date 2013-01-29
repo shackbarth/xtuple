103e457af713c30b98444fb2872846e9570d3c26
mikerodonnell89 <mikerodonnell89@gmail.com>
2013-01-29 13:44:53 -0500

    Incident #19130 - moved code from customer to account

--- client/source/xm/javascript/account.sql
+++ client/source/xm/javascript/account.sql
@@ -18,6 +18,42 @@ select xt.install_js('XM','Account','xtuple', $$
       res = plv8.execute(sql, [id])[0].result;
      return res ? XM.Model.used('XM.Account', id) : true;
   }
+  
+  XM.Account.findExisting = function(key, value, id) {
+    /* look in crmacct for any records that have num */
+    /* if you don't find one, return 0 meaning you didn't find anything
+      if you do find a result, look to see if prospect or cust columns are populated.
+      return an object where 1 property is the crmacct_id and the other is A for account, P for prospect or C for customer*/
+      /* use result.crmacct_id to get the crmacct_id column.  the result of the query will be an array so get the 1st result */
+      
+      
+    var res,
+        retVal,
+        row,
+        sql = "select * from crmacct where crmacct_id = $1";
+        
+    res = XM.Model.findExisting("XM.Account", key, value, id);
+    
+    /*if the result is falsey, no CRM account exists by this name, and return 0. */
+    if (!res) {
+      retVal = 0;
+    } else {
+      row = plv8.execute(sql, [res])[0];
+      retVal = {};
+      if (row.crmacct_cust_id) {
+        retVal.id = row.crmacct_cust_id;
+        retVal.type = "C";
+      } else if (row.crmacct_prospect_id) {
+        retVal.id = row.crmacct_prospect_id;
+        retVal.type = "P";
+      } else {
+        retVal.id = row.crmacct_id;
+        retVal.type = "A";
+      }
+    }
+    
+    return JSON.stringify(retVal);
+  }
 
 $$ );
 
--- client/source/xm/javascript/customer.sql
+++ client/source/xm/javascript/customer.sql
@@ -5,41 +5,5 @@ select xt.install_js('XM','Customer','xtuple', $$
   XM.Customer = {};
 
   XM.Customer.isDispatchable = true,
-  
-  XM.Customer.findExisting = function(key, value, id) {
-    /* look in crmacct for any records that have num */
-    /* if you don't find one, return 0 meaning you didn't find anything
-      if you do find a result, look to see if prospect or cust columns are populated.
-      return an object where 1 property is the crmacct_id and the other is A for account, P for prospect or C for customer*/
-      /* use result.crmacct_id to get the crmacct_id column.  the result of the query will be an array so get the 1st result */
-      
-      
-    var res,
-        retVal,
-        row,
-        sql = "select * from crmacct where crmacct_id = $1";
-        
-    res = XM.Model.findExisting("XM.Account", key, value, id);
-    
-    /*if the result is falsey, no CRM account exists by this name, and return 0. */
-    if (!res) {
-      retVal = 0;
-    } else {
-      row = plv8.execute(sql, [res])[0];
-      retVal = {};
-      if (row.crmacct_cust_id) {
-        retVal.id = row.crmacct_cust_id;
-        retVal.type = "C";
-      } else if (row.crmacct_prospect_id) {
-        retVal.id = row.crmacct_prospect_id;
-        retVal.type = "P";
-      } else {
-        retVal.id = row.crmacct_id;
-        retVal.type = "A";
-      }
-    }
-    
-    return JSON.stringify(retVal);
-  }
 
 $$ );
