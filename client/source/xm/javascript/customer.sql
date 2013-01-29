7b0d28915ac04a502abe304b73766b1d71fb6624
mikerodonnell89 <mikerodonnell89@gmail.com>
2013-01-29 14:04:29 -0500

    Incident #19130 - changed... some things...

--- client/source/xm/javascript/customer.sql
+++
@@ -1,9 +0,0 @@
-select xt.install_js('XM','Customer','xtuple', $$
-  /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
-     See www.xm.ple.com/CPAL for the full text of the software license. */
-  
-  XM.Customer = {};
-
-  XM.Customer.isDispatchable = true,
-
-$$ );
