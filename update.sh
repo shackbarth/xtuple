a1f2d2c22f21b3e787491db588d67b8d3a12f1da
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-02-01 21:18:55 -0500

    issue #19379: update using new installer location

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ git submodule update --recursive
 cd server/source
 psql -U admin  global -f init_script.sql
 cd ../../orm/installer
-./installer.js -cli -h localhost -d global -u admin -p 5432 -P admin --path ../../server/orm
+./installer.js -cli -h localhost -d global -u admin -p 5432 -P admin --path ../../database/server/orm
 cd ../../
 wget -O - http://localhost:442/maintenance
 #./updatedbs.sh
