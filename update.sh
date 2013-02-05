035994c6bc9e7d4f8124541cab0f541c36763421
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-02-04 21:38:18 -0500

    issue #19379: updated update

--- update.sh
+++ update.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/bin
+
+# update source code
 cd /usr/local/xtuple/database
 git pull
 git submodule update --recursive
+
+# update global db
 cd server/source
 psql -U admin  global -f init_script.sql
 cd /usr/local/xtuple/node-datasource/installer
-./installer.js -cli -h localhost -d global -u admin -p 5432 -P admin --path ../../database/server/orm
-cd ../../
-wget -O - http://localhost:442/maintenance
+./installer.js -h localhost -d global -u admin -p 5432 -P admin --path ../../database/server/orm
+
+# update instance dbs
+cd /usr/local/xtuple/node-datasource
+node runMaintenance.js
+#wget -O - http://localhost:442/maintenance
 #./updatedbs.sh
 
