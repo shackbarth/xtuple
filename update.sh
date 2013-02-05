3d88128485285dee3447d98fd07be0cefd67ca90
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-02-04 23:12:43 -0500

    issue #19379: updated update

--- update.sh
+++ update.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 export PATH=$PATH:/usr/local/bin
+PRODUCTION=''
+
+while getopts ":p" opt; do
+  case $opt in
+    p)
+      PRODUCTION=true
+      echo 'production mode'
+      ;;
+  esac
+done
 
 # update source code
 cd /usr/local/xtuple/database
 git pull
-git submodule update --recursive
+if [ $PRODUCTION ]
+  then
+  git checkout `git describe --abbrev=0`
+fi
+git submodule update --init --recursive
 
 # update global db
 cd server/source
@@ -15,6 +29,3 @@ cd /usr/local/xtuple/node-datasource/installer
 # update instance dbs
 cd /usr/local/xtuple/node-datasource
 node runMaintenance.js
-#wget -O - http://localhost:442/maintenance
-#./updatedbs.sh
-
