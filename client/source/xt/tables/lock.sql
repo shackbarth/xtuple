da1918f46c82f915b2e038645e2a8c472477dac5
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-02-07 13:08:12 -0500

    issue #19456: bugfix

--- client/source/xt/tables/lock.sql
+++ client/source/xt/tables/lock.sql
@@ -4,7 +4,7 @@ select xt.create_table('lock');
 select xt.add_column('lock','lock_id', 'serial', 'primary key');
 select xt.add_column('lock','lock_table_oid', 'integer', 'not null');
 select xt.add_column('lock','lock_record_id', 'integer', 'not null');
-select xt.add_column('lock','lock_username', 'string', 'not null');
+select xt.add_column('lock','lock_username', 'text', 'not null');
 select xt.add_column('lock','lock_acquired', 'timestamp', 'not null');
 select xt.add_column('lock','lock_pid', 'integer');
 
