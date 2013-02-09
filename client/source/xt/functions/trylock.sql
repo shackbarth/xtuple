5fa56f819c3c67249b7206e9f20840a1bf230d6a
John Rogelstad <john@xtuple.com>
2013-02-09 12:28:57 -0500

    issue #19456: Fix bugs.

--- client/source/xt/functions/trylock.sql
+++ client/source/xt/functions/trylock.sql
@@ -1,8 +1,9 @@
 create or replace function xt.trylock(oid integer, id integer) returns boolean as $$
   var pid = plv8.execute("select pg_backend_pid() as pid;")[0].pid,
     username = plv8.execute("select geteffectivextuser() as username;")[0].username,
-    data = Object.create(XT.Data);
+    data = Object.create(XT.Data),
+    lock = data.tryLock(oid, id, username, {pid: pid});
     
-  return data.tryLock(oid, id, username, {pid: pid});
+  return lock.key ? true : false;
   
 $$ language plv8;
