5b6ab054bea640b7ae1ca10b6f0d75305f9236a6
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-02-11 16:54:46 -0500

    issue #19295: remove exit warning on logout

--- source/session.js
+++ source/session.js
@@ -78,6 +78,12 @@ white:true*/
     },
 
     logout: function () {
+      if (window.onbeforeunload) {
+        // if we've set up a "are you sure you want to leave?" warning, disable that
+        // here. Presumably we've already asked if they want to leave.
+        // delete window.onbeforeunload; // doesn't work
+        window.onbeforeunload = undefined;
+      }
       window.location = "/logout";
     }
 
