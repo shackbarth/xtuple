91842af2bdf82134d168d9fe2f501d720e4b479c
Ben Thompson <ben@xtuple.com>
2013-02-12 18:40:13 -0500

    Fix indent.

--- main.js
+++ main.js
@@ -233,24 +233,24 @@ require('express/node_modules/connect/lib/middleware/session/cookie').prototype.
 // below so our persisted session gets an expires value, but not the browser cookie.
 // See this issue for more details: https://github.com/senchalabs/connect/issues/328
 require('express/node_modules/cookie').serialize = function (name, val, opt) {
-    "use strict";
+  "use strict";
 
-    // Need to add encode here for the stomp to work.
-    var encode = encodeURIComponent;
+  // Need to add encode here for the stomp to work.
+  var encode = encodeURIComponent;
 
-    var pairs = [name + '=' + encode(val)];
-    opt = opt || {};
+  var pairs = [name + '=' + encode(val)];
+  opt = opt || {};
 
-    if (opt.maxAge) pairs.push('Max-Age=' + opt.maxAge);
-    if (opt.domain) pairs.push('Domain=' + opt.domain);
-    if (opt.path) pairs.push('Path=' + opt.path);
-    // TODO - Override here, skip this.
-    //if (opt.expires) pairs.push('Expires=' + opt.expires.toUTCString());
-    if (opt.httpOnly) pairs.push('HttpOnly');
-    if (opt.secure) pairs.push('Secure');
+  if (opt.maxAge) pairs.push('Max-Age=' + opt.maxAge);
+  if (opt.domain) pairs.push('Domain=' + opt.domain);
+  if (opt.path) pairs.push('Path=' + opt.path);
+  // TODO - Override here, skip this.
+  //if (opt.expires) pairs.push('Expires=' + opt.expires.toUTCString());
+  if (opt.httpOnly) pairs.push('HttpOnly');
+  if (opt.secure) pairs.push('Secure');
 
-    return pairs.join('; ');
-  };
+  return pairs.join('; ');
+};
 
 /**
  * ###################################################
