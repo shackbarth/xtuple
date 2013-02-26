c297d8e71e7e3d8ac0e6e711a98fb26a07912289
John Rogelstad <john@xtuple.com>
2013-02-26 12:09:52 -0500

    issue #19550: Add all total detail to view and redirect quote orm to use view so it has totals.

---
+++ enyo-client/database/source/xt/functions/quote_subtotal.sql
@@ -0,0 +1,8 @@
+create or replace function xt.quote_subtotal(integer) returns numeric stable as $$
+  select coalesce(sum(tax),0.0)
+  from (
+    select round (sum(taxdetail_tax),2) as tax 
+    from tax 
+     join calculateTaxDetailSummary('Q', $1, 'T') ON (taxdetail_tax_id=tax_id)
+    group by tax_id) as data;
+$$ language sql
