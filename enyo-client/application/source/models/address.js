41ba0d8864b6a47f64ccb2b778238ef5fdcf74f8
John Rogelstad <john@xtuple.com>
2013-03-07 14:05:50 -0500

    Reduce use of 'force' option to only skip validation in very narrow cases. Instead use "BUSY" status as an indicator of when a record is loading, and therefore when to ignore certain actions.

--- enyo-client/application/source/models/address.js
+++ enyo-client/application/source/models/address.js
@@ -186,7 +186,7 @@ white:true*/
           findExistingOptions.success = function (resp) {
             // If found, set the address with found id
             if (resp) {
-              address.set('id', resp, {force: true}); // Id is all that matters...
+              address.set('id', resp); // Id is all that matters...
               address.status = K.READY_CLEAN; // So we don't come back here
             }
 
