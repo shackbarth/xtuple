4229309d87ee32d5bc0b3f0c71b2ecacbb5e281e
mikerodonnell89 <mikerodonnell89@gmail.com>
2013-03-11 17:11:42 -0400

    started Quote test

--- node-datasource/test/vows/wip/quote.js
+++ node-datasource/test/vows/wip/quote.js
@@ -12,48 +12,65 @@ var XVOWS = XVOWS || {};
     zombieAuth = require("../lib/zombie_auth"),
     crud = require('../lib/crud');
 
-  var createHash = {
-    // TODO: flesh these out
-    number: "3453458" + Math.random(),
-    customer: {id: 97},
-    miscCharge: 0,
-    status: "O",
+  var data = {};
+
+  data.createHash = {
     calculateFreight: false,
+    customer: 97,
+    quoteDate: new Date(),
+    salesRep: 29,
+    terms: 42
   };
 
-  var updateHash = {
-    shipVia: "Gottingen"
+  data.updateHash = {
+    terms: 43
   };
 
-  /**
-    Test the Honorific model
-   */
-  vows.describe('Quote testing').addBatch({
-    'When we load up our app': {
+  vows.describe('XM.Quote CRUD test').addBatch({
+    'INITIALIZE ': {
       topic: function () {
-        zombieAuth.loadApp(this.callback);
+        var that = this,
+          callback = function () {
+            data.model = new XM.Quote();
+            that.callback(null, data);
+          };
+        zombieAuth.loadApp(callback);
       },
-      'We can create a quote model': {
-        topic: function () {
-          return new XM.Quote();
-        },
-        'The model is created successfully': function (topic) {
-          //console.log(topic.getAttributeNames());
-          //console.log(topic.requiredAttributes);
-          assert.isObject(topic);
-          assert.equal(topic.recordType, "XM.Quote");
-        }
-      },
-      'We can run the CRUD tests for Quote': crud.testCrudOperations("Quote", createHash, updateHash),
-
-      'We can test business logic for Quote': {
-        topic: function () {
-          return new XM.Quote();
-        },
-        'The blah is blah': function (topic) {
-          assert.equal(1 + 2, 3);
-        }
+      'The record type is XM.Quote': function (data) {
+        assert.equal(data.model.recordType, "XM.Quote");
       }
     }
+  }).addBatch({
+    'CREATE ': crud.create(data, {
+      '-> Set values': {
+        topic: function (data) {
+          data.model.set(data.createHash);
+          return data;
+        },
+        'Last Error is null': function (data) {
+          assert.isNull(data.model.lastError);
+        },
+        '-> Save': crud.save(data)
+      }
+    })
+  }).addBatch({
+    'READ': {
+      topic: function () {
+        return data;
+      }
+    }
+  }).addBatch({
+    'UPDATE ': crud.update(data, {
+      '-> Set values': {
+        topic: function () {
+          data.model.set(data.updateHash);
+          return data;
+        },
+        '-> Commit': crud.save(data)
+      }
+    })
+  }).addBatch({
+    'DESTROY': crud.destroy(data)
   }).export(module);
+  
 }());
