373cef2407131fce2c35dcf97369ad5e13ef8071
John Rogelstad <john@xtuple.com>
2013-03-14 11:22:52 -0400

    issue #19453: Fix scale handling problems on number widgets.

--- enyo-client/application/source/views/workspace.js
+++ enyo-client/application/source/views/workspace.js
@@ -1219,7 +1219,10 @@ trailing:true white:true*/
             {kind: "FittableColumns", components: [
               {kind: "FittableRows", components: [
                 {kind: "XV.CurrencyPicker", attr: "currency"},
-                {kind: "XV.NumberWidget", attr: "margin"},
+                {kind: "XV.MoneyWidget", attr:
+                  {amount: "margin", currency: "currency"},
+                  label: "_margin".loc(), currencyShowing: false,
+                  effective: "quoteDate"},
                 {kind: "XV.NumberWidget", attr: "freightWeight"}
               ]},
               {kind: "FittableRows", components: [
@@ -1231,8 +1234,10 @@ trailing:true white:true*/
                   {amount: "miscCharge", currency: "currency"},
                   label: "_miscCharge".loc(), currencyShowing: false,
                   effective: "quoteDate"},
-                {kind: "XV.NumberWidget", attr: "freight",
-                  label: "_freight".loc()},
+                {kind: "XV.MoneyWidget", attr:
+                  {amount: "freight", currency: "currency"},
+                  label: "_freight".loc(), currencyShowing: false,
+                  effective: "quoteDate"},
                 {kind: "XV.MoneyWidget", attr:
                   {amount: "taxTotal", currency: "currency"},
                   label: "_tax".loc(), currencyShowing: false,
