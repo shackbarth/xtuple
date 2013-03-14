7918ae3f37cc4cb0c3f6e61c9e5b04bcbe1aeb3d
mikerodonnell89 <mikerodonnell89@gmail.com>
2013-03-14 15:48:54 -0400

    a few test fixes

--- node-datasource/test/vows/models/to_do.js
+++ node-datasource/test/vows/models/to_do.js
@@ -55,6 +55,9 @@ var XVOWS = XVOWS || {};
     'READ': {
       topic: function () {
         return data;
+      },
+      'Last Error is null': function (data) {
+        assert.isNull(data.model.lastError);
       }
     }
   }).addBatch({
