0418afdb07653427f61abe5cb52ce292a5899122
Ben Thompson <ben@xtuple.com>
2013-03-18 17:23:47 -0400

    Issue #19300 tweak schema to support JWT public cert storage.

--- node-datasource/database/source/xt/tables/oa2client.sql
+++ node-datasource/database/source/xt/tables/oa2client.sql
@@ -14,7 +14,6 @@ select xt.add_column('oa2client','oa2client_issued', 'timestamp', '', 'xt', 'The
 select xt.add_column('oa2client','oa2client_auth_uri', 'text', '', 'xt', 'The Authorization Endpoint URI.');
 select xt.add_column('oa2client','oa2client_token_uri', 'text', '', 'xt', 'The Token Endpoint URI.');
 select xt.add_column('oa2client','oa2client_delegated_access', 'boolean', '', 'xt', 'Flag to allow "service_account" client to use delegated access as another user.');
-select xt.add_column('oa2client','oa2client_client_x509_cert_url', '', 'text', 'xt', 'The URL of the public x509 certificate, used to verify JWTs signed by the client.');
-select xt.add_column('oa2client','oa2client_auth_provider_x509_cert_url', 'text', '', 'xt', 'The URL of the public x509 certificate, used to verify the signature on JWTs, such as ID tokens, signed by the authentication provider.');
+select xt.add_column('oa2client','oa2client_client_x509_pub_cert', '', 'text', 'xt', 'The public x509 certificate, used to verify JWTs signed by the client.');
 
 comment on table xt.oa2client is 'Defines global OAuth 2.0 server registered client storage.';
