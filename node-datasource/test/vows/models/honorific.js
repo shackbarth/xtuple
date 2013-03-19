f70679814deb51a0c96cc6e419da0a48bf7a13d8
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-03-18 20:08:31 -0400

    progress

--- node-datasource/test/vows/models/honorific.js
+++ node-datasource/test/vows/models/honorific.js
@@ -34,33 +34,33 @@ var XVOWS = XVOWS || {};
       },
       'Verify the record type is XM.Honorific': function (data) {
         assert.equal(data.model.recordType, "XM.Honorific");
-      }
+      },
+      'We can CREATE a Honorific Model ': crud.create(data, {
+        '-> Set values to the Honorific': {
+          topic: function (data) {
+            data.model.set(data.createHash);
+            return data;
+          },
+          // create vows
+          'Verify Last Error is null': function (data) {
+            assert.isNull(data.model.lastError);
+          },
+          '-> Save the Honorific': crud.save(data, {
+            'We can UPDATE the Honorific ': crud.update(data, {
+              '-> Set values': {
+                topic: function () {
+                  data.model.set(data.updateHash);
+                  return data;
+                },
+                '-> Commit to the Honorific': crud.save(data, {
+                  'destroy': crud.destroy(data)
+                })
+              }
+            })
+          })
+        }
+      })
     }
-  }).addBatch({
-    'We can CREATE a Honorific Model ': crud.create(data, {
-      '-> Set values to the Honorific': {
-        topic: function (data) {
-          data.model.set(data.createHash);
-          return data;
-        },
-        'Verify Last Error is null': function (data) {
-          assert.isNull(data.model.lastError);
-        },
-        '-> Save the Honorific': crud.save(data)
-      }
-    })
-  }).addBatch({
-    'We can UPDATE the Honorific ': crud.update(data, {
-      '-> Set values': {
-        topic: function () {
-          data.model.set(data.updateHash);
-          return data;
-        },
-        '-> Commit to the Honorific': crud.save(data, {
-          'destroy': crud.destroy(data)
-        })
-      }
-    })
   }).export(module);
 
 }());
