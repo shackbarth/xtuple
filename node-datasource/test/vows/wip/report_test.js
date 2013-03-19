c354099069ed1fe39c97ca0eb01c98baddcf7ab6
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-03-18 22:00:28 -0400

    zombie vows triumph

--- node-datasource/test/vows/wip/report_test.js
+++ node-datasource/test/vows/wip/report_test.js
@@ -1,10 +1,10 @@
 /*jshint node:true, indent:2, curly:false, eqeqeq:true, immed:true, latedef:true, newcap:true, noarg:true,
 regexp:true, undef:true, strict:true, trailing:true, white:true */
-/*global X:true */
+/*global XZ:true */
 
-  var vows = require('vows'),
-      assert = require('assert'),
-      zombieAuth = require('../lib/zombie_auth');
+var vows = require('vows'),
+  assert = require('assert'),
+  zombieAuth = require('../lib/zombie_auth');
 
 (function () {
   "use strict";
@@ -18,21 +18,29 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
         zombieAuth.loadApp(this.callback);
       },
       'a GET to the report route for a list': {
-        topic: function (browser) {
-          var url = "https://localhost:443/report?details={%22query%22:{%22recordType%22:%22XM.Locale%22}}";
-          browser.visit(url, {debug: false}, this.callback);
+        topic: function () {
+          var that = this,
+            url = "https://localhost:443/report?details={%22query%22:{%22recordType%22:%22XM.Locale%22}}",
+            // turn the zombie callback into the vows callback per the befuddling vows requirements
+            callbackAdapter = function (err, browser, status) {
+              that.callback(null, {browser: browser});
+            };
+          XZ.browser.visit(url, {debug: false}, callbackAdapter);
         },
-        'should return ok': function (err, browser, status) {
-          assert.ok(browser.success);
+        'should return ok': function (error, topic) {
+          assert.ok(topic.browser.success);
         },
-        'should go to maxhammer': function (err, browser, status) {
-          console.log(browser.text("body"));
-          assert.equal(browser.location.host, "maxhammer.xtuple.com:8080");
-          assert.equal(browser.text("title"), "Report Web Viewer"); // that's pentaho's title
+        'should go to maxhammer': function (error, topic) {
+          assert.equal(topic.browser.location.host, "maxhammer.xtuple.com:8080");
+          assert.equal(topic.browser.text("title"), "Report Web Viewer"); // that's pentaho's title
         },
         'should generate a data key which we can ask about': {
-          topic: function (err, browser, status) {
-            var redirectUrl = browser.location.href,
+          topic: function (topic) {
+            var that = this,
+              callbackAdapter = function (err, browser, status) {
+                that.callback(null, {browser: browser});
+              },
+              redirectUrl = topic.browser.location.href,
               dataKey = redirectUrl.substring(redirectUrl.indexOf('dataKey') + 8),
               url;
 
@@ -42,14 +50,14 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
             }
             url = "https://localhost:443/dataFromKey?dataKey=" + dataKey;
 
-            browser.visit(url, {debug: false}, this.callback);
+            topic.browser.visit(url, {debug: false}, callbackAdapter);
           },
-          'which will give us some data': function (err, browser, status) {
-            var results = JSON.parse(browser.text("body")).data;
+          'which will give us some data': function (error, topic) {
+            var results = JSON.parse(topic.browser.text("body")).data;
             assert.ok(results.length > 1);
           },
-          'which will give us data with locale information': function (err, browser, status) {
-            var results = JSON.parse(browser.text("body")).data;
+          'which will give us data with locale information': function (error, topic) {
+            var results = JSON.parse(topic.browser.text("body")).data;
             assert.ok(results[0].language);
           }
         }
@@ -62,19 +70,27 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
       },
       'a GET to the report route for a particular record': {
         topic: function (browser) {
-          var url = "https://localhost:443/report?details={%22recordType%22:%22XM.Locale%22,%22id%22:3}";
-          browser.visit(url, {debug: false}, this.callback);
+          var that = this,
+            callbackAdapter = function (err, browser, status) {
+              that.callback(null, {browser: browser});
+            },
+            url = "https://localhost:443/report?details={%22recordType%22:%22XM.Locale%22,%22id%22:3}";
+          XZ.browser.visit(url, {debug: false}, callbackAdapter);
         },
-        'should return ok': function (err, browser, status) {
-          assert.ok(browser.success);
+        'should return ok': function (error, topic) {
+          assert.ok(topic.browser.success);
         },
-        'should go to maxhammer': function (err, browser, status) {
-          assert.equal(browser.location.host, "maxhammer.xtuple.com:8080");
-          assert.equal(browser.text("title"), "Report Web Viewer"); // that's pentaho's title
+        'should go to maxhammer': function (error, topic) {
+          assert.equal(topic.browser.location.host, "maxhammer.xtuple.com:8080");
+          assert.equal(topic.browser.text("title"), "Report Web Viewer"); // that's pentaho's title
         },
         'should generate a data key which we can ask about': {
-          topic: function (err, browser, status) {
-            var redirectUrl = browser.location.href,
+          topic: function (topic) {
+            var that = this,
+              callbackAdapter = function (err, browser, status) {
+                that.callback(null, {browser: browser});
+              },
+              redirectUrl = topic.browser.location.href,
               dataKey = redirectUrl.substring(redirectUrl.indexOf('dataKey') + 8),
               url;
 
@@ -84,10 +100,10 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
             }
             url = "https://localhost:443/dataFromKey?dataKey=" + dataKey;
 
-            browser.visit(url, {debug: false}, this.callback);
+            topic.browser.visit(url, {debug: false}, callbackAdapter);
           },
-          'which will give us data object with locale information': function (err, browser, status) {
-            var result = JSON.parse(browser.text("body")).data;
+          'which will give us data object with locale information': function (error, topic) {
+            var result = JSON.parse(topic.browser.text("body")).data;
             assert.equal(typeof result, "object");
             assert.ok(result.language);
           }
