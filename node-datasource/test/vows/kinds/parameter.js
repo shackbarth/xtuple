62dc77a036f8ad10752b15d8c0bc11d5d7a8dc78
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-03-20 14:40:49 -0400

    issue #19829: parameter filter bugfix

---
+++ node-datasource/test/vows/kinds/parameter.js
@@ -0,0 +1,48 @@
+/*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
+  immed:true, eqeqeq:true, forin:true, latedef:true,
+  newcap:true, noarg:true, undef:true */
+/*global XT:true, XM:true, XV:true, process:true, module:true, require:true */
+
+var XVOWS = XVOWS || {};
+(function () {
+  "use strict";
+
+  var zombieAuth = require("../lib/zombie_auth"),
+    vows = require("vows"),
+    _ = require("underscore"),
+    assert = require("assert");
+
+  /**
+    Test the parameter widget.
+   */
+  vows.describe('The ParameterWidget kind').addBatch({
+    'When we load up our app': {
+      topic: function () {
+        zombieAuth.loadApp(this.callback);
+      },
+      'we can create a NumberWidget': {
+        topic: function () {
+          return new XV.ContactListParameters();
+        },
+        'which can take a string as a filter name and set the item correctly': function (topic) {
+          var valueSet;
+          // XXX probably too much mocking.
+          topic.$.account.setValue = function (value) {
+            valueSet = value.id;
+          };
+          topic.setParameterItemValues([{name: 'account', value: {id: 19}}]);
+          assert.equal(valueSet, 19);
+        },
+        'which can take an array as a filter name and set the item correctly': function (topic) {
+          var valueSet;
+          // XXX probably too much mocking.
+          topic.$.account.setValue = function (value) {
+            valueSet = value.id;
+          };
+          topic.setParameterItemValues([{name: ['account', 'accountParent'], value: {id: 19}}]);
+          assert.equal(valueSet, 19);
+        }
+      }
+    }
+  }).export(module);
+}());
