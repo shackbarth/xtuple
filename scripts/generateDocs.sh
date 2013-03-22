a12d7ec1da3ee5209c46827e31c184a0f0cd286c
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-03-21 22:40:08 -0400

    tweaks

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -1,4 +1,5 @@
-../../jsdoc/jsdoc -l -d ../../jsdoc/out -r ../lib/backbone-x/source ../lib/enyo-x/source/widgets/ ../lib/enyo-x/source/views/
+rm -rf ../../jsdoc/out
+../../jsdoc/jsdoc -l -d ../../jsdoc/out -r ../lib/backbone-x/source ../lib/tools/source ../lib/enyo-x/source/widgets ../lib/enyo-x/source/views #../enyo-client/application/source ../enyo-client/extensions/source ../node-datasource/routes
 
 #using the xtuple templates gives me a Error: fs.writeFileSync requires an encoding on Rhino!
 # not sure why
