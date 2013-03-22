95b1bef32164440a5b9f6b320d30a8e52d0e523e
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-03-21 22:10:40 -0400

    added generate doc script

---
+++ scripts/generateDocs.sh
@@ -0,0 +1,9 @@
+../../jsdoc/jsdoc -l -d ../../jsdoc/out -r ../lib/backbone-x/source
+../../jsdoc/jsdoc -l -d ../../jsdoc/out -r ../lib/enyo-x/source/widgets/
+../../jsdoc/jsdoc -l -d ../../jsdoc/out -r ../lib/enyo-x/source/views/
+
+#using the xtuple templates gives me a Error: fs.writeFileSync requires an encoding on Rhino!
+# not sure why
+#../../jsdoc/jsdoc -l -t ./templates/xtuple -d ../../jsdoc/out -r ../lib/backbone-x/source
+#../../jsdoc/jsdoc -l -t ./templates/xtuple -d ../../jsdoc/out -r ../lib/enyo-x/source/widgets/
+#../../jsdoc/jsdoc -l -t ./templates/xtuple -d ../../jsdoc/out -r ../lib/enyo-x/source/views/
