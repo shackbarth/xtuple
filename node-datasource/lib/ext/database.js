7e8e00b74f351ecd7e331c97206c9e9e3edfd6ed
Ben Thompson <ben@xtuple.com>
2013-03-27 16:15:32 -0400

    Issue #19296 First pass at simplifying X.database.

--- node-datasource/lib/ext/database.js
+++ node-datasource/lib/ext/database.js
@@ -1,6 +1,6 @@
 /*jshint node:true, bitwise:true, indent:2, curly:true eqeqeq:true, immed:true, latedef:true, newcap:true, noarg:true,
 regexp:true, undef:true, strict:true, trailing:true, white:true */
-/*global X:true, issue:true */
+/*global X:true, XM:true, issue:true */
 
 (function () {
   "use strict";
@@ -14,11 +14,58 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
     @extends X.Database
    */
   X.database = X.Database.create(/** @lends X.database */{
-    query: function (organization, query, callback) {
+    query: function (organization, dbQuery, done) {
+      // TODO - Don't be stupid, it's not that hard.
+
+      var that = this,
+          orgColl = new XM.OrganizationCollection(),
+          fetchOptions = {};
+
+      fetchOptions.success = function (res) {
+        // We should only get one record back matching the organization.
+        if (res.models.length !== 1) {
+          var message = "Error fetching organization.";
+          X.log(message);
+
+          // No match or multiple which is not allowed. Send nothing.
+          return done(new Error(message));
+        }
+
+        // Get db connection string and make the query.
+        var options = {
+          user: res.models[0].get("databaseServer").user || "admin",
+          hostname: res.models[0].get("databaseServer").hostname,
+          port: res.models[0].get("databaseServer").port,
+          database: organization,
+          password: res.models[0].get("databaseServer").password
+        };
+        that._super.query.call(that, dbQuery, options, done);
+      };
+
+      fetchOptions.error = function (res, err) {
+        if (err.code === 'xt1007') {
+          // XXX should "result not found" really be an error?
+          return done(null, null);
+        } else {
+          var message = "Error fetching organization.";
+          X.log(message);
+          return done(new Error(message));
+        }
+      };
+
+      // Fetch the collection looking for a matching organization.
+      fetchOptions.username = X.options.globalDatabase.nodeUsername;
+      fetchOptions.query = {};
+      fetchOptions.query.parameters = [{attribute: "name", value: organization}];
+      orgColl.fetch(fetchOptions);
+
+
+
       // we have to do a doubly asynchronous lookup (for now)
       // before we can build the correct connection string
-      X.router.lookup("organization", {name: organization}, _.bind(
-        this.didLookup, this, query, callback, {type: "organization", organization: organization}));
+// TODO - Kill X.router.
+      //X.router.lookup("organization", {name: organization}, _.bind(
+        //this.didLookup, this, query, callback, {type: "organization", organization: organization}));
     },
 
     /**
@@ -29,6 +76,7 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
       if (err || !res) return callback(err? err: "could not find %@".f(which.type));
       switch (which.type) {
         case "organization":
+// TODO - Kill X.router.
           X.router.lookup("database", {name: res.databaseServer}, _.bind(
             this.didLookup, this, query, callback, {type: "database", organization: which.organization}));
           break;
