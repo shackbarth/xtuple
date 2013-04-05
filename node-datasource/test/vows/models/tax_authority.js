735dc7110e3d849991c735ab2d55cc9e02f64b02
mikerodonnell89 <mikerodonnell89@gmail.com>
2013-04-05 15:42:23 -0400

    tax auth not saving

--- node-datasource/test/vows/models/tax_authority.js
+++ node-datasource/test/vows/models/tax_authority.js
@@ -7,7 +7,98 @@ var XVOWS = XVOWS || {};
 (function () {
   "use strict";
 
-//creating a new tax auth creates a new CRM account so we have to do all the fetching stuff
-// like for the customer test
+  var vows = require("vows"),
+    assert = require("assert"),
+    zombieAuth = require("../lib/zombie_auth"),
+    crud = require('../lib/crud');
 
+  var data = {},
+    deleteData = {};
+
+  data.createHash = {
+    code: "TESTTAXAUTH",
+    name: "Thom Yorke"
+  };
+
+  data.updateHash = {
+    name: "Jon Fishman"
+  };
+
+  vows.describe('XM.TaxAuthority CRUD test').addBatch({
+    'INITIALIZE ': {
+      topic: function () {
+        var that = this,
+          callback = function () {
+            data.model = new XM.TaxAuthority();
+            that.callback(null, data);
+          };
+        zombieAuth.loadApp(callback);
+      },
+      'The record type is XM.TaxAuthority': function (data) {
+        assert.equal(data.model.recordType, "XM.TaxAuthority");
+      }
+    }
+  }).addBatch({
+    'CREATE ': crud.create(data, {
+      '-> Set values': {
+        topic: function (data) {
+          data.model.set(data.createHash);
+          return data;
+        },
+        'Last Error is null': function (data) {
+          assert.isNull(data.model.lastError);
+        },
+        '-> Save': crud.save(data)
+      }
+    })
+  }).addBatch({
+    'READ': {
+      topic: function () {
+        return data;
+      },
+      'ID is a number': function (data) {
+        assert.isNumber(data.model.id);
+      }
+    }
+  }).addBatch({
+    'UPDATE ': crud.update(data, {
+      '-> Set values': {
+        topic: function () {
+          deleteData.accntId = data.model.get("account");
+          deleteData.accountModel = new XM.Account();
+          data.model.set(data.updateHash);
+          return data;
+        },
+        'Name is `Updated Test TaxAuthority`': function (data) {
+          assert.equal(data.model.get('name'), data.updateHash.name);
+        },
+        '-> Commit': crud.save(data)
+      }
+    })
+  }).addBatch({
+    'DESTROY': crud.destroy(data, {
+      '-> Set values': {
+        //Destroy the tax authority.  When that is successful, destroy the account
+        'tax authority destroyed': function (data) {
+          assert.isTrue(data.model.getStatus() === XM.Model.DESTROYED_CLEAN);
+        },
+        topic: function () {
+          var that = this,
+            fetchOptionsAccnt = {};
+        
+          fetchOptionsAccnt.id = deleteData.accntId;
+        
+          fetchOptionsAccnt.success = function () {
+            var destroyOptionsAccnt = {};
+            destroyOptionsAccnt.success = function () {
+              that.callback(null, data);
+            };
+            deleteData.accountModel.destroy(destroyOptionsAccnt);
+          };
+          deleteData.accountModel.fetch(fetchOptionsAccnt);
+        }
+      }
+    })
+  }).export(module);
+  
 }());
