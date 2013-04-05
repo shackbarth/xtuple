2c2f1e22843b2c1ec2c767b997b6e20512a4c7d2
lynnaloo <lynnaloo@gmail.com>
2013-04-05 15:13:13 -0400

    Issue 19804: Fixed issue with summary panel in quote being cut off when the currency is changed.

--- enyo-client/application/source/views/workspace.js
+++ enyo-client/application/source/views/workspace.js
@@ -1304,41 +1304,43 @@ trailing:true white:true*/
             {kind: "XV.TextArea", attr: "shipNotes", fit: true}
           ]}
         ]},
-        {kind: "FittableRows", title: "_lineItems".loc(), components: [
+        {kind: "FittableRows", title: "_lineItems".loc(), name: "lineItemsPanel", components: [
           {kind: "XV.QuoteLineItemBox", attr: "lineItems", fit: true},
-          {kind: "XV.Groupbox", fit: true, name: "totalGroup",
-            components: [
-            {kind: "onyx.GroupboxHeader", content: "_summary".loc()},
-            {kind: "FittableColumns", name: "totalBox", classes: "xv-totals-panel", components: [
-              {kind: "FittableRows", components: [
-                {kind: "XV.CurrencyPicker", attr: "currency"},
-                {kind: "XV.MoneyWidget", attr:
-                  {amount: "margin", currency: "currency"},
-                  label: "_margin".loc(), currencyShowing: false,
-                  effective: "quoteDate"},
-                {kind: "XV.WeightWidget", attr: "freightWeight"}
-              ]},
-              {kind: "FittableRows", components: [
-                {kind: "XV.MoneyWidget", attr:
-                  {amount: "subtotal", currency: "currency"},
-                  label: "_subtotal".loc(), currencyShowing: false,
-                  effective: "quoteDate"},
-                {kind: "XV.MoneyWidget", attr:
-                  {amount: "miscCharge", currency: "currency"},
-                  label: "_miscCharge".loc(), currencyShowing: false,
-                  effective: "quoteDate"},
-                {kind: "XV.MoneyWidget", attr:
-                  {amount: "freight", currency: "currency"},
-                  label: "_freight".loc(), currencyShowing: false,
-                  effective: "quoteDate"},
-                {kind: "XV.MoneyWidget", attr:
-                  {amount: "taxTotal", currency: "currency"},
-                  label: "_tax".loc(), currencyShowing: false,
-                  effective: "quoteDate"},
-                {kind: "XV.MoneyWidget", attr:
-                  {amount: "total", currency: "currency"},
-                  label: "_total".loc(), currencyShowing: false,
-                  effective: "quoteDate"}
+          // Quote Summary Panel
+          {kind: "FittableRows", fit: true, name: "totalGroup", components: [
+            {kind: "XV.Groupbox", components: [
+              {kind: "onyx.GroupboxHeader", content: "_summary".loc()},
+              {kind: "FittableColumns", name: "totalBox", classes: "xv-totals-panel", components: [
+                {kind: "FittableRows", components: [
+                  {kind: "XV.CurrencyPicker", attr: "currency"},
+                  {kind: "XV.MoneyWidget", attr:
+                    {amount: "margin", currency: "currency"},
+                    label: "_margin".loc(), currencyShowing: false,
+                    effective: "quoteDate"},
+                  {kind: "XV.WeightWidget", attr: "freightWeight"}
+                ]},
+                {kind: "FittableRows", components: [
+                  {kind: "XV.MoneyWidget", attr:
+                    {amount: "subtotal", currency: "currency"},
+                    label: "_subtotal".loc(), currencyShowing: false,
+                    effective: "quoteDate"},
+                  {kind: "XV.MoneyWidget", attr:
+                    {amount: "miscCharge", currency: "currency"},
+                    label: "_miscCharge".loc(), currencyShowing: false,
+                    effective: "quoteDate"},
+                  {kind: "XV.MoneyWidget", attr:
+                    {amount: "freight", currency: "currency"},
+                    label: "_freight".loc(), currencyShowing: false,
+                    effective: "quoteDate"},
+                  {kind: "XV.MoneyWidget", attr:
+                    {amount: "taxTotal", currency: "currency"},
+                    label: "_tax".loc(), currencyShowing: false,
+                    effective: "quoteDate"},
+                  {kind: "XV.MoneyWidget", attr:
+                    {amount: "total", currency: "currency"},
+                    label: "_total".loc(), currencyShowing: false,
+                    effective: "quoteDate"}
+                ]}
               ]}
             ]}
           ]}
@@ -1374,6 +1376,9 @@ trailing:true white:true*/
       this.inherited(arguments);
       if (inEvent.originator.name === 'customerWidget') {
         this.customerChanged();
+      }
+      if (inEvent.originator.name == 'currencyPicker') {
+        this.$.lineItemsPanel.render();
       }
     },
     copyBilltoToShipto: function () {
