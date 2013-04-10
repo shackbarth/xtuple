f8dede76222a22f8add7eb3c49ad4519eb269e9e
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-04-10 12:10:15 -0400

    issue #20022: found-in fixed-in correctly editable on load of incident

--- enyo-client/extensions/source/incident_plus/client/models/incident.js
+++ enyo-client/extensions/source/incident_plus/client/models/incident.js
@@ -7,32 +7,45 @@ white:true*/
   "use strict";
 
   XT.extensions.incidentPlus.initIncidentModels = function () {
-  
+
     var init = XM.Incident.prototype.bindEvents;
     XM.Incident = XM.Incident.extend({
 
       bindEvents: function () {
         init.apply(this, arguments);
         this.on('change:project', this.projectDidChange);
-        this.projectDidChange();
+        this.on('statusChange', this.statusDidChange);
       },
-      
+
       projectDidChange: function () {
         var project = this.get('project'),
           status = this.getStatus(),
           K = XM.Model;
         this.setReadOnly('foundIn', false);
         this.setReadOnly('fixedIn', false);
-        if (status & K.READY) {
+        if (this.isReady()) {
           this.set('foundIn', null);
           this.set('fixedIn', null);
         }
         this.setReadOnly('foundIn', !project);
         this.setReadOnly('fixedIn', !project);
-      }
+      },
 
+      /**
+        projectDidChange does not pick up the change to
+        project when the model is getting loaded, so take
+        extra steps to set the read-only correctly on the
+        load of the model.
+       */
+      statusDidChange: function (model, status) {
+        var project = this.get('project');
+
+        if (model.isReady()) {
+          this.setReadOnly('foundIn', !project);
+          this.setReadOnly('fixedIn', !project);
+        }
+      }
     });
-    
   };
 
 }());
