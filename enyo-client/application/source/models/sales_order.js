9aa925e08205c7ad5a27ee760ea88797df517187
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-04-12 14:24:48 -0400

    issue #20040: sales order model

---
+++ enyo-client/application/source/models/sales_order.js
@@ -0,0 +1,269 @@
+/*jshint indent:2, curly:true eqeqeq:true, immed:true, latedef:true,
+newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true
+white:true*/
+/*global XT:true, XM:true, Backbone:true, _:true */
+
+(function () {
+
+  /**
+    @class
+
+    @extends XM.Document
+  */
+  XM.SalesOrder = XM.Document.extend(/** @lends XM.SalesOrder.prototype */{
+
+    recordType: 'XM.SalesOrder'
+
+  });
+
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderLine = XM.Model.extend(/** @lends XM.SalesOrderLine.prototype */{
+
+    recordType: 'XM.SalesOrderLine'
+
+  });
+
+
+  /**
+    @class
+
+    @extends XM.Comment
+  */
+  XM.SalesOrderComment = XM.Comment.extend(/** @lends XM.SalesOrderComment.prototype */{
+
+    recordType: 'XM.SalesOrderComment',
+
+    sourceName: 'S'
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderAccount = XM.Model.extend(/** @lends XM.SalesOrderAccount.prototype */{
+
+    recordType: 'XM.SalesOrderAccount',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderContact = XM.Model.extend(/** @lends XM.SalesOrderContact.prototype */{
+
+    recordType: 'XM.SalesOrderContact',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderFile = XM.Model.extend(/** @lends XM.SalesOrderFile.prototype */{
+
+    recordType: 'XM.SalesOrderFile',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderItem = XM.Model.extend(/** @lends XM.SalesOrderItem.prototype */{
+
+    recordType: 'XM.SalesOrderItem',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.CharacteristicAssignment
+  */
+  XM.SalesOrderLineCharacteristic = XM.CharacteristicAssignment.extend(/** @lends XM.SalesOrderLineCharacteristic.prototype */{
+
+    recordType: 'XM.SalesOrderLineCharacteristic',
+
+    readOnlyAttributes: [
+      "price"
+    ]
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Comment
+  */
+  XM.SalesOrderLineComment = XM.Comment.extend(/** @lends XM.SalesOrderLineComment.prototype */{
+
+    recordType: 'XM.SalesOrderLineComment',
+
+    sourceName: 'SI'
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Info
+  */
+  XM.SalesOrderListItem = XM.Info.extend(/** @lends XM.SalesOrderListItem.prototype */{
+
+    recordType: 'XM.SalesOrderListItem',
+
+    editableModel: 'XM.SalesOrder'
+
+    /**
+    Returns quote status as a localized string.
+
+    @returns {String}
+    getSalesOrderStatusString: function () {
+      var K = XM.SalesOrder,
+        status = this.get("status");
+      return status === K.OPEN_STATUS ? "_open".loc() : "_closed".loc();
+    }
+    */
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Info
+  */
+  XM.SalesOrderRelation = XM.Info.extend(/** @lends XM.SalesOrderRelation.prototype */{
+
+    recordType: 'XM.SalesOrderRelation',
+
+    editableModel: 'XM.SalesOrder',
+
+    descriptionKey: "number"
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderUrl = XM.Model.extend(/** @lends XM.SalesOrderUrl.prototype */{
+
+    recordType: 'XM.SalesOrderUrl',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderProject = XM.Model.extend(/** @lends XM.SalesOrderProject.prototype */{
+
+    recordType: 'XM.SalesOrderProject',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderIncident = XM.Model.extend(/** @lends XM.SalesOrderIncident.prototype */{
+
+    recordType: 'XM.SalesOrderIncident',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderOpportunity = XM.Model.extend(/** @lends XM.SalesOrderOpportunity.prototype */{
+
+    recordType: 'XM.SalesOrderOpportunity',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderCustomer = XM.Model.extend(/** @lends XM.SalesOrderCustomer.prototype */{
+
+    recordType: 'XM.SalesOrderCustomer',
+
+    isDocumentAssignment: true
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Model
+  */
+  XM.SalesOrderToDo = XM.Model.extend(/* @lends XM.SalesOrderToDo */{
+
+    recordType: 'XM.SalesOrderToDo',
+
+    isDocumentAssignment: true
+
+  });
+
+  // ..........................................................
+  // COLLECTIONS
+  //
+
+  /**
+    @class
+
+    @extends XM.Collection
+  */
+  XM.SalesOrderListItemCollection = XM.Collection.extend(/** @lends XM.SalesOrderListItemCollection.prototype */{
+
+    model: XM.SalesOrderListItem
+
+  });
+
+  /**
+    @class
+
+    @extends XM.Collection
+  */
+  XM.SalesOrderRelationCollection = XM.Collection.extend(/** @lends XM.SalesOrderRelationCollection.prototype */{
+
+    model: XM.SalesOrderRelation
+
+  });
+
+
+}());
