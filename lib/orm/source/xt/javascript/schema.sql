d579b6daa63002d6a2e57eeb88a08f405eb3f79b
Ben Thompson <ben@xtuple.com>
2013-04-15 13:49:12 -0400

    Issue #19304 first pass at a faster schema function.

--- lib/orm/source/xt/javascript/schema.sql
+++ lib/orm/source/xt/javascript/schema.sql
@@ -244,6 +244,7 @@ select xt.install_js('XT','Schema','xtuple', $$
 
     var schemaTable = orm.table,
         column,
+        columns = [],
         schemaColumnInfo = {},
         ret = {};
 
@@ -270,6 +271,7 @@ select xt.install_js('XT','Schema','xtuple', $$
       /* Basic property */
       if (orm.properties[i].attr && orm.properties[i].attr.column) {
         column = orm.properties[i].attr.column;
+        columns.push(column);
 
         /* Get column's PostgreSQL datatype info. */
         schemaColumnInfo = XT.Schema.columnInfo(schemaTable, column);
@@ -323,6 +325,35 @@ select xt.install_js('XT','Schema','xtuple', $$
         throw new Error("Invalid ORM property. Unable to generate JSON-Schema from this ORM.");
       }
     }
+
+
+
+
+
+
+    /* Assign column attributes in one call. */
+    var schemaColumnInfo2 = XT.Schema.columnInfo2(schemaTable, column2);
+    for (var i = 0; i < ret.properties.length; i++) {
+
+      /* Basic properties only. */
+      if (ret.properties[i].attr && ret.properties[i].attr.column) {
+        /* Get column's PostgreSQL datatype info. */
+        schemaColumnInfo = XT.Schema.columnInfo(schemaTable, column);
+        if (!schemaColumnInfo) {
+          return false;
+        }
+
+        /* Loop through the returned schemaColumnInfo properties and add them. */
+        for (var attrname in schemaColumnInfo2) {
+          ret.properties[i].name[attrname] = schemaColumnInfo[attrname];
+        }
+      }
+    }
+
+
+
+
+
 
     /* If this ORM has no column properties, we have an empty object, return false. */
     if (!ret.properties || !Object.keys(ret.properties).length > 0) {
@@ -394,6 +425,228 @@ select xt.install_js('XT','Schema','xtuple', $$
 
     /* If this ORM has no column properties, we have an empty object, return false. */
     if (!ret.length > 0) return false;
+
+    /* return the results */
+    return ret;
+  }
+
+  /**
+   * Return a JSON-Schema property object that MAY include type, format, required
+   * minimum/maximum number, minLength/maxLength string based on a column's
+   * PostgreSQL information_schema.columns record.
+   *
+   * @param {String} An ORM's "table" property formated like "schema.table" or "table".
+   * @param {String} An ORM's properties' "column" attibute  formatted like "column_name".
+   * @returns {Object}
+   */
+  XT.Schema.columnInfo2 = function(ormSchemaTable, ormColumns) {
+    var schema,
+        table,
+        func,
+        schemaTable,
+        sql,
+        funcSql,
+        res,
+        funcRes,
+        ret = {};
+
+    /* Get the schema and table from the ORM table property. */
+    schemaTable = ormSchemaTable.split(".");
+    if (!schemaTable[1]) {
+      schema = "public";
+      table = schemaTable[0];
+    } else {
+      schema = schemaTable[0];
+      table = schemaTable[1];
+    }
+
+    /* Check if this is a function and not a table. */
+    if (table.indexOf("(") !== -1) {
+      /* Extract just the function name from table. */
+      func = table.substring(0,table.indexOf("("));
+
+      /* Look up the "RETURNS SETOF" type. */
+      funcSql = 'select ' +
+                  'type_udt_schema, ' +
+                  'type_udt_name ' +
+                'from information_schema.routines ' +
+                'where 1=1 ' +
+                  'and specific_schema = $1 ' +
+                  'and routine_name = $2; ';
+
+      funcRes = plv8.execute(funcSql, [schema, func]);
+
+      if (funcRes.length === 1) {
+        /* Name of the schema that the return data type of the function is defined in. */
+        schema = funcRes[0].type_udt_schema;
+        /* Name of the return data type of the function. */
+        table = funcRes[0].type_udt_name;
+      }
+    }
+
+    /* Query to get column's PostgreSQL datatype and other schema info. */
+    sql = 'select ' +
+            'column_name' +
+            'data_type, ' +
+            'character_maximum_length, ' +
+            'is_nullable, ' + /* Pull in required from NOT NULL in database. */
+            'CASE ' +
+              "WHEN column_default ILIKE 'nextval%' THEN 'AUTO_INCREMENT' " +
+              'ELSE NULL ' +
+            'END AS column_default, ' + /* Used to determine if integer is really an AUTO_INCREMENT serial. */
+            'col_description( ' + /* Pull in column comments from db. */
+              '( ' +
+                'select ' +
+                        'oid ' +
+                'from pg_class ' +
+                'where 1=1 ' +
+                        'and relname = information_schema.columns.table_name ' +
+                        'and relnamespace = (select oid from pg_namespace where nspname = information_schema.columns.table_schema) ' +
+              ')::oid, ' +
+              'ordinal_position ' +
+            ') AS comment ' +
+          'from information_schema.columns ' +
+          'where 1=1 ' +
+            'and table_schema = $1 ' +
+            'and table_name = $2 ' +
+            'and column_name in $3 ';
+
+    res = plv8.execute(sql, [schema, table, ormColumns]);
+    if (!res.length) return false;
+
+    for (var i = 0; i < res.length; i++) {
+      /* Set "description" if column "comment" is not null. */
+      if (res[i].comment) {
+        ret[res[i].column_name].description = res[i].comment;
+      }
+
+      /* Set "required" if column is not "is_nullable". */
+      if (res[i].is_nullable === "NO") {
+        ret[res[i].column_name].required = true;
+      }
+
+      /* Map PostgreSQL datatype to JSON-Schema type and format. */
+      /* https://developers.google.com/discovery/v1/type-format */
+      /* type: http://tools.ietf.org/html/draft-zyp-json-schema-03#section-5.1 */
+      /* format: http://tools.ietf.org/html/draft-zyp-json-schema-03#section-5.23 */
+      /* min max: http://tools.ietf.org/html/draft-zyp-json-schema-03#section-5.9 */
+      /* lenght: http://tools.ietf.org/html/draft-zyp-json-schema-03#section-5.17 */
+      /* required: http://tools.ietf.org/html/draft-zyp-json-schema-03#section-5.7 */
+      switch (res[i].data_type) {
+        case "ARRAY":
+          ret[res[i].column_name].type = "array";
+          break;
+        case "bigint":
+          /* http://www.postgresql.org/docs/9.1/static/datatype-numeric.html#DATATYPE-SERIAL */
+          if (res[i].is_nullable === "NO" && res[i].column_default === "AUTO_INCREMENT") {
+            ret[res[i].column_name].type = "string";
+            ret[res[i].column_name].format = "uint64";
+            ret[res[i].column_name].minimum = "1";
+            ret[res[i].column_name].maximum = "9223372036854775807";
+          } else {
+            ret[res[i].column_name].type = "string";
+            ret[res[i].column_name].format = "int64";
+            ret[res[i].column_name].minimum = "-9223372036854775808";
+            ret[res[i].column_name].maximum = "9223372036854775807";
+          }
+          break;
+        case "bigserial":
+          ret[res[i].column_name].type = "string";
+          ret[res[i].column_name].format = "uint64";
+          ret[res[i].column_name].minimum = "1";
+          ret[res[i].column_name].maximum = "9223372036854775807";
+          break;
+        case "boolean":
+          ret[res[i].column_name].type = "boolean";
+          break;
+        case "bytea":
+          ret[res[i].column_name].type = "string";
+          break;
+        case "char":
+        case "character":
+          ret[res[i].column_name].type = "string";
+          ret[res[i].column_name].minLength = res[i].character_maximum_length ? res[i].character_maximum_length : null;
+          ret[res[i].column_name].maxLength = res[i].character_maximum_length ? res[i].character_maximum_length : null;
+          break;
+        case "character varying":
+        case "varchar":
+          ret[res[i].column_name].type = "string";
+          ret[res[i].column_name].maxLength = res[i].character_maximum_length ? res[i].character_maximum_length : null;
+          break;
+        case "date":
+          ret[res[i].column_name].type = "string";
+          ret[res[i].column_name].format = "date";
+          break;
+        case "decimal":
+        case "numeric":
+        case "real":
+          ret[res[i].column_name].type = "number";
+          ret[res[i].column_name].format = "float";
+          break;
+        case "double precision":
+          ret[res[i].column_name].type = "number";
+          ret[res[i].column_name].format = "double";
+          break;
+        case "integer":
+          /* http://www.postgresql.org/docs/9.1/static/datatype-numeric.html#DATATYPE-SERIAL */
+          if (res[i].is_nullable === "NO" && res[i].column_default === "AUTO_INCREMENT") {
+            ret[res[i].column_name].type = "integer";
+            ret[res[i].column_name].format = "uint32";
+            ret[res[i].column_name].minimum = "1";
+            ret[res[i].column_name].maximum = "2147483647";
+          } else {
+            ret[res[i].column_name].type = "integer";
+            ret[res[i].column_name].format = "int32";
+            ret[res[i].column_name].minimum = "-2147483648";
+            ret[res[i].column_name].maximum = "2147483647";
+          }
+          break;
+        case "money":
+          ret[res[i].column_name].type = "number";
+          ret[res[i].column_name].format = "float";
+          ret[res[i].column_name].minimum = "-92233720368547758.08";
+          ret[res[i].column_name].maximum = "92233720368547758.07";
+          break;
+        case "serial":
+          ret[res[i].column_name].type = "integer";
+          ret[res[i].column_name].format = "uint32";
+          ret[res[i].column_name].minimum = "1";
+          ret[res[i].column_name].maximum = "2147483647";
+          break;
+        case "smallint":
+          ret[res[i].column_name].type = "integer";
+          ret[res[i].column_name].format = "int32";
+          ret[res[i].column_name].minimum = "-32768";
+          ret[res[i].column_name].maximum = "32767";
+          break;
+        case "text":
+          ret[res[i].column_name].type = "string";
+          break;
+        case "time":
+        case "time without time zone":
+          ret[res[i].column_name].type = "string";
+          ret[res[i].column_name].format = "time";
+          break;
+        case "timestamp":
+        case "timestamp without time zone":
+          ret[res[i].column_name].type = "string";
+          ret[res[i].column_name].format = "date-time";
+          break;
+        case "time with time zone":
+        case "timestamptz":
+        case "timestamp with time zone":
+          ret[res[i].column_name].type = "string";
+          ret[res[i].column_name].format = "date-time";
+          break;
+        case "unknown":
+        case "USER-DEFINED":
+          ret[res[i].column_name].type = "string";
+          break;
+        default:
+          throw new Error("Unsupported datatype format. No known conversion from PostgreSQL to JSON-Schema.");
+          break;
+      }
+    }
 
     /* return the results */
     return ret;
