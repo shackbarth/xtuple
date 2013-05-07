d5047b9e148dcc3848f33bc5b501a995c370de75
John Rogelstad <john@xtuple.com>
2013-05-07 15:50:50 -0400

    Fix relationships in opportunity. Separate some files.

--- enyo-client/database/source/xt/views/todoiteminfo.sql
+++ enyo-client/database/source/xt/views/todoiteminfo.sql
@@ -1,8 +1,10 @@
 select xt.create_view('xt.todoiteminfo', $$
    select todoitem.*, coalesce(incdtpriority_order, 99999) as priority_order,  
-     crmacct_number, cntct_number  
+     crmacct_number, cntct_number, incdt_number, ophead_number
    from todoitem  
      left join incdtpriority on todoitem_priority_id=incdtpriority_id  
      left join crmacct on crmacct_id = todoitem_crmacct_id  
-     left join cntct on cntct_id = todoitem_cntct_id;;
+     left join cntct on cntct_id = todoitem_cntct_id
+     left join incdt on incdt_id = todoitem_incdt_id
+     left join ophead on ophead_id = todoitem_ophead_id
 $$);
