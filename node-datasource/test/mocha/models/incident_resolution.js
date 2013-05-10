fbbb078cafc8a4b8aa9cf6cf30c441385cfabcd5
gene levow <gene@xtuple.com>
2013-05-10 09:33:29 -0400

    adds new mocha crud tests

--- node-datasource/test/mocha/models/incident_resolution.js
+++ node-datasource/test/mocha/models/incident_resolution.js
@@ -3,57 +3,68 @@
   newcap:true, noarg:true, undef:true */
 /*global XT:true, XM:true, XV:true, process:true, module:true, require:true */
 
-var crud = require('../lib/crud'),
-assert = require('chai').assert,
-expect = require('chai').expect,
-zombieAuth = require('../lib/zombie_auth'),
+//var XVOWS = XVOWS || {};
+//(function () {
+//  "use strict";
 
-data = {
+var crud = require('../lib/crud'),
+		assert = require('chai').assert,
+		expect = require('chai').expect,
+		zombieAuth = require('../../vows/lib/zombie_auth'),
+
+   data = {
       recordType: "XM.IncidentResolution",
       autoTestAttributes: true,
       createHash: {
-        name: "Fixed1",
-        description: "Description"
+        description: "test account",
+        name: "test IncidentResolution",
+				order: 10
       },
       updateHash: {
-        name: "fixed2"
+        order: 20
       }
-};
+    };
 
-timeout = 20*1000;
 
-describe('IncidentResolution CRUD Test', function(){
-  before(function (done){
-    this.timeout(timeout);
-    zombieAuth.loadApp(done);
+
+var timeout = 20 * 1000;
+
+describe.skip('IncidentResolution CRUD Test', function () {
+    this.timeout(20 * 1000);
+    it('should perform all the crud operations', function (done) {
+      crud.runAllCrud(data, done);
+    });
   });
 
-  it('should be able to Initialize an XM.IncidentResolution Model', function(){
-    data.model = new XM.IncidentResolution();
-    expect(data.model).to.exist;
-    assert.equal(data.model.recordType, 'XM.IncidentResolution', 'INIT Value should be XM.IncidentResolution');
-  });
+describe('IncidentResolution CRUD Test', function () {
+	  before(function (done){
+        this.timeout(timeout);
+			  zombieAuth.loadApp(done);
+			});
 
-  it('should create an XM.IncidentResolution Model', function(){
-    data.model.set(data.createHash);
-    crud.save(data)
-  });
+			it('should be able to Initialize an XM.IncidentResolution Model', function(){
+				data.model = new XM.IncidentResolution();
+				expect(data.model).to.exist;
+				assert.equal(data.model.recordType, 'XM.IncidentResolution', 'INIT Value should be XM.IncidentResolution');
+			});
 
-  it('should read an XM.IncidentResolution Model', function(){
-    assert.equal(data.model.get('name'), data.createHash.name, 'Model Name READ Value is equal')
-    assert.equal(data.model.get('description'), data.createHash.description, 'Model Description READ Value is equal')
-  });
+			it('should create an XM.IncidentResolution Model', function(){
+				data.model.set(data.createHash);
+				crud.save(data)
+			});
 
-  it('should update an XM.IncidentResolution Model', function(){
-    data.model.set(data.updateHash);
-    crud.save(data)
-    assert.equal(data.model.get('name'), data.updateHash.name, 'Model Name UPDATE Value is equal')
-  });
+			it('should read an XM.IncidentResolution Model', function(){
+				assert.equal(data.model.get('name'), data.createHash.name, 'Model Code READ Value is equal')
+			});
 
-  it('should delete an XM.IncidentResolution Model', function(){
-    crud.destroy(data)
-  });
+			it('should update an XM.IncidentResolution Model', function(){
+				data.model.set(data.updateHash);
+				crud.save(data)
+				assert.equal(data.model.get('order'), data.updateHash.order, 'Model Code UPDATE Value is equal')
+			});
 
+			it('should delete an XM.IncidentResolution Model', function(){
+				crud.destroy(data)
+			});
 
 });
-
