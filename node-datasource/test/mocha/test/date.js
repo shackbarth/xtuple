451bc0de839eb80d473b6ffc1df43df29c32f391
lynnaloo <lynnaloo@gmail.com>
2013-05-13 17:01:22 -0400

    Issue 20011: Adding tests and fixing bugs in DateWidget

--- node-datasource/test/mocha/test/date.js
+++ node-datasource/test/mocha/test/date.js
@@ -27,9 +27,36 @@
     
     describe('Test Text to Date', function () {
       // Test known bad dates
-      it('Test bad date', function () {
-          assert.isFalse(K.textToDate("********"));
-          assert.isFalse(K.textToDate("TEST"));
+      it('Test just plain bad date', function () {
+        assert.isFalse(K.textToDate("********"));
+        assert.isFalse(K.textToDate("BEWARE. I AM BAD."));
+      });
+      
+      it('Test day of the year using #', function () {
+        var days = 20, newDate = new Date();
+        
+        newDate = new Date(newDate.getFullYear(), 0, 20, 0, 0, 0, 0);
+        newDate = K.applyTimezoneOffset(newDate);
+        assert.notStrictEqual(K.textToDate("#20"), newDate);
+        
+        newDate = new Date(newDate.getFullYear(), 0, 65, 0, 0, 0, 0);
+        newDate = K.applyTimezoneOffset(newDate);
+        assert.notStrictEqual(K.textToDate("#65"), newDate);
+        
+        // really, really big one!
+        newDate = new Date(newDate.getFullYear(), 0, 600000000, 0, 0, 0, 0);
+        newDate = K.applyTimezoneOffset(newDate);
+        assert.notStrictEqual(K.textToDate("#600000000"), newDate);
+        
+        // and it works backward!
+        newDate = new Date(newDate.getFullYear(), 0, -40, 0, 0, 0, 0);
+        newDate = K.applyTimezoneOffset(newDate);
+        assert.notStrictEqual(K.textToDate("#-40"), newDate);
+        
+        assert.isFalse(K.textToDate("#tt"));
+        // This is a bug. Both of these should return false!
+        //assert.isFalse(K.textToDate("#"));
+        //assert.isFalse(K.textToDate("#*"));
       });
       
     });
