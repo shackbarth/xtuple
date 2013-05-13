6951f72fd5631020cf2de9dc0da9d92f7f8b9477
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-05-13 13:58:43 -0400

    more deletion

--- node-datasource/database/source/xt/tables/usr.sql
+++
@@ -1,7 +0,0 @@
--- table definition
-
-select xt.create_table('usr');
-select xt.add_column('usr','usr_id', 'text', 'primary key');
-select xt.add_column('usr','usr_password', 'text');
-
-comment on table xt.usr is 'Global users';
