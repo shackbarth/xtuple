e4175a9be450996e7652e4ea52c8bb930941681d
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-05-15 13:05:49 -0400

    moved out admin and incident-plus extensions

--- enyo-client/extensions/source/incident_plus/client/package.js
+++
@@ -1,8 +0,0 @@
-enyo.depends(
-  "core.js",
-  "en",
-  "models",
-  "widgets",
-  "views",
-  "postbooks.js"
-);
--- enyo-client/extensions/source/incident_plus/client/views/package.js
+++
@@ -1,5 +0,0 @@
-enyo.depends(
-  "list_relations.js",
-  "list_relations_editor_box.js",
-  "workspace.js"
-);
