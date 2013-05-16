11ecc0be46f47d0c3f27fe992ad79f16cf88455b
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-05-16 15:20:18 -0400

    cleanup

--- enyo-client/database/source/xt/tables/useruserrole.sql
+++
@@ -1,9 +0,0 @@
-
-insert into xt.useruserrole
-select src.usrgrp_id, usrgrp_grp_id, usrgrp_username
-from usrgrp src
-where not exists (
-  select chk.useruserrole_id
-  from xt.useruserrole chk
-  where chk.useruserrole_id=src.usrgrp_id
-);
