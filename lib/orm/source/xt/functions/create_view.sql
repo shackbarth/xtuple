f307ef691d6b85ed46144b6ac7d56b09402de6ad
John Rogelstad <john@xtuple.com>
2013-05-17 12:52:17 -0400

    issue #20157: tweak.

--- lib/orm/source/xt/functions/create_view.sql
+++ lib/orm/source/xt/functions/create_view.sql
@@ -24,7 +24,7 @@ create or replace function xt.create_view(view_name text, select_text text, read
 
   plv8.execute("grant all on table " + view_name + " to xtrole;");
 
-  /* remove any previously misnamed views */
+  /* remove any previously misnamed rules */
   plv8.execute("drop rule if exists _create on " + view_name);
   plv8.execute("drop rule if exists _update on " + view_name);
   plv8.execute("drop rule if exists _delete on " + view_name);
