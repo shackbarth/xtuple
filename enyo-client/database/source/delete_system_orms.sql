3163d9450666698f0d6d7739c0af96552d06f8e2
Ben Thompson <ben@xtuple.com>
2013-05-21 16:34:46 -0400

    Issue #20254 fix some bugs for init_instance run.

--- enyo-client/database/source/delete_system_orms.sql
+++ enyo-client/database/source/delete_system_orms.sql
@@ -32,6 +32,13 @@ DO $$
       plv8.execute('drop view if exists xm.' + viewname + ' cascade;');
     }
 
+    sql = "select viewname from pg_views where schemaname = 'sys';"
+    result = plv8.execute(sql);
+    for (i = 0; i < result.length; i++) {
+      viewname = result[i].viewname;
+      plv8.execute('drop view if exists sys.' + viewname + ' cascade;');
+    }
+
     plv8.execute("select xt.js_init()");
     plv8.execute("delete from xt.orm where orm_json ~ '\"isSystem\":true';");
   }
--- enyo-client/database/source/xt/tables/incdtemlprofile.sql
+++ enyo-client/database/source/xt/tables/incdtemlprofile.sql
@@ -7,4 +7,4 @@ select xt.add_constraint('incdtemlprofile','incdtemlprofile_pkey', 'primary key 
 comment on table xt.incdtemlprofile is 'Core table for incident email profiles';
 
 -- this priv does not exist in postbooks so create it here
-select xt.add_priv('MaintainEmailProfiles', 'Can Maintain Incident Email Profiles', 'MaintainEmailProfiles', 'Incidents', 'xtuple', 'Incidents', false);
+select xt.add_priv('MaintainEmailProfiles', 'Can Maintain Incident Email Profiles', 'Incidents', 'Incident');
