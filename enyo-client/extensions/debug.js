9033bb2af360b609bf97345fae2d70d9b696e3b8
John Rogelstad <john@xtuple.com>
2013-05-23 15:12:06 -0400

    issue #20254: Make debug available and workable at all times.

---
+++ enyo-client/extensions/debug.js
@@ -0,0 +1,5 @@
+enyo.depends(
+  '/dev/core-extensions/source/project/client/package.js',
+  '/dev/core-extensions/source/crm/client/package.js',
+  '/dev/core-extensions/source/sales/client/package.js'
+);
