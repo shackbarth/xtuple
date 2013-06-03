af6e511ca518a5325f9d1ae224ff7ab8b10dbe23
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-06-03 16:12:58 -0400

    issue #20373: made xt.usrinfo view less verbose by building on usr view

--- enyo-client/database/source/xt/trigger_functions/usr_did_change.sql
+++ enyo-client/database/source/xt/trigger_functions/usr_did_change.sql
@@ -1,4 +1,4 @@
-﻿create or replace function xt.usr_did_change() returns trigger as $$
+create or replace function xt.usr_did_change() returns trigger as $$
 /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
    See www.xm.ple.com/CPAL for the full text of the software license. */
 
@@ -19,6 +19,7 @@
    plv8.execute(sql.replace("{name}", 'locale_id'), [NEW.usr_locale_id + ""]);
    plv8.execute(sql.replace("{name}", 'active'), [NEW.usr_active ? 't' : 'f'] );
    plv8.execute(sql.replace("{name}", 'UseEnhancedAuthentication'), [NEW.usr_enhancedauth ? 't' : 'f'] );
+   plv8.execute(sql.replace("{name}", 'DisableExportContents'), [NEW.usr_disableExport ? 't' : 'f'] );
    XT.UserPreferencesUpdating = false;
  }
   
