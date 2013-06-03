826f23fa787e0b080296d8c04cd330f6f36f860a
John Rogelstad <john@xtuple.com>
2013-06-03 16:31:30 -0400

    issue #19797: Incremental progress

--- lib/orm/source/xt/javascript/data.sql
+++ lib/orm/source/xt/javascript/data.sql
@@ -1517,8 +1517,8 @@
         pkey = XT.Orm.primaryKey(orm),
         nkey = XT.Orm.naturalKey(orm),
         props = orm.properties,
-        readPriv = orm.privileges && orm.privileges.attribute && orm.privileges.attribute.read ?
-          orm.privileges.attribute.read : false,
+        viewPriv = orm.privileges && orm.privileges.attribute && orm.privileges.attribute.view ?
+          orm.privileges.attribute.view : false,
         inclKeys = options.inclKeys,
         superUser = options.superUser,
         c,
@@ -1538,8 +1538,8 @@
           prop = props[i];
 
           /* Remove unprivileged attribute if applicable */
-          if (!superUser && readPriv && readPriv.indexOf(prop.name) != -1 &&
-              !checkPrivilege(readPriv.privilege)) {
+          if (!superUser && viewPriv && viewPriv.indexOf(prop.name) != -1 &&
+              !checkPrivilege(viewPriv.privilege)) {
             delete item[prop.name];
           }
 
