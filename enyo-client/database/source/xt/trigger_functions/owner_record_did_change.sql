690243e92b09bf5a6bb88d1a86903f47e390ba36
John Rogelstad <john@xtuple.com>
2013-06-11 21:57:47 -0400

    issue #18711: What was I thinking...

--- enyo-client/database/source/xt/trigger_functions/owner_record_did_change.sql
+++ enyo-client/database/source/xt/trigger_functions/owner_record_did_change.sql
@@ -2,12 +2,13 @@
 /* Copyright (c) 1999-2013 by OpenMFG LLC, d/b/a xTuple.
    See www.xm.ple.com/CPAL for the full text of the software license. */
 
- var sql;
- 
+ var id = TG_TABLE_NAME + "_id",
+   sql;
+
  /* Set server side default enforcement if no privilege for owner */
  if (TG_OP === 'INSERT' && !XT.Data.checkPrivilege("editOwner")) {
-   sql = "update " + TG_TABLE_SCHEMA + "." + TG_TABLE_NAME + " set " +
-         "owner = geteffectivextuser() where id = " + NEW.id;
+   sql = "update " + TG_TABLE_SCHEMA + "." + TG_TABLE_NAME + " set " + TG_TABLE_NAME +
+         "_owner_username = geteffectivextuser() where " + id + " = " + NEW[id];
    plv8.execute(sql);
  }
 
