326edd0e1da9c1b0c7ca708e988c9f3b29eb0e41
John Rogelstad <john@xtuple.com>
2013-06-13 15:37:57 -0400

    Do not update the price unless the user responds affirmatively to do so.

--- enyo-client/application/source/models/sales_order_base.js
+++ enyo-client/application/source/models/sales_order_base.js
@@ -1101,7 +1101,7 @@ white:true*/
       var settings = XT.session.settings,
         privileges = XT.session.privileges;
       this.taxDetail = [];
-      this._updatePrice = true; // TODO: This probably is un-needed.
+      this._updatePrice = true;
       this._unitIsFractional = false;
 
       //  Disable the Discount Percent stuff if we don't allow them
@@ -1256,8 +1256,8 @@ white:true*/
           } else if (updatePolicy !== K.ALWAYS_UPDATE) {
             this.notify("_updatePrice?".loc(), {
               type: K.QUESTION,
-              callback: function (answer) {
-                that._updatePrice = answer;
+              callback: function (response) {
+                that._updatePrice = response.answer;
                 _calculatePrice(that);
               }
             });
