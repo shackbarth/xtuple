8b19f2eb8155ccdbb0addbb03488812a53e3745b
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-06-18 16:08:24 -0400

    issue #20601: extension versions to display in about popup

--- enyo-client/extensions/source/crm/client/core.js
+++ enyo-client/extensions/source/crm/client/core.js
@@ -1,11 +1,15 @@
-/*jshint indent:2, curly:true eqeqeq:true, immed:true, latedef:true,
-newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true
+/*jshint indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
+newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true,
 white:true*/
 /*global XT:true, XM:true, Backbone:true, _:true, console:true */
 
 (function () {
   "use strict";
 
-  XT.extensions.crm = {};
+  XT.extensions.crm = {
+    setVersion: function () {
+      XT.setVersion("1.3.8", "crm");
+    }
+  };
 
 }());
