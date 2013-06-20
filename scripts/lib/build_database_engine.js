07b5fb9ff8d6d25cbb5ab168a02efcf9c311d388
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-06-20 15:34:49 -0400

    issue #20524: first pass at loading scripts

--- scripts/lib/build_database_engine.js
+++ scripts/lib/build_database_engine.js
@@ -35,29 +35,71 @@ var _ = require('underscore'),
         database: 'dev2' }
   */
   exports.buildDatabase = function (specs, creds) {
+
     //winston.info("Building with specs", JSON.stringify(specs));
+    console.log(creds);
     console.log(specs);
+
+
+
+
     _.each(specs, function (extensions, databaseName) {
+      var errorInDb = false;
+
+      winston.info("Installing on database", databaseName);
+      creds.database = databaseName;
+      var pgClient = new pg.Client(creds);
+      pgClient.connect();
+      // TODO: begin transaction
+      // queries are queued and executed one after another once the connection becomes available
+
+      // TODO: we probably need to use async at every level here to "know" when we're totally done
       _.each(extensions, function (extension) {
-        var manifestFilename = path.join(extension, "database/source/manifest.js"),
+        if (errorInDb) {
+          winston.error("Not installing extension", extension, "due to earlier error in db", databaseName);
+          return;
+        }
+        winston.info("Installing extension", extension);
+        var dbSourceRoot = path.join(extension, "database/source"),
+          manifestFilename = path.join(dbSourceRoot, "manifest.js"),
           manifestString,
           manifest;
 
         if (!fs.existsSync(manifestFilename)) {
           winston.error("Cannot find manifest", manifestFilename);
-          process.exit(1);
+          errorInDb = true;
+          return;
         }
         manifestString = fs.readFileSync(manifestFilename, "utf8");
         try {
           manifest = JSON.parse(manifestString);
         } catch (error) {
           winston.error("Manifest is not valid JSON", manifestFilename);
-          process.exit(1);
+          errorInDb = true;
+          return;
         }
-        console.log(manifest);
 
 
+        var installFile = function (filename, callback) {
+          var fileContents = fs.readFileSync(path.join(dbSourceRoot, filename), "utf8");
+          var result = pgClient.query(fileContents, function (err, res) {
+            callback(err, res);
+          });
+        };
 
+        async.map(manifest.databaseFiles, installFile, function (err, res) {
+          if (err) {
+            winston.error("Error installing file", err);
+            // TODO: rollback
+            pgClient.end();
+            errorInDb = true;
+            return;
+          }
+          // TODO: commit
+          winston.info(databaseName, extension, "files installed successfully");
+          pgClient.end();
+
+        });
 
       });
     });
