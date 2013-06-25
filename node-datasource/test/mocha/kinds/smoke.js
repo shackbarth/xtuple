53756a8ad590b21136d05eac8a671b75c5db2d84
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-06-25 00:12:21 -0400

    got sales order smoke test working

--- node-datasource/test/mocha/kinds/smoke.js
+++ node-datasource/test/mocha/kinds/smoke.js
@@ -1,7 +1,8 @@
 /*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
   immed:true, eqeqeq:true, forin:true, latedef:true,
   newcap:true, noarg:true, undef:true */
-/*global XT:true, XM:true, XV:true, describe:true, it:true, before:true, module:true, require:true, console:true */
+/*global XT:true, XM:true, XV:true, describe:true, it:true,
+  before:true, module:true, require:true */
 
 (function () {
   "use strict";
@@ -13,8 +14,11 @@
     assert = require("chai").assert;
 
   describe('Smoke test', function () {
-    this.timeout(45 * 1000);
+    this.timeout(30 * 1000);
 
+    //
+    // We'll want to have TTOYS and BTRUCK/WH1 onhand and ready for the test to work.
+    //
     before(function (done) {
       zombieAuth.loadApp(function () {
         customerModel = new XM.CustomerProspectRelation();
@@ -23,7 +27,8 @@
             var itemSiteCollection = new XM.ItemSiteRelationCollection();
             itemSiteCollection.fetch({success: function () {
               itemSiteModel = _.find(itemSiteCollection.models, function (model) {
-                return model.getValue("item.number") === "BPAINT1";
+                return model.getValue("item.number") === "BTRUCK1" &&
+                  model.getValue("site.code") === "WH1";
               });
               done();
             }});
@@ -70,24 +75,31 @@
         assert.equal(workspace.value.recordType, "XM.SalesOrder");
 
         //
-        // Set the values from the workspace widgets
+        // Set the customer from the appropriate workspace widget
         //
         workspace.$.customerProspectWidget.doValueChange({value: customerModel});
         assert.equal(workspace.value.getValue("customer.number"), "TTOYS");
         assert.equal(workspace.value.get("shiptoCity"), "Walnut Hills");
         workspace.$.salesOrderLineItemBox.newItem();
 
+        // In sales order, setting the line item fields will set off a series
+        // of asynchronous calls. Once the "total" field is computed, we
+        // know that the workspace is ready to save.
+        workspace.value.on("change:total", function () {
+          workspace.save({success: function (model, resp, options) {
+            assert.equal(model.get("total"), 58.84);
+            assert.equal(model.getStatus(), XM.Model.READY_CLEAN);
+            done();
+          }});
+        });
+
+        //
+        // Set the line item fields
+        //
         var lineItemEditor = workspace.$.salesOrderLineItemBox.$.editor;
         lineItemEditor.$.itemSiteWidget.doValueChange({value: itemSiteModel});
         lineItemEditor.$.quantityWidget.doValueChange({value: 5});
-
-        workspace.save({success: function (model, resp, options) {
-          console.log("here!");
-
-          done();
-        }});
       });
     });
   });
-
 }());
