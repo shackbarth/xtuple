3e7b317f73b66c0ac94aa7fb74dba1ecb4cd9217
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-06-26 21:31:28 -0400

    issue #20715: start crm dashboard

--- enyo-client/extensions/source/crm/client/postbooks.js
+++ enyo-client/extensions/source/crm/client/postbooks.js
@@ -1,6 +1,6 @@
-/*jshint bitwise:true, indent:2, curly:true eqeqeq:true, immed:true,
+/*jshint bitwise:true, indent:2, curly:true, eqeqeq:true, immed:true,
 latedef:true, newcap:true, noarg:true, regexp:true, undef:true,
-trailing:true white:true*/
+trailing:true, white:true*/
 /*global XT:true, XV:true, XM:true, enyo:true*/
 
 (function () {
@@ -51,6 +51,7 @@ trailing:true white:true*/
       name: "crm",
       label: "_crm".loc(),
       panels: [
+        {name: "dashboard", kind: "XV.CrmDashboard"},
         {name: "accountList", kind: "XV.AccountList"},
         {name: "contactList", kind: "XV.ContactList"},
         {name: "toDoList", kind: "XV.ToDoList"},
