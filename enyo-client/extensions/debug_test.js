6515366e4e887d726faf9c6cd7754e339fbb3374
Linda Nichols <lnichols@xtuple.com>
2013-06-28 14:16:56 -0400

    removed debug code

--- enyo-client/extensions/debug_test.js
+++
@@ -1,6 +0,0 @@
-enyo.depends(
-  '/dev/core-extensions/source/project/client/package.js',
-  '/dev/core-extensions/source/crm/client/package.js',
-  '/dev/core-extensions/source/sales/client/package.js',
-  '/dev/private-extensions/source/incident_plus/client/package.js'
-);
