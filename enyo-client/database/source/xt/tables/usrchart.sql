d93c19948bd6e3b307975dd31311f8b104ff2259
Linda Nichols <lnichols@xtuple.com>
2013-07-11 16:25:13 -0400

    Issue #20715: More bug fixes with models.

--- enyo-client/database/source/xt/tables/usrchart.sql
+++ enyo-client/database/source/xt/tables/usrchart.sql
@@ -7,6 +7,7 @@ select xt.add_column('usrchart','usrchart_chart', 'text');
 select xt.add_column('usrchart','usrchart_ext_name', 'text');
 select xt.add_column('usrchart','usrchart_filter_option', 'text');
 select xt.add_column('usrchart','usrchart_groupby_option', 'text');
+select xt.add_column('usrchart','usrchart_order', 'integer');
 
 
 comment on table xt.usrchart is 'Charts users have selected for dashboard';
