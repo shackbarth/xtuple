ca6ad47979eb88c1b2c1d18a68473d6e32a09fd4
Linda Nichols <lnichols@xtuple.com>
2013-07-16 10:33:01 -0400

    Remove the q at the beginning of the file

--- enyo-client/application/source/views/workspace.js
+++ enyo-client/application/source/views/workspace.js
@@ -1,4 +1,4 @@
-q/*jshint bitwise:false, indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
+/*jshint bitwise:false, indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
 newcap:true, noarg:true, regexp:true, undef:true, trailing:true, white:true*/
 /*global XV:true, XM:true, _:true, Backbone:true, enyo:true, XT:true, window:true */
 
