66c071b54adc28313fd7f613c400e06264a354d8
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-07-31 14:58:34 -0400

    moved some more simple tests

---
+++ test/mocha/models/opportunity_source.js
@@ -0,0 +1,28 @@
+/*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
+  immed:true, eqeqeq:true, forin:true, latedef:true,
+  newcap:true, noarg:true, undef:true */
+/*global XT:true, XM:true, XV:true, exports:true, describe:true, it:true, require:true */
+
+(function () {
+  "use strict";
+
+  var crud = require('../lib/crud'),
+    data = {
+      recordType : "XM.OpportunitySource",
+      autoTestAttributes : true,
+      createHash : {
+        name: 'Agent' + Math.random(),
+        description: 'Sales Agent'
+      },
+      updateHash : {
+        description : 'Update Description'
+      }
+    };
+
+  describe('OpportunitySource CRUD Test', function () {
+    this.timeout(20 * 1000);
+    it('should perform all the crud operations', function (done) {
+      crud.runAllCrud(data, done);
+    });
+  });
+}());
---
+++ test/mocha/models/opportunity_stage.js
@@ -0,0 +1,29 @@
+/*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
+  immed:true, eqeqeq:true, forin:true, latedef:true,
+  newcap:true, noarg:true, undef:true */
+/*global XT:true, XM:true, XV:true, exports:true, describe:true, it:true, require:true */
+
+(function () {
+  "use strict";
+
+  var crud = require('../lib/crud'),
+    data = {
+      recordType : "XM.OpportunityStage",
+      autoTestAttributes : true,
+      createHash : {
+        name: 'Stage' + Math.random(),
+        description: 'Description',
+        deactivate: true
+      },
+      updateHash : {
+        description : 'Update Description'
+      }
+    };
+
+  describe('OpportunityStage CRUD Test', function () {
+    this.timeout(20 * 1000);
+    it('should perform all the crud operations', function (done) {
+      crud.runAllCrud(data, done);
+    });
+  });
+}());
