f143f678a3f813762ad0c4334170ae1a5681ada3
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-08-06 14:23:46 -0400

    issue #20983: test model idAttributes

--- test/mocha/models/model.js
+++ test/mocha/models/model.js
@@ -1,31 +1,61 @@
 /*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
   immed:true, eqeqeq:true, forin:true, latedef:true,
   newcap:true, noarg:true, undef:true */
-/*global XT:true, XM:true, XV:true, process:true, module:true, require:true */
+/*global describe:true, it:true, XT:true, XM:true, XV:true, process:true, module:true, require:true */
 
 (function () {
   "use strict";
 
   var zombieAuth = require("../lib/zombie_auth"),
+    _ = require("underscore"),
     assert = require("chai").assert;
+
+  describe('Model integrity', function () {
+    this.timeout(20 * 1000);
+    it('should log in first', function (done) {
+      zombieAuth.loadApp(done);
+    });
+
+    it('should contain its idAttribute', function () {
+      _.each(XM, function (Klass, key) {
+        var model;
+        if (typeof Klass === 'function') {
+          if (!Klass.prototype.recordType) {
+            // guard against exceptions
+            return;
+          }
+          model = new Klass();
+          if (model instanceof XM.Model && !model instanceof XM.Settings) {
+            assert.include(model.getAttributeNames(), model.idAttribute, key + " does not contain its idAttribute");
+          }
+          /*
+          if (model instanceof XM.Document) {
+            if (model.getAttributeNames().indexOf(model.nameAttribute) < 0) {
+              console.log(key);
+            }
+
+            //assert.include(model.getAttributeNames(), model.nameAttribute, key + " does not contain its nameAttribute");
+          }
+          */
+        }
+      });
+    });
+  });
 
   describe('Model read-only recursion', function () {
     this.timeout(20 * 1000);
-    it('should know to stop if an attribute is null', function (done) {
-      var testReadOnly = function () {
-        var model, status;
+    it('should log in first', function (done) {
+      zombieAuth.loadApp(done);
+    });
 
-        model = new XM.QuoteLine();
-        model.set("itemSite", null);
+    it('should know to stop if an attribute is null', function () {
+      var model, status;
 
-        status = model.isReadOnly("itemSite.site.standardCost");
-        assert.isNull(status);
+      model = new XM.QuoteLine();
+      model.set("itemSite", null);
 
-        done();
-      };
-
-
-      zombieAuth.loadApp(testReadOnly);
+      status = model.isReadOnly("itemSite.site.standardCost");
+      assert.isNull(status);
     });
   });
 }());
