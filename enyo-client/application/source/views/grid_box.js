4d4477ac74c9737780e959b2befb6588c0fdafda
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-08-15 13:12:00 -0400

    issue #21099: replace unit pickers with comboboxes in grid entry

--- enyo-client/application/source/views/grid_box.js
+++ enyo-client/application/source/views/grid_box.js
@@ -107,7 +107,7 @@ Globalize:true */
       ]},
       {classes: "xv-grid-column quantity", components: [
         {kind: "XV.QuantityWidget", attr: "quantity", name: "quantityWidget"},
-        {kind: "XV.UnitPickr", attr: "quantityUnit", name: "quantityUnitPicker" }
+        {kind: "XV.UnitCombobox", attr: "quantityUnit", name: "quantityUnitPicker" }
       ]},
       {classes: "xv-grid-column discount", components: [
         {kind: "XV.PercentWidget", name: "discount", attr: "discount" }
@@ -116,7 +116,7 @@ Globalize:true */
         {kind: "XV.MoneyWidget", attr:
           {localValue: "price", currency: ""},
           currencyDisabled: true, currencyShowing: false, scale: XT.SALES_PRICE_SCALE},
-        {kind: "XV.UnitPickr", attr: "priceUnit", name: "priceUnitPicker"},
+        {kind: "XV.UnitCombobox", attr: "priceUnit", name: "priceUnitPicker"},
         {kind: "XV.MoneyWidget", attr:
           {localValue: "extendedPrice", currency: ""},
           currencyDisabled: true, currencyShowing: false, scale: XT.EXTENDED_PRICE_SCALE}
