7a4a1b444f85e57f3cdfebdeaaeb84ad0323a5b6
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-08-20 11:50:16 -0400

    issue #21038: define our own component array

--- enyo-client/extensions/source/inventory/client/views/enter_receipt.js
+++ enyo-client/extensions/source/inventory/client/views/enter_receipt.js
@@ -1,7 +1,8 @@
 /*jshint bitwise:true, indent:2, curly:true, eqeqeq:true, immed:true,
 latedef:true, newcap:true, noarg:true, regexp:true, undef:true,
 trailing:true, white:true*/
-/*global XT:true, XV:true, XM:true, _:true, enyo:true*/
+/*global XT:true, XV:true, XM:true, _:true, enyo:true, console:true */
+// TODO: remove console:true
 
 (function () {
 
@@ -17,7 +18,52 @@ trailing:true, white:true*/
       published: {keyAttribute: null},
       handlers: {
         onListItemMenuTap: "showListItemMenu"
-      }, 
+      },
+      components: [
+        {name: "parameterPanel", kind: "FittableRows", classes: "left",
+          components: [
+          {kind: "onyx.Toolbar", classes: "onyx-menu-toolbar", components: [
+            {kind: "onyx.Button", name: "backButton", content: "_back".loc(),
+              ontap: "close"},
+            {
+              kind: "onyx.MenuDecorator",
+              components: [
+                {kind: "XV.IconButton", src: "/assets/menu-icon-gear.png",
+                  content: "_actions".loc(), name: "headerActionButton"},
+                {kind: "onyx.Menu",
+                    ontap: "headerActionSelected",
+                    name: "headerActionMenu", components: [
+                  {kind: "XV.MenuItem",
+                    content: "receiveAll".loc(),
+                    name: "receiveAll",
+                    prerequisite: "canReceiveAll",
+                    method: "doReceiveAll",
+                    notifyMessage: "_receiveAll?",
+                    modelName: "XM.PurchaseOrderLine"
+                  }
+                ]}
+              ]
+            }
+          ]},
+          {name: "leftTitle", content: "_advancedSearch".loc(), classes: "xv-parameter-title"},
+          {kind: "Scroller", name: "parameterScroller", fit: true},
+        ]},
+        {name: "listPanel", kind: "FittableRows", components: [
+          // the onyx-menu-toolbar class keeps the popups from being hidden
+          {kind: "onyx.MoreToolbar", name: "contentToolbar",
+            classes: "onyx-menu-toolbar", movedClass: "xv-toolbar-moved", components: [
+            {kind: "onyx.Grabber", classes: "left-float"},
+            {name: "rightLabel", content: "_search".loc(), classes: "left-float"},
+            {name: "search", kind: "onyx.InputDecorator", classes: "right-float",
+              components: [
+              {name: 'searchInput', kind: "onyx.Input", style: "width: 200px;",
+                placeholder: "_search".loc(), onchange: "requery"},
+              {kind: "Image", src: "/assets/search-input-search.png"}
+            ]}
+          ]},
+          {name: "contentPanels", kind: "Panels", margin: 0, fit: true, draggable: false, panelCount: 0}
+        ]}
+      ],
       requery: function (inSender, inEvent) {
         this.inherited(arguments);
         var key = inEvent.originator.getParameter().value.id;
@@ -25,6 +71,7 @@ trailing:true, white:true*/
       create: function () {
         this.inherited(arguments);
         this.setList({list: "XV.EnterReceiptList"});
+        /*
         this.$.listPanel.createComponent({
           name: "listItemMenu",
           kind: "onyx.Menu",
@@ -34,29 +81,7 @@ trailing:true, white:true*/
           components: [],
           owner: this
         });
-        this.$.toolbar.createComponent({
-          name: "headerAction",
-          kind: "onyx.MenuDecorator",
-          style: "margin: 0;", 
-          ontap: "headerActionSelected",
-          components: [
-            {kind: "XV.IconButton", src: "/assets/menu-icon-gear.png", content: "_actions".loc(), name: "headerActionButton"},
-            {kind: "onyx.Menu", name: "headerActionMenu", components: [
-              {kind: "XV.MenuItem", 
-                content: "receiveAll".loc(),
-                name: "receiveAll",
-                prerequisite: "canReceiveAll",
-                method: "doReceiveAll",
-                notifyMessage: "_receiveAll?",
-                modelName: "XM.PurchaseOrderLine"
-              }
-            ]}
-          ],
-          headerActionSelected: function (inSender, inEvent) {
-            alert("header action selected");
-            console.log("header action selected");
-          }
-        }); 
+        */
       },
       /**
       @todo Document the itemTap method.
@@ -85,17 +110,16 @@ trailing:true, white:true*/
         });
       },*/
       listActionSelected: function (inSender, inEvent) {
-        alert("List Action Selected!");
-        console.log("header action selected");
+        console.log("list action selected");
       },
       headerActionSelected: function (inSender, inEvent) {
-        alert("Header Action Selected!");
+        console.log("header action selected");
       }
     };
 
     enyo.mixin(enterReceipt, XV.ListMenuManagerMixin);
     enyo.kind(enterReceipt);
 
-    };
+  };
 
 }());
