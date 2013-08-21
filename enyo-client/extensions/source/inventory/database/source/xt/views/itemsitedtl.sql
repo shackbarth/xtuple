4cbbdf70a4c38c1905353afd2233f2b5e4179dc9
John Rogelstad <john@xtuple.com>
2013-08-21 13:21:07 -0400

    issue #20734: Have to have detail nested to ensure it is always fresh.

--- enyo-client/extensions/source/inventory/database/source/xt/views/itemsitedtl.sql
+++ enyo-client/extensions/source/inventory/database/source/xt/views/itemsitedtl.sql
@@ -1,6 +1,5 @@
 select xt.create_view('xt.itemsitedtl', $$
    select itemloc.*,  
-     itemsite.obj_uuid as itemsite_uuid,
      0 as distributed
    from itemloc
      join itemsite on itemsite_id = itemloc_itemsite_id;
