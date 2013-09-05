9be6d13d12df9d1c26f687d16f0caf07a3650598
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-09-05 16:28:21 -0400

    issue #21251: generalize

--- enyo-client/application/source/app.js
+++ enyo-client/application/source/app.js
@@ -8,11 +8,17 @@ white:true*/
   enyo.kind({
     name: "App",
     kind: "XV.App",
+    keyCapturePatterns: [
+      {method: "captureMagstripe", start: [16, 53, 16, 66], end: [191, 13]}
+    ],
     components: [
       { name: "postbooks", kind: "XV.Postbooks",  onTransitionStart: "handlePullout" },
       { name: "pullout", kind: "XV.Pullout", onAnimateFinish: "pulloutAnimateFinish" },
       { name: "signals", kind: enyo.Signals, onkeydown: "handleKeyDown" }
-    ]
+    ],
+    captureMagstripe: function (input) {
+      console.log("deal with ", input);
+    }
   });
 
 }());
