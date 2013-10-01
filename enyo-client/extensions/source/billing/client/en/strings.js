989f54599b7a3059ef8181528d2d4bf9762da882
tjwebb <travis@traviswebb.com>
2013-10-01 17:17:21 -0400

    initial commit of billing extension, with sales_category

---
+++ enyo-client/extensions/source/billing/client/en/strings.js
@@ -0,0 +1,16 @@
+// ==========================================================================
+// Project:   XT` Strings
+// Copyright: ©2011 OpenMFG LLC, d/b/a xTuple
+// ==========================================================================
+/*globals XT */
+
+var lang = XT.stringsFor("en_US", {
+
+  // ********
+  // Labels
+  // ********
+
+  "_billing": "Billing",
+  "_billingDescription": "Corporate Relationship Management",
+  "_salesCategory": "Sales Category"
+});
---
+++ enyo-client/extensions/source/billing/client/models/settings.js
@@ -0,0 +1,24 @@
+/*jshint indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
+newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true,
+white:true*/
+/*global XT:true, XM:true, Backbone:true, _:true, console:true */
+
+(function () {
+  "use strict";
+
+  XT.extensions.billing.initSettings = function () {
+    /**
+     * @class XM.Billing
+     * @extends XM.Settings
+     */
+    XM.Billing = XM.Settings.extend(
+      /** @scope XM.Billing.Settings.prototype */ {
+
+      recordType: 'XM.Billing',
+      privileges: 'ConfigureBilling',
+    });
+    
+    XM.billing = new XM.Billing();
+  };
+})();
+
