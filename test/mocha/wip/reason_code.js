20099bfff60955fadafe0bba6f4bf88d4d5d3b54
Linda Nichols <lynnaloo@gmail.com>
2013-10-02 19:46:26 +0000

    start to reason code

---
+++ test/mocha/wip/reason_code.js
@@ -0,0 +1,23 @@
+/*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
+  immed:true, eqeqeq:true, forin:true, latedef:true,
+  newcap:true, noarg:true, undef:true */
+/*global describe:true, it:true, XT:true, XM:true, XV:true, process:true, module:true, require:true */
+
+(function () {
+  "use strict";
+
+  var crud = require('../lib/crud'),
+    data = {
+          recordType: "XM.ReasonCode",
+          autoTestAttributes: true,
+          createHash: {
+            name: "" + Math.random(),
+          },
+          updateHash: {
+          }
+        };
+
+  describe('Reason Code CRUD Test', function () {
+    crud.runAllCrud(data);
+  });
+}());
