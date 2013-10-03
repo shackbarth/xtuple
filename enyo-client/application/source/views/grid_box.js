dbaba50dc0a14716fdae25a92633820cc6513b15
John Rogelstad <john@xtuple.com>
2013-10-03 13:02:53 -0400

    Tweak layout of SO grid and locale usage.

--- enyo-client/application/source/views/grid_box.js
+++ enyo-client/application/source/views/grid_box.js
@@ -1,7 +1,6 @@
 /*jshint bitwise:false, indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
-newcap:true, noarg:true, regexp:true, undef:true, trailing:true, white:true*/
-/*global XV:true, XM:true, _:true, Backbone:true, enyo:true, XT:true, window:true,
-Globalize:true */
+newcap:true, noarg:true, regexp:true, undef:true, trailing:true, white:true, strict: false*/
+/*global XV:true, _:true, enyo:true, XT:true, Globalize:true */
 
 (function () {
 
