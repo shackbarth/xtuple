c69ea29b4698fe83a940c232e7bc2770655cb3fb
julesstp <julianmcarlin@gmail.com>
2013-10-14 14:57:59 -0400

    New trans base kinds stable and working in inventory and mfg

--- enyo-client/extensions/source/inventory/client/models/inventory.js
+++ enyo-client/extensions/source/inventory/client/models/inventory.js
@@ -41,7 +41,7 @@ white:true*/
 
       quantityAttribute: "toIssue",
 
-      issueMethod: "issueToShipping",
+      issueMethod: "issueItem",
 
       readOnlyAttributes: [
         "atShipping",
@@ -70,7 +70,7 @@ white:true*/
         this.on("change:toIssue", this.toIssueDidChange);
       },
 
-      canIssueStock: function (callback) {
+      canIssueItem: function (callback) {
         var isShipped = this.getValue("shipment.isShipped") || false,
           hasPrivilege = XT.session.privileges.get("IssueStockToShipping");
         if (callback) {
@@ -79,7 +79,7 @@ white:true*/
         return this;
       },
 
-      canReturnStock: function (callback) {
+      canReturnItem: function (callback) {
         var isShipped = this.getValue("shipment.isShipped") || false,
           hasPrivilege = XT.session.privileges.get("ReturnStockFromShipping"),
           atShipping = this.get("atShipping");
@@ -156,7 +156,7 @@ white:true*/
       @params {Array} Data
       @params {Object} Options
     */
-    XM.Inventory.issueToShipping = function (params, options) {
+    XM.Inventory.issueItem = function (params, options) {
       var obj = XM.Model.prototype;
       obj.dispatch("XM.Inventory", "issueToShipping", params, options);
     };
@@ -167,7 +167,7 @@ white:true*/
       @params {Array} Array of model ids
       @params {Object} Options
     */
-    XM.Inventory.returnFromShipping = function (params, options) {
+    XM.Inventory.returnItem = function (params, options) {
       var obj = XM.Model.prototype;
       obj.dispatch("XM.Inventory", "returnFromShipping", params, options);
     };
--- enyo-client/extensions/source/inventory/client/views/transaction_list.js
+++ enyo-client/extensions/source/inventory/client/views/transaction_list.js
@@ -1,60 +1,271 @@
 /*jshint bitwise:true, indent:2, curly:true, eqeqeq:true, immed:true,
 latedef:true, newcap:true, noarg:true, regexp:true, undef:true,
 trailing:true, white:true, strict:false*/
-/*global XT:true, XM:true, _:true, enyo:true */
+/*global XM:true, _:true, XT:true, XV:true, enyo:true, Globalize:true*/
 
 (function () {
 
   XT.extensions.inventory.initTransactionList = function () {
 
+    // ..........................................................
+    // ISSUE TO SHIPPING
+    //
+
     enyo.kind({
-      name: "XV.IssueToShipping",
+      name: "XV.IssueToShippingList",
       kind: "XV.TransactionList",
-      prerequisite: "canIssueStock",
-      notifyMessage: "_issueAll?".loc(),
-      list: "XV.IssueToShippingListContainer",
-      actions: [
-        {name: "issueAll", label: "_issueAll".loc(),
-          prerequisite: "canIssueStock" }
+      label: "_issueToShipping".loc(),
+      collection: "XM.IssueToShippingCollection",
+      parameterWidget: "XV.IssueToShippingParameters",
+      query: {orderBy: [
+        {attribute: "lineNumber"},
+        {attribute: "subNumber"}
+      ]},
+      published: {
+        shipment: null,
+        transModule: XM.Inventory,
+        transWorkspace: "XV.IssueStockWorkspace"
+      },
+      components: [
+        {kind: "XV.ListItem", components: [
+          {kind: "FittableColumns", components: [
+            {kind: "XV.ListColumn", classes: "first", components: [
+              {kind: "FittableColumns", components: [
+                {kind: "XV.ListAttr", attr: "lineNumber"},
+                {kind: "XV.ListAttr", attr: "itemSite.site.code",
+                  classes: "right"},
+                {kind: "XV.ListAttr", attr: "itemSite.item.number", fit: true}
+              ]},
+              {kind: "XV.ListAttr", attr: "itemSite.item.description1",
+                fit: true,  style: "text-indent: 18px;"}
+            ]},
+            {kind: "XV.ListColumn", components: [
+              {kind: "XV.ListAttr", attr: "unit.name", style: "text-align: right"}
+            ]},
+            {kind: "XV.ListColumn", classes: "money", components: [
+              {kind: "XV.ListAttr", attr: "ordered",
+                formatter: "formatQuantity", style: "text-align: right"}
+            ]},
+            {kind: "XV.ListColumn", classes: "money", components: [
+              {kind: "XV.ListAttr", attr: "balance",
+                formatter: "formatQuantity", style: "text-align: right"}
+            ]},
+            {kind: "XV.ListColumn", classes: "money", components: [
+              {kind: "XV.ListAttr", attr: "atShipping",
+                formatter: "formatQuantity", style: "text-align: right"}
+            ]},
+            {kind: "XV.ListColumn", classes: "money", components: [
+              {kind: "XV.ListAttr", attr: "scheduleDate",
+                formatter: "formatScheduleDate", style: "text-align: right"}
+            ]}
+          ]}
+        ]}
       ],
-      handlers: {
-        onShipmentChanged: "shipmentChanged"
-      },
-      canIssueStock: function () {
-        var hasPrivilege = XT.session.privileges.get("IssueStockToShipping"),
-          model = this.getModel(),
-          validModel = _.isObject(model) ? !model.get("isShipped") : false,
-          hasOpenLines = this.$.list.value.length;
-        return hasPrivilege && validModel && hasOpenLines;
-      },
-      create: function () {
+      fetch: function () {
+        this.setShipment(null);
         this.inherited(arguments);
-        var button = this.$.postButton;
-        button.setContent("_ship".loc());
-        button.setShowing(true);
+      },
+      formatScheduleDate: function (value, view, model) {
+        var today = new Date(),
+          isLate = XT.date.compareDate(value, today) < 1 &&
+            model.get("balance") > 0;
+        view.addRemoveClass("error", isLate);
+        return value;
+      },
+      formatLineNumber: function (value, view, model) {
+        var lineNumber = model.get("lineNumber"),
+          subnumber = model.get("subNumber");
+        if (subnumber === 0) {
+          value = lineNumber;
+        } else {
+          value = lineNumber + "." + subnumber;
+        }
+        return value;
+      },
+      formatQuantity: function (value) {
+        var scale = XT.locale.quantityScale;
+        return Globalize.format(value, "n" + scale);
+      },
+      /**
+        Helper function for transacting `issue` on an array of models
+
+        @param {Array} Models
+        @param {Boolean} Prompt user for confirmation on every model
+      */ /*
+      issue: function (models, prompt, issueStock) {
+        var that = this,
+          i = -1,
+          callback,
+          data = [];
+
+        // Recursively issue everything we can
+        callback = function (workspace) {
+          var model,
+            options = {},
+            toIssue,
+            transDate,
+            params,
+            dispOptions = {},
+            wsOptions = {},
+            wsParams;
+
+          // If argument is false, this whole process was cancelled
+          if (workspace === false) {
+            return;
+
+          // If a workspace brought us here, process the information it obtained
+          } else if (workspace) {
+            model = workspace.getValue();
+            toIssue = model.get("toIssue");
+            transDate = model.transactionDate;
+
+            if (toIssue) {
+              wsOptions.detail = model.formatDetail();
+              wsOptions.asOf = transDate;
+              wsParams = {
+                orderLine: model.id,
+                quantity: toIssue,
+                options: wsOptions
+              };
+              data.push(wsParams);
+            }
+            workspace.doPrevious();
+          }
+
+          i++;
+          // If we've worked through all the models then forward to the server
+          if (i === models.length) {
+            if (data[0]) {
+              that.doProcessingChanged({isProcessing: true});
+              dispOptions.success = function () {
+                that.doProcessingChanged({isProcessing: false});
+              };
+              XM.Inventory.issueToShipping(data, dispOptions);
+            } else {
+              return;
+            }
+
+          // Else if there's something here we can issue, handle it
+          } else {
+            model = models[i];
+            toIssue = model.get("toIssue");
+            transDate = model.transactionDate;
+
+            // See if there's anything to issue here
+            if (toIssue || issueStock) {
+
+              // If prompt or distribution detail required,
+              // open a workspace to handle it
+              if (prompt || model.undistributed()) {
+                that.doWorkspace({
+                  workspace: "XV.IssueStockWorkspace",
+                  id: model.id,
+                  callback: callback,
+                  allowNew: false,
+                  success: function (model) {
+                    model.transactionDate = transDate;
+                  }
+                });
+
+              // Otherwise just use the data we have
+              } else {
+                options.asOf = transDate;
+                options.detail = model.formatDetail();
+                params = {
+                  orderLine: model.id,
+                  quantity: toIssue,
+                  options: options
+                };
+                data.push(params);
+                callback();
+              }
+
+            // Nothing to issue, move on
+            } else {
+              callback();
+            }
+          }
+        };
+        callback();
       },
       issueAll: function () {
-        this.$.list.issueAll();
+        var models = this.getValue().models;
+        this.issue(models);
       },
-      post: function () {
-        var that = this,
-          shipment = this.$.parameterWidget.$.shipment.getValue(),
-          callback = function (resp) {
-            if (resp) { that.$.parameterWidget.$.order.setValue(null); }
+      issueLine: function () {
+        var models = this.selectedModels();
+        this.issue(models);
+      },
+      issueStock: function () {
+        var models = this.selectedModels();
+        this.issue(models, true, true);
+      },
+      returnStock: function () {
+        var models = this.selectedModels(),
+          that = this,
+          data =  [],
+          options = {},
+          atShipping,
+          model,
+          i;
+
+        for (i = 0; i < models.length; i++) {
+          model = models[i];
+          atShipping = model.get("atShipping");
+
+          // See if there's anything to issue here
+          if (atShipping) {
+            data.push(model.id);
+          }
+        }
+
+        if (data.length) {
+          that.doProcessingChanged({isProcessing: true});
+          options.success = function () {
+            that.doProcessingChanged({isProcessing: false});
           };
-        this.doWorkspace({
-          workspace: "XV.ShipShipmentWorkspace",
-          id: shipment.id,
-          callback: callback
-        });
+          XM.Inventory.returnFromShipping(data, options);
+        }
       },
-      shipmentChanged: function (inSender, inEvent) {
-        var disabled = _.isEmpty(inEvent.shipment) ||
-                       !XT.session.privileges.get("ShipOrders");
-        this.$.parameterWidget.$.shipment.setValue(inEvent.shipment);
-        this.$.postButton.setDisabled(disabled);
+      selectedModels: function () {
+        var collection = this.getValue(),
+          models = [],
+          selected,
+          prop;
+        if (collection.length) {
+          selected = this.getSelection().selected;
+          for (prop in selected) {
+            if (selected.hasOwnProperty(prop)) {
+              models.push(this.getModel(prop - 0));
+            }
+          }
+        }
+        return models;
+      }, */
+      /**
+        Overload: used to keep track of shipment.
+      */
+      setupItem: function (inSender, inEvent) {
+        this.inherited(arguments);
+        var collection = this.getValue(),
+          listShipment = collection.at(inEvent.index).get("shipment"),
+          listShipmentId = listShipment ? listShipment.id : false,
+          shipment = this.getShipment(),
+          shipmentId = shipment ? shipment.id : false;
+        if (listShipmentId !== shipmentId) {
+          this.setShipment(listShipment);
+          // Update all rows to match
+          _.each(collection.models, function (model) {
+            model.set("shipment", listShipment);
+          });
+        }
+      },
+      shipmentChanged: function () {
+        this.doShipmentChanged({shipment: this.getShipment()});
       }
     });
+
+    XV.registerModelList("XM.SalesOrderRelation", "XV.SalesOrderLineListItem");
   };
 
 }());
