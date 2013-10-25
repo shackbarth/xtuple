a83fbeeec8c38ee738f3635de54b5daeea045283
Linda Nichols <lynnaloo@gmail.com>
2013-10-25 20:52:44 +0000

    Issue #21628: Fixed issue with package.js and errors

--- enyo-client/extensions/source/billing/client/models/package.js
+++ enyo-client/extensions/source/billing/client/models/package.js
@@ -1,7 +1,5 @@
 enyo.depends(
   "configure.js",
   "sales_category.js",
-  "receivable.js",
-  "startup.js",
-  "static.js"
+  "receivable.js"
 );
--- enyo-client/extensions/source/billing/client/models/static.js
+++ enyo-client/extensions/source/billing/client/models/static.js
@@ -6,23 +6,19 @@ white:true*/
 (function () {
   "use strict";
 
-  // These are hard coded collections that may be turned into tables at a later date
-  var i, K;
-
   // Receivable Types
-  K = XM.Receivable;
   var receivableTypeJson = [
-    { id: K.INVOICE, name: "_invoice".loc() },
-    { id: K.DEBIT_MEMO, name: "_debitMemo".loc() },
-    { id: K.CREDIT_MEMO, name: "_creditMemo".loc() },
-    { id: K.CUSTOMER_DEPOSIT, name: "_customerDeposit".loc() }
+    { id: XM.Receivable.INVOICE, name: "_invoice".loc() },
+    { id: XM.Receivable.DEBIT_MEMO, name: "_debitMemo".loc() },
+    { id: XM.Receivable.CREDIT_MEMO, name: "_creditMemo".loc() },
+    { id: XM.Receivable.CUSTOMER_DEPOSIT, name: "_customerDeposit".loc() }
   ];
   XM.ReceivableTypeModel = Backbone.Model.extend({});
   XM.ReceivableTypeCollection = Backbone.Collection.extend({
     model: XM.ReceivableTypeModel
   });
   XM.receivableTypes = new XM.ReceivableTypeCollection();
-  for (i = 0; i < receivableTypeJson.length; i++) {
+  for (var i = 0; i < receivableTypeJson.length; i++) {
     var receivableType = new XM.ReceivableTypeModel(receivableTypeJson[i]);
     XM.receivableTypes.add(receivableType);
   }
