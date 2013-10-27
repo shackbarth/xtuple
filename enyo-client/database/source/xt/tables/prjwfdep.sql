754ee9e09070c0d3a546a2d912cc05bab244bb08
John Rogelstad <john@xtuple.com>
2013-10-27 17:43:17 -0400

    issue #21836: Give up trying to rationalize with todoitem. Makes too complicated.

--- enyo-client/database/source/xt/tables/prjwfdep.sql
+++ enyo-client/database/source/xt/tables/prjwfdep.sql
@@ -1,8 +1,8 @@
 select xt.create_table('prjwfdep', 'xt', false, 'xt.wfdep');
 
 select xt.add_constraint('prjwfdep', 'prjwfdep_pkey', 'primary key (wfdep_id)');
-select xt.add_constraint('prjwfdep', 'prjwfdep_wfdep_source_id_fkey', 'foreign key (wfdep_source_id) references xt.prjwf (todoitem_id) on delete cascade');
-select xt.add_constraint('prjwfdep', 'prjwfdep_wfdep_target_id_fkey', 'foreign key (wfdep_target_id) references xt.prjwf (todoitem_id) on delete cascade');
+select xt.add_constraint('prjwfdep', 'prjwfdep_wfdep_source_id_fkey', 'foreign key (wfdep_source_id) references xt.prjwf (wf_id) on delete cascade');
+select xt.add_constraint('prjwfdep', 'prjwfdep_wfdep_target_id_fkey', 'foreign key (wfdep_target_id) references xt.prjwf (wf_id) on delete cascade');
 select xt.add_constraint('prjwfdep','prjwfdep_prjwfdep_source_id_prjwfdep_target_id', 'unique(wfdep_source_id, wfdep_target_id)');
 
 comment on table xt.prjwf is 'Project workflow table';
--- enyo-client/database/source/xt/tables/wf.sql
+++ enyo-client/database/source/xt/tables/wf.sql
@@ -1,5 +1,18 @@
-select xt.create_table('wf');
+select xt.create_table('wf', 'xt', false, 'xt.obj');
 
+select xt.add_column('wf','wf_id', 'serial', 'primary key');
+select xt.add_column('wf','wf_name', 'text');
+select xt.add_column('wf','wf_description', 'text');
+select xt.add_column('wf','wf_status', 'text', 'not null');
+select xt.add_column('wf','wf_active', 'boolean', 'not null default true');
+select xt.add_column('wf','wf_start_date', 'date');
+select xt.add_column('wf','wf_due_date', 'date');
+select xt.add_column('wf','wf_assigned_date', 'date');
+select xt.add_column('wf','wf_completed_date', 'date');
+select xt.add_column('wf','wf_notes', 'text');
+select xt.add_column('wf','wf_priority_id', 'integer', 'not null');
+select xt.add_column('wf','wf_owner_username', 'text');
+select xt.add_column('wf','wf_assigned_username', 'text');
 select xt.add_column('wf','wf_parent_id', 'integer');
 select xt.add_column('wf','wf_parent_status', 'text');
 
