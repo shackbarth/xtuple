06b989ac14b9826ed4d0fda5fb93cc7d0e6e87ad
Linda Nichols <lynnaloo@gmail.com>
2013-10-29 06:07:04 +0000

    Issue #21628: Fixed view

--- enyo-client/extensions/source/billing/database/source/xt/views/aropeninfo.sql
+++ enyo-client/extensions/source/billing/database/source/xt/views/aropeninfo.sql
@@ -4,6 +4,7 @@ select xt.create_view('xt.aropeninfo', $$
 
 $$,false);
 
-create or replace rule "_INSERT" as on insert to xt.invcitemtaxinfo do instead nothing;
-create or replace rule "_DELETE" as on delete to xt.invcheadinfo do instead nothing;
+create or replace rule "_INSERT" as on insert to xt.aropeninfo do instead nothing;
+create or replace rule "_UPDATE" as on delete to xt.aropeninfo do instead nothing;
+create or replace rule "_DELETE" as on delete to xt.aropeninfo do instead nothing;
 
