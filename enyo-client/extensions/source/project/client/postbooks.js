bf12a6d26a1a31e9937bdf309133ac349ad2777d
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-10-31 10:42:03 -0400

    issue #21853: put project dashboard on its own menu item

--- enyo-client/extensions/source/project/client/postbooks.js
+++ enyo-client/extensions/source/project/client/postbooks.js
@@ -6,7 +6,7 @@ trailing:true, white:true*/
 (function () {
 
   XT.extensions.project.initPostbooks = function () {
-    var module, panels, relevantPrivileges;
+    var module, dashboardModule, panels, relevantPrivileges;
 
     // ..........................................................
     // APPLICATION
@@ -23,11 +23,19 @@ trailing:true, white:true*/
 
     XT.app.$.postbooks.appendPanels("setup", panels);
 
+    dashboardModule = {
+      name: "projectDashboard",
+      label: "_dashboard".loc(),
+      panels: [
+        {name: "projectDashboard", kind: "XV.ProjectDashboard"}
+      ]
+    };
+    XT.app.$.postbooks.insertModule(dashboardModule, 0);
+
     module = {
       name: "project",
       label: "_project".loc(),
       panels: [
-        {name: "projectDashboard", kind: "XV.ProjectDashboard"},
         {name: "projectList", kind: "XV.ProjectList"},
         {name: "taskList", kind: "XV.TaskList"}
       ]
