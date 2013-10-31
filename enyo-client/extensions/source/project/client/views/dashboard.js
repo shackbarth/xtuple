1a148ada3387dd53901dbde068a08d721996c356
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-10-30 23:23:36 -0400

    issue #21853: a few looks at project charts

--- enyo-client/extensions/source/project/client/views/dashboard.js
+++ enyo-client/extensions/source/project/client/views/dashboard.js
@@ -1,7 +1,7 @@
 /*jshint bitwise:true, indent:2, curly:true, eqeqeq:true, immed:true,
 latedef:true, newcap:true, noarg:true, regexp:true, undef:true,
 trailing:true, white:true*/
-/*global XT:true, XM:true, XV:true, _:true, window: true, enyo:true, nv:true, d3:true, console:true */
+/*global XT:true, XM:true, XV:true, enyo:true */
 
 (function () {
 
@@ -14,7 +14,10 @@ trailing:true, white:true*/
     // title is what show in the "add chart" picker on the
     // dashboard and the chart is the widget to be added
     newActions: [
-      {name: "projects", label: "_projects".loc(), item: "XV.ProjectBarChart"}
+      {name: "projects", label: "_projects".loc(), item: "XV.ProjectBarChart"},
+      {name: "tasks", label: "_tasks".loc(), item: "XV.TaskBarChart"},
+      {name: "taskDueDates", label: "_task".loc() + " " + "_dueDate".loc(), item: "XV.TaskTimeSeriesChart"},
+      {name: "projectHours", label: "_hours".loc(), item: "XV.ProjectHoursBarChart"}
     ]
   });
 }());
--- enyo-client/extensions/source/project/client/widgets/chart.js
+++ enyo-client/extensions/source/project/client/widgets/chart.js
@@ -6,15 +6,57 @@ trailing:true, white:true*/
 (function () {
 
   enyo.kind({
+    name: "XV.ProjectHoursBarChart",
+    kind: "XV.DrilldownBarChart",
+    collection: "XM.ProjectListItemCollection",
+    chartTitle: "_projects".loc(),
+    groupByOptions: [
+      { name: "status", content: "_status".loc() },
+      { name: "account", content: "_account".loc() },
+      { name: "owner" },
+      { name: "assignedTo" }
+    ],
+    totalField: "actualHours"
+  });
+
+  enyo.kind({
     name: "XV.ProjectBarChart",
     kind: "XV.DrilldownBarChart",
     collection: "XM.ProjectListItemCollection",
     chartTitle: "_projects".loc(),
     groupByOptions: [
-      { name: "projectStatus", content: "_status".loc() },
-      { name: "projectType", content: "_type".loc() },
+      { name: "status" },
+      { name: "account" },
       { name: "owner" },
       { name: "assignedTo" }
     ]
+  });
+
+  enyo.kind({
+    name: "XV.TaskBarChart",
+    kind: "XV.DrilldownBarChart",
+    collection: "XM.TaskListItemCollection",
+    chartTitle: "_tasks".loc(),
+    groupByOptions: [
+      { name: "status" },
+      { name: "project" },
+      { name: "owner" },
+      { name: "assignedTo" }
+    ]
+  });
+
+  enyo.kind({
+    name: "XV.TaskTimeSeriesChart",
+    kind: "XV.TimeSeriesChart",
+    collection: "XM.TaskListItemCollection",
+    chartTitle: "_tasks".loc() + " " + "_dueDate".loc(),
+    groupByOptions: [
+      { name: "status" },
+      { name: "project" },
+      { name: "owner" },
+      { name: "assignedTo" }
+    ],
+    dateField: "dueDate",
+    totalField: "actualHours"
   });
 }());
