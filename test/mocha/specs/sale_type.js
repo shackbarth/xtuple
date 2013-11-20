33f7f98f4b1ce23902ca1e6557d07d17e78a8571
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-11-20 15:22:05 -0500

    issue #21986: spec saletype

---
+++ test/mocha/specs/sale_type.js
@@ -0,0 +1,88 @@
+/*jshint indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
+newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true,
+white:true*/
+/*global XV:true, XT:true, _:true, console:true, XM:true, Backbone:true, require:true, assert:true,
+setTimeout:true, before:true, clearTimeout:true, exports:true, it:true, describe:true, beforeEach:true */
+
+(function () {
+  "use strict";
+
+  var async = require("async"),
+    _ = require("underscore"),
+    smoke = require("../lib/smoke"),
+    common = require("../lib/common"),
+    assert = require("chai").assert;
+
+  var spec = {
+    recordType: "XM.SaleType",
+    collectionType: "XM.SaleTypeCollection",
+    cacheName: null,
+    listKind: "XV.SaleTypeList",
+    instanceOf: "XM.Document",
+    isLockable: true,
+    idAttribute: "code",
+    enforceUpperKey: true,
+    attributes: ["code", "description"],
+    extensions: ["sales"],
+    privileges: {
+      createUpdate: "MaintainSaleTypes",
+      read: true
+    },
+    createHash: {
+      code: "TESTSALE" + Math.random(),
+      description: "Test Sale Type"
+    },
+    updatableField: "description"
+  };
+
+  var additionalTests = function () {
+    describe("ProjectTypeWorkflow", function () {
+      var projectTypeModel,
+        workflowSourceModel;
+      //var models = {
+      //  projectTypeModel: null,
+      //  workflowSourceModel : null
+      //};
+
+      before(function (done) {
+        //common.prepModels(models);
+        async.parallel([
+          function (done) {
+            common.initializeModel(projectTypeModel, XM.ProjectType, function (err, model) {
+              projectTypeModel = model;
+              projectTypeModel.set(spec.createHash);
+              done();
+            });
+          },
+          function (done) {
+            common.initializeModel(workflowSourceModel, XM.ProjectTypeWorkflow, function (err, model) {
+              workflowSourceModel = model;
+              done();
+            });
+          }
+        ], done);
+      });
+
+
+      it("can get added to a project type", function (done) {
+        assert.isTrue(workflowSourceModel.isReady());
+        workflowSourceModel.set({
+          name: "First step",
+          priority: XM.priorities.models[0]
+        });
+        projectTypeModel.get("workflow").add(workflowSourceModel);
+        assert.isUndefined(JSON.stringify(projectTypeModel.validate(projectTypeModel.attributes)));
+        projectTypeModel.save(null, {success: function () {
+          done();
+        }});
+      });
+
+
+    });
+  };
+
+  exports.spec = spec;
+  exports.additionalTestspork = additionalTests;
+
+}());
+
