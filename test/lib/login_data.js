afcbfa7601314e26678592c69dd14c5ab3f7ece5
elevow <elevow@xtuple.com>
2013-11-22 14:29:29 -0500

    remove login_data.js

--- test/lib/login_data.js
+++
@@ -1,8 +0,0 @@
-exports.data = {
-  webaddress: '',
-  username: 'admin', //------- Enter the xTuple username
-  pwd: 'admin', //------ enter the password here
-  org: 'postbooks', //------ enter the database name here
-  suname: '', //-------enter the sauce labs username
-  sakey: '' //------enter the sauce labs access key
-}
