661b040b94b7f33f041fae86279326eabc3c1377
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-11-27 11:30:02 -0500

    issue #21986: use parent_uuid in wf

--- lib/orm/source/xt/tables/wf.sql
+++ lib/orm/source/xt/tables/wf.sql
@@ -1,5 +1,6 @@
 select xt.create_table('wf', 'xt', false, 'xt.obj');
 
+
 select xt.add_column('wf','wf_id', 'serial', 'primary key');
 select xt.add_column('wf','wf_name', 'text');
 select xt.add_column('wf','wf_description', 'text');
@@ -13,7 +14,7 @@ select xt.add_column('wf','wf_notes', 'text');
 select xt.add_column('wf','wf_priority_id', 'integer', 'not null');
 select xt.add_column('wf','wf_owner_username', 'text');
 select xt.add_column('wf','wf_assigned_username', 'text');
-select xt.add_column('wf','wf_parent_id', 'integer');
+select xt.add_column('wf','wf_parent_uuid', 'uuid');
 select xt.add_column('wf','wf_completed_parent_status', 'text');
 select xt.add_column('wf','wf_deferred_parent_status', 'text');
 select xt.add_column('wf','wf_sequence', 'integer', 'not null default 0');
@@ -21,3 +22,14 @@ select xt.add_column('wf','wf_completed_successors', 'text');
 select xt.add_column('wf','wf_deferred_successors', 'text');
 
 comment on table xt.wf is 'Workflow extension table';
+
+-- migrate from previous implementation (for 1.5.1)
+update xt.wf 
+set wf_parent_uuid = prj.obj_uuid 
+from prj 
+where wf_parent_id = prj_id
+and wf_parent_uuid is null
+
+-- once migration is done we can drop the column
+-- TODO
+--alter table xt.wf drop column if exists wf_parent_id cascade
