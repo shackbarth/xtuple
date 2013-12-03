338e36a4839969b1f105e81ff20f281c88c91ab9
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-12-03 16:42:15 -0500

    issue #21989: repurpose itemsForCustomer

--- enyo-client/database/source/xm/javascript/item_site.sql
+++ enyo-client/database/source/xm/javascript/item_site.sql
@@ -29,20 +29,39 @@ select xt.install_js('XM','item_site','xtuple', $$
     @returns {Array}
    */
   XM.ItemSite.itemsForCustomer = function (customerId, shiptoId, effectiveDate, query) {
-    customerId = XT.Data.getId(XT.Orm.fetch('XM', 'CustomerProspectRelation'), customerId);
+    customerId = customerId ? XT.Data.getId(XT.Orm.fetch('XM', 'CustomerProspectRelation'), customerId) : null;
     shiptoId = shiptoId ? XT.Data.getId(XT.Orm.fetch('XM', 'CustomerShipto'), shiptoId) : -1;
     effectiveDate = effectiveDate || new Date();
     query = query || {};
-    var limit = query.rowLimit ? 'limit ' + query.rowLimit : '',
-      offset = query.rowOffset ? 'offset ' + query.rowOffset : '',
+    var limit = query.rowLimit ? 'limit ' + Number(query.rowLimit) : '',
+      offset = query.rowOffset ? 'offset ' + Number(query.rowOffset) : '',
       clause = XT.Data.buildClause("XM", "ItemSite", query.parameters, query.orderBy),
+      spliceIndex,
+      oneIfSplice = 0,
+      aliasInjection,
+      itemNumber,
       sql = 'select * from xm.item_site_list_item where id in ' +
             '(select id ' +
             ' from xm.item_site_list_item ' +
-            ' where {conditions} ' +
-            '  and (item).id in (select * from custitem(${p1}, ${p2}::integer, ${p3}::date)) ' +
-            '{orderBy} {limit} {offset}) ' +
-            '{orderBy}';
+            ' where {conditions} ';
+
+    if (customerId) {
+      sql = sql + ' and (item).id in (select * from custitem(${p2}, ${p3}::integer, ${p4}::date)) ';
+    }
+    sql = sql + '{orderBy} {limit} {offset}) ' +
+      '{orderBy}';
+
+    /* crazily splice in the option of querying by alias in the middle of the item number part of the WHERE */
+    spliceIndex = clause.conditions.indexOf('or (item).barcode');
+    if (spliceIndex >= 0) {
+      oneIfSplice = 1;
+      aliasInjection = " or (item).number in (select item_number from item inner join itemalias " + 
+        "on item_id = itemalias_item_id where itemalias_number ~^ ${p1}) "
+      clause.conditions = clause.conditions.substring(0, spliceIndex) + aliasInjection + clause.conditions.substring(spliceIndex);
+      itemNumber = query.parameters.filter(function (param) {
+        return param.attribute && param.attribute.length && param.attribute.indexOf("item.number") >= 0;
+      })[0].value;
+    }
 
     /* query the model */
     sql = sql.replace('{conditions}', clause.conditions)
@@ -50,9 +69,16 @@ select xt.install_js('XM','item_site','xtuple', $$
              .replace('{limit}', limit)
              .replace('{offset}', offset)
              .replace('{p1}', clause.parameters.length + 1)
-             .replace('{p2}', clause.parameters.length + 2)
-             .replace('{p3}', clause.parameters.length + 3);
-    clause.parameters = clause.parameters.concat([customerId, shiptoId, effectiveDate]);
+             .replace('{p2}', clause.parameters.length + 1 + oneIfSplice)
+             .replace('{p3}', clause.parameters.length + 2 + oneIfSplice)
+             .replace('{p4}', clause.parameters.length + 3 + oneIfSplice);
+
+    if (spliceIndex >= 0) {
+      clause.parameters = clause.parameters.concat([itemNumber]);
+    }
+    if (customerId) {
+      clause.parameters = clause.parameters.concat([customerId, shiptoId, effectiveDate]);
+    }
     if (DEBUG) { plv8.elog(NOTICE, 'sql = ', sql); }
     return plv8.execute(sql, clause.parameters);
   };
