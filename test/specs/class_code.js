be030d8fd1e7ec3d7bb9616b2022c4553b69b1ba
Gene Levow <elevow@xtuple.com>
2013-12-03 08:25:07 -0500

    added class code spec

---
+++ test/specs/class_code.js
@@ -0,0 +1,43 @@
+/*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
+  immed:true, eqeqeq:true, forin:true, latedef:true,
+  newcap:true, noarg:true, undef:true */
+/*global describe:true, it:true, XT:true, XM:true, XV:true, process:true,
+  module:true, require:true, exports:true */
+
+(function () {
+  "use strict";
+
+  /**
+  An Inventory control mechanism used for categorizing items regardless of item type.
+  Distinguished from product category, which categorizes sold items only.
+  @class
+  @alias ClassCode
+  @property {String} Code
+  @property {String} Description
+  **/
+
+  var spec = {
+      recordType: "XM.ClassCode",
+      enforceUpperKey: false,
+      collectionType: "XM.ClassCodeCollection",
+      listKind: "XV.ClassCodeList",
+      instanceOf: "XM.Document",
+      attributes: ["code", "description"],
+      idAttribute: "code",
+      extensions: ["crm"],
+      isLockable: true,
+      cacheName: "XM.classCodes",
+      privileges: {
+        createUpdateDelete: "MaintainClassCodes",
+        //read: "ViewClassCodes" //Incident 22098
+      },
+      createHash: {
+        code: "CCode" + Math.random(),
+        description: 'Class Code Description'
+      },
+      updatableField: "code",
+    };
+
+  exports.spec = spec;
+
+}());
