4e1ea3e0c9d600f6f71023c387cea62e7a0c73ce
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-12-10 14:16:58 -0500

    issue #22067: add obj_uuid to public cmitem table

--- enyo-client/database/source/manifest.js
+++ enyo-client/database/source/manifest.js
@@ -17,6 +17,7 @@
     "public/tables/cashrcpt.sql",
     "public/tables/cohead.sql",
     "public/tables/comment.sql",
+    "public/tables/cmitem.sql",
     "public/tables/cntct.sql",
     "public/tables/coitem.sql",
     "public/tables/cohist.sql",
