fe77cc3f44cdef8afa78800c90f0444c165b88ee
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-12-13 17:32:19 -0500

    issue #20689: proof of concept with backtick prefix

--- enyo-client/application/source/views/workspace.js
+++ enyo-client/application/source/views/workspace.js
@@ -1935,6 +1935,9 @@ strict: false*/
   var lineItem = {
     kind: "XV.Workspace",
     modelAmnesty: true,
+    handlers: {
+      onBarcodeCapture: "handleBarcodeCapture"
+    },
     components: [
       {kind: "Panels", name: "salesLinePanels", arrangerKind: "CarouselArranger",
         fit: true, components: [
@@ -2021,6 +2024,10 @@ strict: false*/
 
       // Add the Comment Box to Panels
       this.$.salesLinePanels.createComponents([comments], {owner: this});
+    },
+    handleBarcodeCapture: function (inSender, inEvent) {
+      this.$.itemSiteWidget.$.privateItemSiteWidget.$.input.setValue(inEvent.data);
+      this.$.itemSiteWidget.$.privateItemSiteWidget.autocomplete();
     }
   };
   enyo.mixin(lineItem, XV.LineMixin);
