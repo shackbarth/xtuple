bda78f0168f717b1d601a5c718a11a0adca99102
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-12-26 12:08:35 -0500

    issue #22067: clean up diffs

--- enyo-client/database/source/xt/functions/cm_line_tax_total.sql
+++ enyo-client/database/source/xt/functions/cm_line_tax_total.sql
@@ -1,5 +1,4 @@
 create or replace function xt.cm_line_tax_total(cmitem_id integer) returns numeric stable as $$
-  --select 0.0;
   select -1 * sum(taxhist_tax) as tax_total
   from cmitemtax
   where taxhist_parent_id=$1;
