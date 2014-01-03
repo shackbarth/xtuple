042dd6e10fca12bb23389a08794bf10a8a68f718
John Rogelstad <john@xtuple.com>
2014-01-02 21:55:26 -0500

    Swat

--- enyo-client/application/source/widgets/characteristics.js
+++ enyo-client/application/source/widgets/characteristics.js
@@ -1,6 +1,6 @@
-F/*jshint indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
+/*jshint indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
 newcap:true, noarg:true, regexp:true, undef:true, trailing:true,
-white:true*/
+white:true, strict:false*/
 /*global enyo:true, XT:true, XV:true, Globalize:true, XM:true */
 
 (function () {
