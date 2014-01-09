8d447999ef827faa829927d3f44a9f568420b63a
John Rogelstad <john@xtuple.com>
2014-01-09 09:59:53 -0500

    issue #22341: Add support for processing arrays of models at the database level.

--- lib/orm/source/xt/functions/delete.sql
+++ lib/orm/source/xt/functions/delete.sql
@@ -1,7 +1,7 @@
 /**
     Procedure for deleting a record and its children from the database.
 
-    @param {Text} Data hash that can parsed into a JavaScript object.
+    @param {Text} Data hash that can parsed into a JavaScript object or array.
     @param {String} [dataHash.username] Username. Required.
     @param {String} [dataHash.nameSpace] Namespace. Required.
     @param {String} [dataHash.type] Type. Required.
@@ -16,41 +16,68 @@
       "username": "admin",
       "nameSpace":"XM",
       "type": "Contact",
-      "id": "99999",
+      "id": "10009",
       "etag": "e5b73834-492d-47b1-89a2-10b354b8f5e3",
       "prettyPrint": true
     }');
+
+    select xt.delete('[
+      {
+        "username": "admin",
+        "nameSpace":"XM",
+        "type": "Contact",
+        "id": "10009",
+        "etag": "46f5e363-79b5-4302-fa19-05a184ce15f1"
+      },
+      {
+        "username": "admin",
+        "nameSpace":"XM",
+        "type": "Contact",
+        "id": "10010",
+        "etag": "a8d40bcd-efb2-4ec4-c590-3320ee3ceb34"
+      }    
+    ]');
 */
 create or replace function xt.delete(data_hash text) returns boolean as $$
+  var dataArray = JSON.parse(data_hash),
+    isArray = XT.typeOf(dataArray) === 'array',
+    result = [],
+    data = Object.create(XT.Data),
+    options,
+    observer,
+    rec;
+
+  /* Make sure the input is an array */
+  dataArray = isArray ? dataArray : [dataArray];
+  
   try {
-    var dataHash = JSON.parse(data_hash),
-      data = Object.create(XT.Data),
-      options = JSON.parse(JSON.stringify(dataHash)),
-      prettyPrint = dataHash.prettyPrint ? 2 : null,
-      observer,
-      rec,
-      ret;
+    /* Loop through each input and do the work */
+    dataArray.forEach(function (dataHash) {
+      options = JSON.parse(JSON.stringify(dataHash));
 
-    dataHash.superUser = false;
-    if (dataHash.username) { XT.username = dataHash.username; }
+      dataHash.superUser = false;
+      if (dataHash.username) { XT.username = dataHash.username; }
 
-    /* get the current version of the record */
-    dataHash.includeKeys = true;
-    rec = data.retrieveRecord(dataHash);
-    if (!rec.data) { plv8.elog(ERROR, "Record not found"); };
-    dataHash.data = rec.data;
+      /* get the current version of the record */
+      dataHash.includeKeys = true;
+      rec = data.retrieveRecord(dataHash);
+      if (!rec.data) { plv8.elog(ERROR, "Record not found"); };
+      dataHash.data = rec.data;
 
-    /* mark for deletion */
-    XT.jsonpatch.updateState(dataHash.data, "delete");
+      /* mark for deletion */
+      XT.jsonpatch.updateState(dataHash.data, "delete");
 
-    /* commit the record */
-    data.commitRecord(dataHash);
+      /* commit the record */
+      data.commitRecord(dataHash);
 
-    /* Unset XT.username so it isn't cached for future queries. */
-    XT.username = undefined;
+      /* Unset XT.username so it isn't cached for future queries. */
+      XT.username = undefined;
 
-    XT.message(204, "No Content");
+      XT.message(204, "No Content");
+    });
+    
     return true;
+    
   } catch (err) {
     XT.error(err);
   }
