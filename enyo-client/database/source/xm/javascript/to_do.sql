0700fdb18bbc40a2ae8e68d024e2651daef8d34b
Greg Pazo <gpazo@xtuple.com>
2014-01-10 14:47:51 -0500

    update misc copyright dates

--- enyo-client/database/source/xm/javascript/to_do.sql
+++ enyo-client/database/source/xm/javascript/to_do.sql
@@ -1,5 +1,5 @@
 select xt.install_js('XM','ToDo','xtuple', $$
-  /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
+  /* Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
      See www.xm.ple.com/CPAL for the full text of the software license. */
 
   XM.ToDo = {};
