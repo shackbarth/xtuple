0f06fe8523e334b46161c216513b94aab81f33ed
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-01-21 12:34:22 -0500

    issue #22390: bugfixes

--- enyo-client/database/source/xt/tables/custcustemlprofile.sql
+++ enyo-client/database/source/xt/tables/custcustemlprofile.sql
@@ -4,4 +4,4 @@ select xt.create_table('custcustemlprofile');
 select xt.add_column('custcustemlprofile','custcustemlprofile_cust_id', 'integer', 'primary key');
 select xt.add_column('custcustemlprofile','custcustemlprofile_custemlprofile_id', 'integer', 'references xt.custemlprofile (emlprofile_id)');
 
-comment on table xt.incdtcatemlprofile is 'Core table links customers to customer email profiles';
+comment on table xt.custcustemlprofile is 'Core table links customers to customer email profiles';
--- enyo-client/database/source/xt/tables/custemlprofile.sql
+++ enyo-client/database/source/xt/tables/custemlprofile.sql
@@ -4,7 +4,7 @@ select xt.create_table('custemlprofile', 'xt', false, 'xt.emlprofile');
 select xt.add_constraint('custemlprofile','custemlprofile_emlprofile_name', 'unique(emlprofile_name)');
 select xt.add_constraint('custemlprofile','custemlprofile_pkey', 'primary key (emlprofile_id)');
 
-comment on table xt.poemlprofile is 'Table for customer email profiles';
+comment on table xt.custemlprofile is 'Table for customer email profiles';
 
 -- this priv does not exist in postbooks so create it here
 select xt.add_priv('MaintainCustomerEmailProfiles', 'Can Maintain Customer Email Profiles', 'Customer', 'Customer');
