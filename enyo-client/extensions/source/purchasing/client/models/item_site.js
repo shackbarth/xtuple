5eb1700ed583b4ff5e73108d25b62158ca2b3d54
John Rogelstad <john@xtuple.com>
2014-01-30 10:46:46 -0500

    issue #22534: Add purchase supply biz logic to item site.

--- enyo-client/extensions/source/purchasing/client/models/item_site.js
+++ enyo-client/extensions/source/purchasing/client/models/item_site.js
@@ -11,6 +11,7 @@ white:true*/
     var _proto = XM.ItemSite.prototype,
       _defaults = _proto.defaults;
 
+    // Unfortunately can't augment this
     _proto.defaults = function () {
       var defaults = _defaults.apply(this, arguments);
 
@@ -27,7 +28,62 @@ white:true*/
       readOnlyAttributes: [
         'isCreatePurchaseOrdersForSalesOrders',
         'isCreatePurchaseRequestsForSalesOrders'
-      ]
+      ],
+
+      handlers: {
+        "change:isCreatePurchaseOrdersForSalesOrders":
+          "isCreatePurchaseOrdersForSalesOrdersChanged",
+        "change:isCreatePurchaseRequestsForSalesOrders":
+          "isCreatePurchaseRequestsForSalesOrdersChanged",
+        "change:isPurchased": "isPurchasedChanged",
+        "status:READY_CLEAN": "statusReadyClean"
+      },
+
+      isCreatePurchaseOrdersForSalesOrdersChanged: function () {
+        var isCreatePo = this.get("isCreatePurchaseOrdersForSalesOrders");
+
+        if (isCreatePo) {
+          this.set("isCreatePurchaseRequestsForSalesOrders", false);
+        }
+      },
+
+      isCreatePurchaseRequestsForSalesOrdersChanged: function () {
+        var isCreatePr = this.get("isCreatePurchaseRequestsForSalesOrders");
+
+        if (isCreatePr) {
+          this.set("isCreatePurchaseOrdersForSalesOrders", false);
+        }
+      },
+
+      isPurchasedChanged: function (model, changes, options) {
+        options = options ? _.clone(options) : {};
+        var isPurchased = this.get("isPurchased"),
+          itemType = this.getValue("item.itemType"),
+          K = XM.Item;
+
+        if (isPurchased && itemType === K.PURCHASED ||
+          itemType === K.OUTSIDE_PROCESS) {
+          this.setReadOnly([
+            "isCreatePurchaseRequestsForSalesOrders",
+            "isCreatePurchaseOrdersForSalesOrders"
+          ], false);
+        } else {
+          if (!options.isLoading) {
+            this.set({
+              isCreatePurchaseRequestsForSalesOrders: false,
+              isCreatePurchaseOrdersForSalesOrders: false
+            });
+          }
+          this.setReadOnly([
+            "isCreatePurchaseRequestsForSalesOrders",
+            "isCreatePurchaseOrdersForSalesOrders"
+          ]);
+        }
+      },
+
+      statusReadyClean: function () {
+        this.isPurchasedChanged(this, null, {isLoading: true});
+      }
     });
 
   };
