e48dcb573547c3f7a34e01feb654997daa83c16d
Ben Thompson <ben@xtuple.com>
2014-02-10 13:12:00 -0500

    fix merge conflict

--- node-datasource/oauth2/passport.js
+++ node-datasource/oauth2/passport.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*jshint node:true, indent:2, curly:false, eqeqeq:true, immed:true, latedef:true, newcap:true, noarg:true,
 regexp:true, undef:true, strict:true, trailing:true, white:true */
 /*global X:true, XM:true, SYS:true, XT:true, _:true, console:true*/
