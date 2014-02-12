f1657aae52dd5a11eadc6d9de8db0f7b60e33927
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-02-12 13:59:19 -0500

    issue #22689: better related information subgrouping

--- scripts/generateSpecs.sh
+++ scripts/generateSpecs.sh
@@ -1,2 +1,2 @@
 rm -rf ../../jsdoc/outspecs
-../../jsdoc/jsdoc -t ./templates/xtuple_specs -c ./jsdoc.conf.json -d ../../jsdoc/outspecs ../test/lib/specs.js ../test/specs ./jsdoc_spec_README.md
+../../jsdoc/jsdoc -t ./templates/xtuple_specs -c ./jsdoc.conf.json -d ../../jsdoc/outspecs ../test/specs/invoice.js ./jsdoc_spec_README.md
