801df0e9b783bf57cf3bda9a96b213f9794324be
Ben Thompson <ben@xtuple.com>
2014-03-03 15:20:49 -0500

    swat.

--- enyo-client/extensions/source/purchasing/database/source/xt/views/poiteminfo.sql
+++ enyo-client/extensions/source/purchasing/database/source/xt/views/poiteminfo.sql
@@ -6,7 +6,7 @@ select poitem.*,
   case when poitem_itemsite_id is null then true else false end as poitem_is_misc,
   pohead_curr_id as curr_id,
   xt.po_line_extended_price(poitem) as extended_price,
-  xt.ppo_line_tax(poitem) as tax,
+  xt.po_line_tax(poitem) as tax,
   coalesce(poitem_qty_toreceive,0) as qty_toreceive
 from poitem
   left join itemsite on itemsite_id=poitem_itemsite_id
