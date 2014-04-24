dce449faada25793e17207259452d143eb8b966c
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-04-23 22:31:14 -0400

    make and tar the postbooks updater package

--- scripts/build_basic_database_workproducts.sh
+++ scripts/build_basic_database_workproducts.sh
@@ -1,19 +1,27 @@
-git fetch XTUPLE
-git checkout XTUPLE/4_4_x
-rm -rf scripts/output
-npm run-script build-basic-postbooks-package-sql
-npm run-script build-basic-empty
-npm run-script build-basic-postbooks-demo
-npm run-script build-basic-quickstart
+#git fetch XTUPLE
+#git checkout XTUPLE/4_4_x
 
-../private-extensions
-git fetch XTUPLE
-git checkout XTUPLE/4_4_x
-npm run-script build-basic-manufacturing-package-sql
-npm run-script build-basic-distribution-package-sql
-npm run-script build-basic-manufacturing-empty
-npm run-script build-basic-manufacturing-quickstart
-npm run-script build-basic-manufacturing-demo
-npm run-script build-basic-distribution-empty
-npm run-script build-basic-distribution-quickstart
-npm run-script build-basic-distribution-demo
+
+#rm -rf scripts/output
+#npm run-script build-basic-postbooks-package-sql
+#npm run-script build-basic-empty
+#npm run-script build-basic-postbooks-demo
+#npm run-script build-basic-quickstart
+#cd ../private-extensions
+
+#git fetch XTUPLE
+#git checkout XTUPLE/4_4_x
+
+#npm run-script build-basic-manufacturing-package-sql
+#npm run-script build-basic-distribution-package-sql
+#npm run-script build-basic-manufacturing-empty
+#npm run-script build-basic-manufacturing-quickstart
+#npm run-script build-basic-distribution-empty
+#npm run-script build-basic-distribution-quickstart
+
+mkdir scripts/output/pb440to441
+cp scripts/xml/postbooks_package.xml scripts/output/pb440to441
+cp scripts/output/toolkit.sql scripts/output/pb440to441
+cp scripts/output/updates.sql scripts/output/pb440to441
+tar -zcvf scripts/output/pb440to441.gz scripts/output/pb440to441
+
