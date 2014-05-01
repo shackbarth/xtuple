e0d33640d06a7bef6c9c1827385b510546fa483a
Greg Pazo <gpazo@xtuple.com>
2014-05-01 11:00:07 -0400

    whoops two .sh scripts, fix'd

--- scripts/release_build.sh
+++ scripts/release_build.sh
@@ -3,20 +3,20 @@
 
 rm -rf scripts/output
 npm run-script build-basic-postbooks-package-sql
-#npm run-script build-basic-empty
-#npm run-script build-basic-postbooks-demo
-#npm run-script build-basic-quickstart
+npm run-script build-basic-empty
+npm run-script build-basic-postbooks-demo
+npm run-script build-basic-quickstart
 cd ../private-extensions
 
 #git fetch XTUPLE
 #git checkout XTUPLE/4_4_x
 
 npm run-script build-basic-manufacturing-package-sql
-#npm run-script build-basic-manufacturing-empty
-#npm run-script build-basic-manufacturing-quickstart
+npm run-script build-basic-manufacturing-empty
+npm run-script build-basic-manufacturing-quickstart
 npm run-script build-basic-distribution-package-sql
-#npm run-script build-basic-distribution-empty
-#npm run-script build-basic-distribution-quickstart
+npm run-script build-basic-distribution-empty
+npm run-script build-basic-distribution-quickstart
 
 cd ../xtuple
 mkdir scripts/output/pb441
