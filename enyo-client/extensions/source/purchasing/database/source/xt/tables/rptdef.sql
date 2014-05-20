e57650e0d4cded5011beb78270ed570fd6acace7
Linda Nichols <lynnaloo@gmail.com>
2014-05-20 20:13:36 +0000

    Issue #22238: Created Purchase Order Report

--- enyo-client/extensions/source/purchasing/database/source/xt/tables/rptdef.sql
+++ enyo-client/extensions/source/purchasing/database/source/xt/tables/rptdef.sql
@@ -18,12 +18,12 @@ select xt.add_report_definition('XM.PurchaseOrder', 0, $${
       "options": {"x": 200, "y": 40, "width": 150}
     },
     {
-      "definition": [{"text": "_purchaseOrder"}],
-      "options": {"fontBold": true, "fontSize": 18, "x": 450, "y": 40, "align": "right"}
-    },
-    {
       "definition": [{"text": "_orderDate", "label": true}],
       "options": {"fontBold": true, "x": 400, "y": 60}
+    },
+    {
+      "definition": [{"text": "_purchaseOrder"}],
+      "options": {"fontBold": true, "fontSize": 18, "x": 450, "y": 40, "align": "right"}
     },
     {
       "definition": [{"attr": "orderDate"}],
@@ -38,40 +38,98 @@ select xt.add_report_definition('XM.PurchaseOrder', 0, $${
       "options": {"x": 500, "y": 75, "align": "right"}
     },
     {
-      "definition": [{"text": "_billto", "label": true}],
-      "options": {"x": 0, "y": 150, "fontBold": true}
+      "definition": [{"text": "_to", "label": true}],
+      "options": {"x": 100, "y": 140, "fontBold": true}
     },
     {
       "definition": [
-        {"attr": "billtoName"},
-        {"attr": "billtoAddress1"},
-        {"attr": "billtoAddress2"},
-        {"attr": "billtoAddress3"},
-        {"attr": "billtoCity"},
-        {"attr": "billtoState"},
-        {"attr": "billtoPostalCode"},
-        {"attr": "billtoCountry"},
-        {"attr": "billtoPhone"}
+        {"attr": "vendorContact"},
+        {"attr": "vendorAddress1"},
+        {"attr": "vendorAddress2"},
+        {"attr": "vendorAddress3"},
+        {"attr": "vendorCity"},
+        {"attr": "vendorState"},
+        {"attr": "vendorPostalCode"},
+        {"attr": "vendorCountry"},
+        {"attr": "vendorPhone"}
       ],
       "transform": "address",
-      "options": {"x": 120, "y": 150, "width": 250}
+      "options": {"x": 100, "y": 160, "width": 250}
+    },
+    {
+      "definition": [{"text": "_shipTo", "label": true}],
+      "options": {"x": 350, "y": 140, "fontBold": true}
+    },
+    {
+      "definition": [
+        {"attr": "shiptoName"},
+        {"attr": "shiptoAddress1"},
+        {"attr": "shiptoAddress2"},
+        {"attr": "shiptoAddress3"},
+        {"attr": "shiptoCity"},
+        {"attr": "shiptoState"},
+        {"attr": "shiptoPostalCode"},
+        {"attr": "shiptoCountry"},
+        {"attr": "shiptoPhone"}
+      ],
+      "transform": "address",
+      "options": {"x": 350, "y": 160, "width": 250}
+    },
+    {
+      "definition": [{"text": "_fob", "label": true}],
+      "options": {"fontBold": true, "x": 0, "y": 230}
+    },
+    {
+      "definition": [{"attr": "fob"}],
+      "options": {"x": 0, "y": 245}
+    },
+    {
+      "definition": [{"text": "_shipVia", "label": true}],
+      "options": {"fontBold": true, "x": 150, "y": 230}
+    },
+    {
+      "definition": [{"attr": "shipVia"}],
+      "options": {"x": 150, "y": 245}
+    },
+    {
+      "definition": [{"text": "_terms", "label": true}],
+      "options": {"fontBold": true, "x": 300, "y": 230}
+    },
+    {
+      "definition": [{"attr": "terms.description"}],
+      "options": {"x": 300, "y": 245}
     },
     {"element": "fontBold"},
     {
       "element": "band",
       "definition": [
-        {"text": "_quantity", "width": 100},
-        {"text": "_uom", "width": 50},
-        {"text": "_item", "width": 100},
-        {"text": "_currency", "width": 80},
-        {"text": "_unitPrice", "width": 100},
+        {"text": "_item", "width": 90},
+        {"text": "_vendorItem", "width": 100},
+        {"text": "_description", "width": 100},
+        {"text": "_ordered", "width": 60},
+        {"text": "_uom", "width": 45},
+        {"text": "_unitPrice", "width": 90},
         {"text": "_extendedPrice", "width": 100}
       ],
-      "options": {"border": 0, "padding": 5, "x": 0, "y": 325}
+      "options": {"border": 0, "padding": 5, "x": 0, "y": 300}
     },
     {"element": "bandLine", "size": 2}
   ],
   "detailElements": [
+    {"element": "fontNormal"},
+    {
+      "element": "band",
+      "definition": [
+        {"attr": "lineItems*vendorItemNumber", "width": 90},
+        {"attr": "lineItems*item.number", "width": 100},
+        {"attr": "lineItems*item.description1", "width": 100},
+        {"attr": "lineItems*quantity", "width": 60},
+        {"attr": "lineItems*vendorUnit", "width": 45},
+        {"attr": "lineItems*price", "width": 90},
+        {"attr": "lineItems*extendedPrice", "width": 100}
+      ],
+      "options": {"fontBold": true, "border": 0, "padding": 14}
+    }
   ],
   "footerElements": [
   ],
