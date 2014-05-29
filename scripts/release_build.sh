313ce3e9d38c658ae6ca4a44509a9c2b3e144a48
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-05-29 12:33:31 -0400

    issue #23243: update package naming convention and payloads

--- scripts/release_build.sh
+++ scripts/release_build.sh
@@ -3,6 +3,7 @@ MAJ=$1
 MIN=$2
 PAT=$3
 
+# Usage: ./scripts/release_build.sh 4 5 0-beta
 echo "BUILDING RELEASE "$MAJ"."$MIN"."$PAT""
 
 git fetch XTUPLE
@@ -25,47 +26,58 @@ npm run-script build-basic-manufacturing-demo
 npm run-script build-basic-distribution-package-sql
 npm run-script build-basic-distribution-empty
 npm run-script build-basic-distribution-quickstart
-#postbooks package
-cd ../xtuple
-mkdir scripts/output/pb$MAJ$MIN$PAT
-cp scripts/xml/postbooks_package.xml scripts/output/pb$MAJ$MIN$PAT/package.xml
-cp scripts/output/toolkit.sql scripts/output/pb$MAJ$MIN$PAT
-cp scripts/output/updates.sql scripts/output/pb$MAJ$MIN$PAT
-cd scripts/output
-tar -zcvf pb$MAJ$MIN$PAT.gz pb$MAJ$MIN$PAT/
-#distribution package
-cd ../../
-mkdir scripts/output/dist$MAJ$MIN$PAT
-cp scripts/xml/distribution_package.xml scripts/output/dist$MAJ$MIN$PAT/package.xml
-cp scripts/output/updates.sql scripts/output/dist$MAJ$MIN$PAT
-cp scripts/output/inventory_upgrade.sql scripts/output/dist$MAJ$MIN$PAT
-cd scripts/output
-tar -zcvf dist$MAJ$MIN$PAT.gz dist$MAJ$MIN$PAT/
-#postbooks to distribution
-cd ../../
-mkdir scripts/output/pbtodist$MAJ$MIN$PAT
-cp scripts/xml/distribution_install.xml scripts/output/pbtodist$MAJ$MIN$PAT/package.xml
-cp scripts/output/inventory_basic_install.sql scripts/output/pbtodist$MAJ$MIN$PAT
-cp scripts/output/inventory_upgrade.sql scripts/output/pbtodist$MAJ$MIN$PAT
-cd scripts/output
-tar -zcvf pbtodist$MAJ$MIN$PAT.gz pbtodist$MAJ$MIN$PAT/
-#xtmfg packages
-cd ../../
-mkdir scripts/output/xtmfg$MAJ$MIN$PAT
-cp scripts/xml/xtmfg_package.xml scripts/output/xtmfg$MAJ$MIN$PAT/package.xml
-cp scripts/output/updates.sql scripts/output/xtmfg$MAJ$MIN$PAT
-cp scripts/output/inventory_upgrade.sql scripts/output/xtmfg$MAJ$MIN$PAT
-cp scripts/output/manufacturing_upgrade.sql scripts/output/xtmfg$MAJ$MIN$PAT
-cd scripts/output
-tar -zcvf xtmfg_upgrade-$MAJ$MIN$PAT.gz xtmfg$MAJ$MIN$PAT/
 
-cd ../../
-mkdir scripts/output/xtmfg_install$MAJ$MIN$PAT
-cp scripts/xml/xtmfg_install.xml scripts/output/xtmfg_install$MAJ$MIN$PAT/package.xml
-cp scripts/output/manufacturing_basic_install.sql scripts/output/xtmfg_install$MAJ$MIN$PAT
-cp scripts/output/manufacturing_upgrade.sql scripts/output/xtmfg_install$MAJ$MIN$PAT
+#postbooks upgrade
+cd ../xtuple
+mkdir scripts/output/postbooks-upgrade-$MAJ$MIN$PAT
+cp scripts/xml/postbooks_package.xml scripts/output/postbooks-upgrade-$MAJ$MIN$PAT/package.xml
+cp scripts/output/postbooks_upgrade.sql scripts/output/postbooks-upgrade-$MAJ$MIN$PAT
 cd scripts/output
-tar -zcvf xtmfg_install-$MAJ$MIN$PAT.gz xtmfg_install$MAJ$MIN$PAT/
+tar -zcvf postbooks-upgrade-$MAJ$MIN$PAT.gz postbooks-upgrade-$MAJ$MIN$PAT/
+
+#distribution upgrade
+cd ../../
+mkdir scripts/output/distribution-upgrade-$MAJ$MIN$PAT
+cp scripts/xml/distribution_package.xml scripts/output/distribution-upgrade-$MAJ$MIN$PAT/package.xml
+cp scripts/output/postbooks_upgrade.sql scripts/output/distribution-upgrade-$MAJ$MIN$PAT
+cp scripts/output/inventory_upgrade.sql scripts/output/distribution-upgrade-$MAJ$MIN$PAT
+cp scripts/output/distribution_upgrade.sql scripts/output/distribution-upgrade-$MAJ$MIN$PAT
+cd scripts/output
+tar -zcvf distribution-upgrade-$MAJ$MIN$PAT.gz distribution-upgrade-$MAJ$MIN$PAT/
+
+#distribution install
+cd ../../
+mkdir scripts/output/distribution-install-$MAJ$MIN$PAT
+cp scripts/xml/distribution_install.xml scripts/output/distribution-install-$MAJ$MIN$PAT/package.xml
+cp scripts/output/postbooks_upgrade.sql scripts/output/distribution-install-$MAJ$MIN$PAT
+cp scripts/output/inventory_basic_install.sql scripts/output/distribution-install-$MAJ$MIN$PAT
+cp scripts/output/inventory_upgrade.sql scripts/output/distribution-install-$MAJ$MIN$PAT
+cp scripts/output/distribution_basic_install scripts/output/distribution-install-$MAJ$MIN$PAT
+cp scripts/output/distribution_upgrade.sql scripts/output/distribution-install-$MAJ$MIN$PAT
+cd scripts/output
+tar -zcvf distribution-install-$MAJ$MIN$PAT.gz distribution-install-$MAJ$MIN$PAT/
+
+#manufacturing upgrade
+cd ../../
+mkdir scripts/output/manufacturing-upgrade-$MAJ$MIN$PAT
+cp scripts/xml/xtmfg_package.xml scripts/output/manufacturing-upgrade-$MAJ$MIN$PAT/package.xml
+cp scripts/output/postbooks_upgrade.sql scripts/output/manufacturing-upgrade-$MAJ$MIN$PAT
+cp scripts/output/inventory_upgrade.sql scripts/output/manufacturing-upgrade-$MAJ$MIN$PAT
+cp scripts/output/manufacturing_upgrade.sql scripts/output/manufacturing-upgrade-$MAJ$MIN$PAT
+cd scripts/output
+tar -zcvf manufacturing-upgrade-$MAJ$MIN$PAT.gz manufacturing-upgrade-$MAJ$MIN$PAT/
+
+#manufacturing install
+cd ../../
+mkdir scripts/output/manufacturing-install-$MAJ$MIN$PAT
+cp scripts/xml/xtmfg_package.xml scripts/output/manufacturing-install-$MAJ$MIN$PAT/package.xml
+cp scripts/output/postbooks_upgrade.sql scripts/output/manufacturing-install-$MAJ$MIN$PAT
+cp scripts/output/inventory_basic_install.sql scripts/output/manufacturing-install-$MAJ$MIN$PAT
+cp scripts/output/inventory_upgrade.sql scripts/output/manufacturing-install-$MAJ$MIN$PAT
+cp scripts/output/manufacturing_basic_install.sql scripts/output/manufacturing-install-$MAJ$MIN$PAT
+cp scripts/output/manufacturing_upgrade.sql scripts/output/manufacturing-install-$MAJ$MIN$PAT
+cd scripts/output
+tar -zcvf manufacturing-install-$MAJ$MIN$PAT.gz manufacturing-install-$MAJ$MIN$PAT/
 
 ADMIN=admin
 PORT=5432
@@ -78,16 +90,15 @@ done
 
 #cleanup
 cd ../..
-rm -rf scripts/output/pb$MAJ$MIN$PAT/
-rm -rf scripts/output/updates.sql
-rm -rf scripts/output/toolkit.sql
-rm -rf scripts/output/pbtodist$MAJ$MIN$PAT/
-rm -rf scripts/output/dist$MAJ$MIN$PAT/
+rm -rf scripts/output/postbooks-upgrade-$MAJ$MIN$PAT/
+rm -rf scripts/output/postbooks_upgrade.sql
+rm -rf scripts/output/distribution-install-$MAJ$MIN$PAT/
+rm -rf scripts/output/distribution-upgrade-$MAJ$MIN$PAT/
 rm -rf scripts/output/distribution_upgrade.sql
 rm -rf scripts/output/distribution_basic_install.sql
 rm -rf scripts/output/inventory_basic_install.sql
 rm -rf scripts/output/inventory_upgrade.sql
-rm -rf scripts/output/xtmfg$MAJ$MIN$PAT/
-rm -rf scripts/output/xtmfg_install$MAJ$MIN$PAT/
+rm -rf scripts/output/manufacturing-install-$MAJ$MIN$PAT/
+rm -rf scripts/output/manufacturing-upgrade-$MAJ$MIN$PAT/
 rm -rf scripts/output/manufacturing_basic_install.sql
 rm -rf scripts/output/manufacturing_upgrade.sql
