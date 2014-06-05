f9a783abd99401f98870a7a003e6d11a3f3aa3a5
Ben Thompson <ben@xtuple.com>
2014-06-05 14:58:52 -0400

    Fix test query

--- test/database/joins.js
+++ test/database/joins.js
@@ -202,7 +202,7 @@ var _ = require("underscore"),
     });
 
     it('should be able to search item-site by number', function (done) {
-      var sql = 'select xt.js_init(true);select xt.post(${"nameSpace":"XM","type":"ItemSiteListItem","dispatch":{"functionName":"fetch","parameters":{"orderBy":[{"attribute":"item.number"}],"rowOffset":0,"rowLimit":50,"parameters":[{"attribute":"isActive","operator":"=","value":true},{"attribute":"item.number","operator":"MATCHES","isCharacteristic":false,"value":"yt"}]}},"encoding":"rjson","username":"admin","encryptionKey":"foo"}$);';
+      var sql = 'select xt.js_init(true);select xt.post($${"nameSpace":"XM","type":"ItemSiteListItem","dispatch":{"functionName":"fetch","parameters":{"orderBy":[{"attribute":"item.number"}],"rowOffset":0,"rowLimit":50,"parameters":[{"attribute":"isActive","operator":"=","value":true},{"attribute":"item.number","operator":"MATCHES","isCharacteristic":false,"value":"yt"}]}},"encoding":"rjson","username":"admin","encryptionKey":"foo"}$$);';
 
       datasource.query(sql, creds, function (err, res) {
         var results;
