2e8b1d15871c3fbc0d9f9e1d4072df4efe7bfb45
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-07-30 12:23:58 -0400

    issue #23962: undo workaround

--- scripts/lib/build_all.js
+++ scripts/lib/build_all.js
@@ -72,7 +72,6 @@ var _ = require('underscore'),
             }
 
           },
-          /*
           function (done) {
             // step 1: npm install extension if necessary
             // an alternate approach would be only npm install these
@@ -102,7 +101,6 @@ var _ = require('underscore'),
               }, done);
             });
           },
-          */
           function (done) {
             // step 2: build the client
             buildClient(specs, done);
