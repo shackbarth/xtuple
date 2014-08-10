0f1c0568cf1ad6fab75dbe839dbb888c4d9b031f
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-08-10 09:18:09 -0400

    split out model itself

--- node-datasource/public/customer.js
+++ node-datasource/public/customer.js
@@ -9,16 +9,32 @@ var XN = {};
 
 
   console.log("Foo");
-  XN.CustomerListItem = Backbone.Model.extend({
-    urlRoot: "/dev/restapi/customerListItem",
-    //toJSON: function () {
 
+  XN.Model = Backbone.RelationalModel.extend({
+    parse: function (resp, options) {
+      return resp.data.data;
+    }
+  });
+  XN.Collection = Backbone.Collection.extend({
     parse: function (resp, options) {
       return resp.data.data;
     }
   });
 
-  XN.CustomerListItemCollection = Backbone.Model.extend({
+  XN.ContactRelation = XN.Model.extend({
+    urlRoot: "/dev/restapi/contactRelaton"
+  });
+  XN.CustomerListItem = XN.Model.extend({
+    urlRoot: "/dev/restapi/customerListItem",
+	relations: [{
+		type: Backbone.HasOne,
+		key: 'billingContact',
+		relatedModel: 'XN.ContactRelation',
+		collectionType: 'XN.ContactRelationCollection'
+	}]
+  });
+
+  XN.CustomerListItemCollection = XN.Collection.extend({
     model: XN.CustomerListItem,
     url: "/dev/restapi/customerListItem",
     parse: function (resp, options) {
