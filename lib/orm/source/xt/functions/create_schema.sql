a9000f815153198567876eee12c07d074a626ef1
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-08-19 09:34:20 -0500

    rewrite create_schema in plpgsql

--- lib/orm/source/xt/functions/create_schema.sql
+++ lib/orm/source/xt/functions/create_schema.sql
@@ -1,9 +1,25 @@
-create or replace function xt.create_schema(schema_name text) returns boolean volatile as $$
+create or replace function xt.create_schema(sch text) returns boolean volatile as $$
+declare
+  count integer;
+  query text;
+begin
+
   /* Only create the schema if it hasn't been created already */
-  var res, sql = "select schema_name from information_schema.schemata where schema_name = $1",
-  res = plv8.execute(sql, [schema_name]);
-  if (!res.length) {
-    sql = "create schema %1$I; grant all on schema %1$I to group xtrole;"
-    plv8.execute(XT.format(sql, [schema_name]));
-  }
-$$ language plv8;
+  perform *
+  from information_schema.schemata
+  where schema_name = sch;
+
+  get diagnostics count = row_count;
+
+  if (count > 0) then
+    return false;
+  end if;
+
+  query = 'create schema ' || sch || ';' ||
+           'grant all on schema ' || sch || ' to group xtrole;';
+  execute query;
+
+  return true;
+
+end;
+$$ language 'plpgsql';
