b07790b9d30ef536da32d28f2230b622ed4cddc9
Gil Moskowitz <gmoskowitz@xtuple.com>
2014-09-16 12:17:46 -0400

    fix? missing plv8 error

--- enyo-client/database/source/delete_system_orms.sql
+++ enyo-client/database/source/delete_system_orms.sql
@@ -1,13 +1,15 @@
-DO $$
+do $$
  /* Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
-     See www.xm.ple.com/CPAL for the full text of the software license. */
+    See www.xtuple.com/CPAL for the full text of the software license. */
 
   var result,
     viewname,
     schemaname,
-    i;
+    i,
+    sql = "select schemaname, viewname " +
+          "from pg_views " +
+          "where schemaname in ('xm','sys', 'xt');";
 
-  sql = "select schemaname, viewname from pg_views where schemaname in ('xm','sys', 'xt');"
   result = plv8.execute(sql);
   for (i = 0; i < result.length; i++) {
     viewname = result[i].viewname;
@@ -15,9 +17,8 @@ DO $$
     plv8.execute('drop view if exists ' + schemaname + '.' + viewname + ' cascade;');
   }
 
-  plv8.execute("select xt.js_init()");
-  plv8.execute("alter table xt.orm disable trigger orm_did_change");
+  plv8.execute("alter table xt.orm disable trigger orm_did_change;");
   plv8.execute("delete from xt.orm where orm_json ~ '\"isSystem\":true';");
-  plv8.execute("alter table xt.orm enable trigger orm_did_change");
+  plv8.execute("alter table xt.orm enable trigger orm_did_change;");
 
 $$ language plv8;
