bd840bd51a0491b7ef570d3d6865b441887eab39
Ben Thompson <ben@xtuple.com>
2014-10-07 17:37:43 -0400

    Fix a few share user edge cases.

--- enyo-client/extensions/source/crm/database/source/xt/trigger_functions/refresh_crmacct_share_users_cache.sql
+++ enyo-client/extensions/source/crm/database/source/xt/trigger_functions/refresh_crmacct_share_users_cache.sql
@@ -11,6 +11,9 @@ return (function () {
     if (NEW.crmacct_usr_username !== null || NEW.crmacct_usr_username !== '') {
       /* Refresh this CRM Account's share access. */
       XT.ShareUsers.refreshCacheUser(NEW.crmacct_usr_username);
+
+      /* Refresh this CRM Account's Owner share access. */
+      XT.ShareUsers.refreshCacheUser(NEW.crmacct_owner_username);
     }
   } else if (TG_OP === 'UPDATE') {
     /* If the CRM Account's crmacct_usr_username changed, refresh the CRM Account user's share access. */
@@ -20,9 +23,20 @@ return (function () {
       /* Refresh this CRM Account new user's share access. */
       XT.ShareUsers.refreshCacheUser(NEW.crmacct_usr_username);
     }
+
+    /* If the CRM Account's crmacct_owner_username changed, refresh the CRM Account Owner's share access. */
+    if (OLD.crmacct_owner_username !== NEW.crmacct_owner_username) {
+      /* Refresh this CRM Account old user's share access. */
+      XT.ShareUsers.refreshCacheUser(OLD.crmacct_owner_username);
+      /* Refresh this CRM Account new user's share access. */
+      XT.ShareUsers.refreshCacheUser(NEW.crmacct_owner_username);
+    }
   } else if (TG_OP === 'DELETE') {
     /* Delete share access cache for this CRM Account user by refreshing it. */
     XT.ShareUsers.refreshCacheUser(OLD.crmacct_usr_username);
+
+    /* Delete share access cache for this CRM Account Owner by refreshing it. */
+    XT.ShareUsers.refreshCacheUser(OLD.crmacct_owner_username);
   }
 
   return NEW;
--- enyo-client/extensions/source/sales/database/source/xt/trigger_functions/refresh_custinfo_share_users_cache.sql
+++ enyo-client/extensions/source/sales/database/source/xt/trigger_functions/refresh_custinfo_share_users_cache.sql
@@ -7,7 +7,11 @@ return (function () {
                     'from addr ' +
                     'left join cntct ON cntct.cntct_addr_id = addr.addr_id ' +
                     'where cntct_id = $1',
-    cntctUuidSql = 'select obj_uuid from cntct where cntct_id = $1';
+    cntctUuidSql = 'select obj_uuid from cntct where cntct_id = $1',
+    repUserSql = 'select crmacct_usr_username as username ' +
+                 'from crmacct ' +
+                 'left join salesrep on crmacct_number = salesrep_number ' +
+                 'where salesrep_id = $1;';
 
   if (typeof XT === 'undefined') {
     plv8.execute("select xt.js_init();");
@@ -16,6 +20,9 @@ return (function () {
   if (TG_OP === 'INSERT') {
     /* Refresh this Customer's share access. */
     XT.ShareUsers.refreshCacheObj(NEW.obj_uuid);
+
+    /* Refresh this Customer's Sales Rep share access. */
+    XT.ShareUsers.refreshRelationCacheUser(repUserSql, [NEW.cust_salesrep_id]);
   } else if (TG_OP === 'UPDATE') {
     /**
      * Because the Ship To's Contact is used to provide limited Child CRM Account
@@ -36,6 +43,12 @@ return (function () {
       XT.ShareUsers.refreshRelationCacheObj(cntctUuidSql, [OLD.cust_corrcntct_id]);
       XT.ShareUsers.refreshRelationCacheObj(cntctUuidSql, [NEW.cust_corrcntct_id]);
     }
+
+    /* If the Sales Rep changes, refresh the old and new Rep's share access. */
+    if (OLD.cust_salesrep_id !== NEW.cust_salesrep_id) {
+      XT.ShareUsers.refreshRelationCacheUser(repUserSql, [OLD.cust_salesrep_id]);
+      XT.ShareUsers.refreshRelationCacheUser(repUserSql, [NEW.cust_salesrep_id]);
+    }
   } else if (TG_OP === 'DELETE') {
     /* Delete share access cache for this Customer. */
     XT.ShareUsers.deleteCacheObj(OLD.obj_uuid);
@@ -47,6 +60,9 @@ return (function () {
     /* Refresh the old Contact's share access. */
     XT.ShareUsers.refreshRelationCacheObj(cntctUuidSql, [OLD.cust_cntct_id]);
     XT.ShareUsers.refreshRelationCacheObj(cntctUuidSql, [OLD.cust_corrcntct_id]);
+
+    /* Refresh the old Sales Rep's share access. */
+    XT.ShareUsers.refreshRelationCacheUser(repUserSql, [OLD.cust_salesrep_id]);
   }
 
   return NEW;
