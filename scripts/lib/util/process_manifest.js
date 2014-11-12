06e22a5383fa9f40fabef070d30996a54845d954
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-11-12 15:17:56 -0500

    more stylish hack

--- scripts/lib/util/process_manifest.js
+++ scripts/lib/util/process_manifest.js
@@ -29,11 +29,7 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
     // infer dependencies from manifest.js -> dependencies
     var isPackageJson = !!options.engines; // XXX this is a pretty rough proxy
     var dependencies = (isPackageJson ? options.peerDependencies : options.dependencies) || [];
-    _.each(dependencies, function (dependency, key) {
-      if (key === "xtuple") {
-        // xtuple itself is not really a dependency
-        return;
-      }
+    _.each(_.omit(dependencies, "xtuple"), function (dependency) {
       var dependencySql = "select xt.register_extension_dependency('%@', '%@');\n"
           .f(options.name, dependency),
         grantDependToAdmin = "select xt.grant_role_ext('ADMIN', '%@');\n"
