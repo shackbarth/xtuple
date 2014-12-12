eb04db9baf38dc96de7bb2b2990949522b07d30b
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-12-12 15:37:30 -0500

    Try check on 4_5_x

--- scripts/install_xtuple.sh
+++ scripts/install_xtuple.sh
@@ -241,6 +241,9 @@ init_everythings() {
 	log "Created testing login_data.js"
 
 	cdir $XT_DIR
+    cd enyo-client/application/tools
+    ./deploy.sh
+    log "finish test enyo deploy"
 	npm run-script test-build 2>&1 | tee -a $LOG_FILE
 
 	log "You can login to the database and mobile client with:"
