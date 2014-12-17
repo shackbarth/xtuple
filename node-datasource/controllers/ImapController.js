65e7fff8588db7444462148fa2dedc33417888d1
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-12-17 16:46:24 -0500

    add todos

--- node-datasource/controllers/ImapController.js
+++ node-datasource/controllers/ImapController.js
@@ -12,18 +12,8 @@
   var reportedFields = ["html", "text", "subject", "from", "to", "date"],
     returnResults = [];
 
-  // https://shackbarth-470-dev.localhost/demo_dev/imap/search?address=to_or_from@mycompany.com
-
-  /*
-    Necessary setup: configure your mail server so that it automatically bcc's an "archive"
-    email address. Put the creds for that address in the config file.
-
-    Not sure if this is possible with gmail, I think it's unlikely:
-    http://thenextweb.com/google/2012/07/03/if-youre-a-google-apps-admin-you-will-now-have-access-to-a-gmail-log-search
-
-    Gmail has lots of nice integration options, and this route could easily be expanded to
-    use the gmail API without changing its own API to the xTuple clients.
-  */
+  // the helper function that does all the work of searching imap.
+  // can be called by the route or through the CLI.
   var searchImap = function (creds, box, query, callback) {
     var imap = new Imap(creds);
     imap.once('ready', function () {
@@ -88,10 +78,17 @@
     });
   };
 
+  // https://localhost/demo_dev/imap/search?address=you@yourcompany.com
   exports.search = function (req, res) {
     var query = [ 'ALL', ['OR', ['FROM', req.query.address], ['TO', req.query.address] ] ];
     // TODO: secure this route with a privilege
-    // TODO: we should really store these creds in the database and have a UI to manage them
+    // TODO: allow other kinds of queries other than to/from address?
+    // https://github.com/mscdex/node-imap#examples
+    // TODO for v2.0: we should really store these creds in the database and have a UI to manage them
+
+    // TODO: if (X.options.datasource.imap.useCache) { search the database instead of the imap server }
+    //    see scripts/cache_email.js
+
     searchAllImap(X.options.datasource.imap, query, function (err, results) {
       if (err) {
         console.log("Error searching imap", err);
--- scripts/cache_email.js
+++ scripts/cache_email.js
@@ -5,8 +5,8 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
 /*global _:true */
 
 //
-// This file really just parses the arguments, and sends the real work
-// off to scripts/lib/build_all.js.
+// In order to cache emails in the database, customers are going to want to set this
+// to run on a cron.
 //
 
 (function () {
@@ -37,16 +37,34 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
       return;
     }
     var options = JSON.parse(JSON.stringify(config.databaseServer));
-    var sql = "SELECT setMetric('Test234', $1);";
+    var sql = "SELECT setMetric('Test234', $1);"; // XXX obviously it won't be setMetric here
 
     // TODO: persist to email table
-    // TODO: if the message line matches some document, add to docass
+    // no need to use the existing eml tables
+    // probably one table will suffice
 
-    options.database = config.databases[0];
-    options.parameters = [JSON.stringify(results)];
+    // columns could be:
+    // id
+    // subject
+    // date
+    // from
+    // to
+    // cc ? bcc ?
+    // subject
+    // body as text
+    // body as html
+    // attachments (maybe v2.0)
+    // hash (hash the body as a way to check to see if this email is already in the table)
+    // other fields from the header? (imap message id)
+
+    // TODO: if the subject line matches some document, add to docass
+    //   use the existing conventions for what the email subject should look like to denote
+    //   a document link
+
+    options.database = config.databases[0]; // XXX bit of an assumption here
+    options.parameters = [JSON.stringify(results)]; // XXX obviously no
     dataSource.query(sql, options, function (err, insertResults) {
       console.log("Imap cache succeeded with " + results.length + " records");
-      console.log("done", arguments);
     });
   });
 
